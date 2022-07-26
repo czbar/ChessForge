4fc8f58abb7f7c981ed2c87b1f64a8f6f008476a
czbar <czbar@users.noreply.github.com>
2022-07-26 18:48:08 +1000

    Cosmetics and comments

--- ChessForge/BookmarkView.cs
+++ ChessForge/BookmarkView.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using GameTree;
-using ChessPosition;
+﻿using GameTree;
 
 namespace ChessForge
 {
@@ -14,6 +8,7 @@ namespace ChessForge
     /// </summary>
     public class BookmarkView
     {
+
         public BookmarkView(ChessBoard board)
         {
             _guiBoard = board;
