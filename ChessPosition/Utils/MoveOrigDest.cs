3ab8aec77b7185ca142696157d5c9d1943b0a628
czbar <czbar@users.noreply.github.com>
2022-08-12 21:30:33 +1000

    Create MoveOrigDest.cs

---
+++ ChessPosition/Utils/MoveOrigDest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessPosition.Utils
+{
+    /// <summary>
+    /// A convenience class holding
+    /// the move's origin and destination coordinates.
+    /// </summary>
+    public struct MoveOrigDest
+    {
+        /// <summary>
+        /// Move's origin coordinates.
+        /// </summary>
+        public SquareCoords Origin;
+
+        /// <summary>
+        /// Move's destination coordinates.
+        /// </summary>
+        public SquareCoords Destination;
+
+        /// <summary>
+        /// Construct the object and sets the coordinates.
+        /// </summary>
+        /// <param name="orig"></param>
+        /// <param name="dest"></param>
+        public MoveOrigDest(SquareCoords orig, SquareCoords dest)
+        {
+            Origin = orig;
+            Destination = dest;
+        }
+    }
+}
