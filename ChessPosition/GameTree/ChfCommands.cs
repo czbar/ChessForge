4b024d524f9260a117bd65eed5684f936098510e
czbar <czbar@users.noreply.github.com>
2022-08-15 16:21:55 +1000

    Renamed ChfCommands to ChfCommands

--- ChessPosition/GameTree/ChfCommands.cs
+++ ChessPosition/GameTree/ChfCommands.cs
@@ -6,19 +6,36 @@ using System.Threading.Tasks;
 
 namespace GameTree
 {
-    public class CfhCommands
+    /// <summary>
+    /// Encapsulates special Chess Forge commands extending
+    /// the PGN format.
+    /// </summary>
+    public class ChfCommands
     {
+        /// <summary>
+        /// Command IDs
+        /// </summary>
         public enum Command
         {
             NONE,
-            BOOKMARK
+            BOOKMARK,
+            ENGINE_EVALUATION
         }
 
+        /// <summary>
+        /// Map of PGN strings to Command IDs
+        /// </summary>
         private static Dictionary<string, Command> _dictCommands = new Dictionary<string, Command>()
         {
             ["%chf-bkm"] = Command.BOOKMARK,
+            ["%chf-eev"] = Command.ENGINE_EVALUATION,
         };
 
+        /// <summary>
+        /// Returns the command id given a string.
+        /// </summary>
+        /// <param name="sCmd"></param>
+        /// <returns></returns>
         public static Command GetCommand(string sCmd)
         {
             Command cmd;
@@ -27,6 +44,12 @@ namespace GameTree
             else
                 return Command.NONE;
         }
+
+        /// <summary>
+        /// Returns a string for a given Command Id.
+        /// </summary>
+        /// <param name="cmd"></param>
+        /// <returns></returns>
         public static string GetStringForCommand(Command cmd)
         {
             return _dictCommands.FirstOrDefault(x => x.Value == cmd).Key;
