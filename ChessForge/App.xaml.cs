96d7ebade4f24d906ea9c4c9e85ededdff0f6c83
czbar <czbar@users.noreply.github.com>
2022-08-21 16:30:47 +1000

    Implemented Application_Startup

--- ChessForge/App.xaml.cs
+++ ChessForge/App.xaml.cs
@@ -13,5 +13,21 @@ namespace ChessForge
     /// </summary>
     public partial class App : Application
     {
+        // file name passed as the first command line parameter
+        public static string CmdLineFileName = "";
+
+        /// <summary>
+        /// Invoked on application start up.
+        /// Processes command line parameters.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            if (e.Args.Length > 0)
+            {
+                CmdLineFileName = e.Args[0];
+            }
+        }
     }
 }
