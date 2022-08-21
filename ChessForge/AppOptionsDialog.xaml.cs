9e150702d5af0ee4d6441adaddbc99ef2f0e90cd
czbar <czbar@users.noreply.github.com>
2022-08-22 00:23:17 +1000

    Implemented Options dialogs

---
+++ ChessForge/AppOptionsDialog.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.IO;
+
+namespace ChessForge
+{
+    /// <summary>
+    /// Interaction logic for AppOptions.xaml
+    /// </summary>
+    public partial class AppOptionsDialog : Window
+    {
+        /// <summary>
+        /// True if dialog exited by clicking Save
+        /// </summary>
+        public bool ExitOK = false;
+
+        public string EnginePath;
+
+        public double ReplaySpeed;
+
+        public double EngineTimePerMoveInGame;
+
+        public double EngineTimePerMoveInEvaluation;
+
+        public AppOptionsDialog()
+        {
+            InitializeComponent();
+            EnginePath = Configuration.EngineExePath;
+            ReplaySpeed = (double)Configuration.MoveSpeed / 1000.0;
+            EngineTimePerMoveInGame = (double)Configuration.EngineMoveTime / 1000.0;
+            EngineTimePerMoveInEvaluation = (double)Configuration.EngineEvaluationTime / 1000.0;
+
+            _tbEngineExe.Text = EnginePath;
+            _tbReplaySpeed.Text = ReplaySpeed.ToString("F1");
+            _tbEngTimeInGame.Text = EngineTimePerMoveInGame.ToString("F1");
+            _tbEngEvalTime.Text = EngineTimePerMoveInEvaluation.ToString("F1");
+        }
+
+        private void _btnLocateEngine_Click(object sender, RoutedEventArgs e)
+        {
+            string searchPath = Path.GetDirectoryName(Configuration.EngineExePath);
+            string res = Configuration.SelectEngineExecutable(searchPath);
+            if (!string.IsNullOrEmpty(res))
+            {
+                EnginePath = res;
+            }
+        }
+
+        private void _btnSave_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void _btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ ChessForge/WorkbookOptionsDialog.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using ChessPosition;
+using GameTree;
+
+namespace ChessForge
+{
+    /// <summary>
+    /// Interaction logic for WorkbookOptionsDialog.xaml
+    /// </summary>
+    public partial class WorkbookOptionsDialog : Window
+    {
+        private readonly string _strWhite = "WHITE";
+        private readonly string _strBlack = "BLACK";
+
+        /// <summary>
+        /// Creates the dialog, initializes controls
+        /// </summary>
+        public WorkbookOptionsDialog(WorkbookTree _workbook)
+        {
+            InitializeComponent();
+            WorkbookTitle = _workbook.Title;
+            TrainingSide = _workbook.TrainingSide;
+
+            _tbTitle.Text = _workbook.Title;
+            _tbSideOnMove.Text = _workbook.TrainingSide == PieceColor.White ? _strWhite : _strBlack;
+        }
+
+        /// <summary>
+        /// The Training Side selected in the dialog.
+        /// </summary>
+        public PieceColor TrainingSide;
+
+        /// <summary>
+        /// The Workbook Title entered by the user
+        /// </summary>
+        public string WorkbookTitle;
+
+        /// <summary>
+        /// True if dialog exited by clicking Save
+        /// </summary>
+        public bool ExitOK = false;
+
+        /// <summary>
+        /// The user pressed the OK button.
+        /// Saves the workbook's title and exits.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void _btnOK_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            ExitOK = true;
+            this.Close();
+        }
+
+        /// <summary>
+        /// In response to the user clicking on the Swap icon,
+        /// swaps the training side.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void _imgSwapColor_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (_tbSideOnMove.Text == _strWhite)
+            {
+                _tbSideOnMove.Text = _strBlack;
+                TrainingSide = PieceColor.Black;
+            }
+            else
+            {
+                _tbSideOnMove.Text = _strWhite;
+                TrainingSide = PieceColor.White;
+            }
+        }
+
+        /// <summary>
+        /// Exists the dialog without setting ExitOK to true.
+        /// The caller should consider such exit as cancellation.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void _btnCancel_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
