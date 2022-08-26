14e29cb72829440346728180b3d0b9c66ed51042
czbar <czbar@users.noreply.github.com>
2022-08-26 18:17:51 +1000

    Implemented ShowDebugMessage()

--- ChessPosition/Utils/DebugUtils.cs
+++ ChessPosition/Utils/DebugUtils.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows;
 
 namespace ChessPosition
 {
     public class DebugUtils
     {
+        /// <summary>
+        /// Maps Pieces to letters for the debug output.
+        /// </summary>
         public static Dictionary<PieceType, char> FenPieceToChar = new Dictionary<PieceType, char>()
         {
             [PieceType.Rook] = 'r',
@@ -18,6 +22,21 @@ namespace ChessPosition
             [PieceType.None] = '-'
         };
 
+        /// <summary>
+        /// Displays an error identified in debug mode to the user.
+        /// </summary>
+        /// <param name="msg"></param>
+        public static void ShowDebugMessage(string msg)
+        {
+            MessageBox.Show(msg, "DEBUG", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        /// <summary>
+        /// Prints the passed position to the Console in 
+        /// a human readable format.
+        /// </summary>
+        /// <param name="board"></param>
+        /// <param name="fenString"></param>
         public static void PrintPosition(BoardPosition board, string fenString = null)
         {
             if (!string.IsNullOrEmpty(fenString))
@@ -77,6 +96,11 @@ namespace ChessPosition
             Console.WriteLine("");
         }
 
+        /// <summary>
+        /// Prints a single board row to the Console.
+        /// </summary>
+        /// <param name="row"></param>
+        /// <param name="board"></param>
         private static void PrintRow(int row, BoardPosition board)
         {
             Console.Write((row + 1) + ": ");
@@ -93,6 +117,13 @@ namespace ChessPosition
             Console.WriteLine("");
         }
 
+        /// <summary>
+        /// Pronts the list of "attackers" of a given square
+        /// to the Console.
+        /// </summary>
+        /// <param name="algSquare"></param>
+        /// <param name="col"></param>
+        /// <param name="attackers"></param>
         public static void PrintAttackers(string algSquare, PieceColor col, List<Square> attackers)
         {
             Console.WriteLine("");
@@ -117,6 +148,11 @@ namespace ChessPosition
             Console.WriteLine("");
         }
 
+        /// <summary>
+        /// Prints a single move to the Console.
+        /// </summary>
+        /// <param name="move"></param>
+        /// <param name="alg"></param>
         public static void PrintMove(MoveData move, string alg)
         {
             Console.WriteLine("");
