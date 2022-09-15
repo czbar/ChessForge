98cc68c94a7d8a10723ed5e7246250b2a4c15626
czbar <czbar@users.noreply.github.com>
2022-09-15 16:59:15 +1000

    Comment

--- ChessForge/MainWindow.xaml.cs
+++ ChessForge/MainWindow.xaml.cs
@@ -1316,6 +1316,11 @@ namespace ChessForge
             UpdateLastMoveTextBox(moveTxt);
         }
 
+        /// <summary>
+        /// Sets text for the label showing the last/current
+        /// move (depending on the context it can be e.g. the move being evaluated).
+        /// </summary>
+        /// <param name="moveTxt"></param>
         public void UpdateLastMoveTextBox(string moveTxt)
         {
             UiLblMoveUnderEval.Dispatcher.Invoke(() =>
