15b1e3ae264cb58b90751e033dcb13c889718cec
czbar <czbar@users.noreply.github.com>
2022-09-16 18:45:38 +1000

    Removed unnecessary assignment

--- ChessForge/ActiveLineManager.cs
+++ ChessForge/ActiveLineManager.cs
@@ -108,7 +108,6 @@ namespace ChessForge
                     }
                 }
 
-                nd.EngineEvaluation = eval;
                 AppStateManager.IsDirty = true;
             }
         }
