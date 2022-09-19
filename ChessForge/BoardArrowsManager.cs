cf52e06df3f8fc12843ef70675f4804c3e19e7ec
czbar <czbar@users.noreply.github.com>
2022-09-20 04:13:06 +1000

    Coding/decoding Arrows string for CHF/PGN files

--- ChessForge/BoardArrowsManager.cs
+++ ChessForge/BoardArrowsManager.cs
@@ -44,38 +44,12 @@ namespace ChessForge
                 string[] tokens = arrows.Split(',');
                 foreach (string token in tokens)
                 {
-                    // should be exactly 5 chars
-                    if (token.Length == 5)
+                    if (DecodeArrowString(token, out string color, out SquareCoords start, out SquareCoords end))
                     {
-                        string color = GetColorName(token[0]);
-                        SquareCoords start = PositionUtils.ConvertAlgebraicToXY(token.Substring(1, 2));
-                        SquareCoords end = PositionUtils.ConvertAlgebraicToXY(token.Substring(3, 2));
-                        if (AppStateManager.MainWin.IsMainChessboardFlipped())
-                        {
-                            start.Flip();
-                            end.Flip();
-                        }
                         StartArrowDraw(start, color);
-                        FinalizeArrow(end);
+                        FinalizeArrow(end, false);
                     }
                 }
-            }
-        }
-
-        private static string GetColorName(char c)
-        {
-            switch (c)
-            {
-                case 'G':
-                    return "green";
-                case 'B':
-                    return "blue";
-                case 'R':
-                    return "red";
-                case 'Y':
-                    return "yellow";
-                default:
-                    return "yellow";
             }
         }
 
@@ -94,6 +68,7 @@ namespace ChessForge
 
             _isArrowBuildInProgress = false;
         }
+
         /// <summary>
         // Flags if there is a new arrow being built
         /// </summary>
@@ -132,12 +107,18 @@ namespace ChessForge
         /// Finishes drawing the arrow and saves it in the list.
         /// </summary>
         /// <param name="current"></param>
-        public static void FinalizeArrow(SquareCoords current)
+        public static void FinalizeArrow(SquareCoords current, bool isNew)
         {
             _endSquare = new SquareCoords(current);
             UpdateArrowDraw(current);
             _boardArrows.Add(_arrowInProgress);
             CancelArrowDraw();
+
+            SaveArrowsString();
+            if (isNew)
+            {
+                AppStateManager.IsDirty = true;
+            }
         }
 
         /// <summary>
@@ -171,6 +152,121 @@ namespace ChessForge
             _endSquare = null;
             _isArrowBuildInProgress = false;
             _arrowInProgress = null;
+        }
+
+        /// <summary>
+        /// Saves the Arrow positions to the Node.
+        /// </summary>
+        private static void SaveArrowsString()
+        {
+            AppStateManager.MainWin.SaveArrowsStringInCurrentNode(CodeArrowsString());
+        }
+
+        /// <summary>
+        /// Decodes the Arrow data string. 
+        /// </summary>
+        /// <param name="code"></param>
+        /// <param name="color"></param>
+        /// <param name="start"></param>
+        /// <param name="end"></param>
+        /// <returns></returns>
+        private static bool DecodeArrowString(string code, out string color, out SquareCoords start, out SquareCoords end)
+        {
+            start = end = null;
+            color = "";
+
+            // must be exactly 5 chars
+            if (code.Length == 5)
+            {
+                color = GetColorName(code[0]);
+                start = PositionUtils.ConvertAlgebraicToXY(code.Substring(1, 2));
+                end = PositionUtils.ConvertAlgebraicToXY(code.Substring(3, 2));
+                if (AppStateManager.MainWin.IsMainChessboardFlipped())
+                {
+                    start.Flip();
+                    end.Flip();
+                }
+            }
+
+            return start != null && end != null;
+        }
+
+        /// <summary>
+        /// Encodes Arrow positions into a string.
+        /// </summary>
+        /// <returns></returns>
+        private static string CodeArrowsString()
+        {
+            StringBuilder sb = new StringBuilder();
+            bool first = true;
+            foreach (BoardArrow arrow in _boardArrows)
+            {
+                if (first)
+                {
+                    first = false;
+                }
+                else
+                {
+                    sb.Append(',');
+                }
+                sb.Append(GetCharForColor(arrow.Color));
+                SquareCoords start = new SquareCoords(arrow.StartSquare);
+                SquareCoords end = new SquareCoords(arrow.EndSquare);
+                if (AppStateManager.MainWin.IsMainChessboardFlipped())
+                {
+                    start.Flip();
+                    end.Flip();
+                }
+                sb.Append(PositionUtils.ConvertXYtoAlgebraic(start.Xcoord, start.Ycoord));
+                sb.Append(PositionUtils.ConvertXYtoAlgebraic(end.Xcoord, end.Ycoord));
+            }
+
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Converts the PGN char to the name of a color that
+        /// BoardArrow object understands.
+        /// </summary>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        private static string GetColorName(char c)
+        {
+            switch (c)
+            {
+                case 'G':
+                    return "green";
+                case 'B':
+                    return "blue";
+                case 'R':
+                    return "red";
+                case 'Y':
+                    return "yellow";
+                default:
+                    return "yellow";
+            }
+        }
+
+        /// <summary>
+        /// Converts the name of a color to a PGN char.
+        /// </summary>
+        /// <param name="color"></param>
+        /// <returns></returns>
+        private static char GetCharForColor(string color)
+        {
+            switch (color)
+            {
+                case "green":
+                    return 'G';
+                case "blue":
+                    return 'B';
+                case "red":
+                    return 'R';
+                case "yellow":
+                    return 'Y';
+                default:
+                    return 'Y';
+            }
         }
     }
 }
