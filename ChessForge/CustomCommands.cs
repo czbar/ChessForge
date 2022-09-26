e42bcb8926742bac17950a246f6804b98a5fc557
czbar <czbar@users.noreply.github.com>
2022-09-26 20:15:40 +1000

    Added CustomCommands class

---
+++ ChessForge/CustomCommands.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ChessForge
+{
+    public static class CustomCommands
+    {
+        public static RoutedCommand FlipBoard = new RoutedCommand();
+    }
+}
