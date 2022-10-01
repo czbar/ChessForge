40f9ec509d1dea32efca1de4e52b31923cbbfaf7
czbar <czbar@users.noreply.github.com>
2022-10-01 21:58:55 +1000

    New objects to support multi-chapter Workbooks

---
+++ ChessForge/Chapter.cs
@@ -0,0 +1,30 @@
+﻿using GameTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessForge
+{
+    /// <summary>
+    /// A chapter within a Workbook.
+    /// A chapter comprises one or more VariationTrees. 
+    /// </summary>
+    public class Chapter
+    {
+        // number of this chapter
+        private int _number;
+
+        // Variation Trees constituting this chapter.
+        private List<WorkbookTree> _variationTrees = new List<WorkbookTree>();
+
+        /// <summary>
+        /// Number of this chapter.
+        /// </summary>
+        public int Number
+        {
+            get => _number; set => _number = value;
+        }
+    }
+}
---
+++ ChessForge/Workbook.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessForge
+{
+    /// <summary>
+    /// Holds references to all components of a Workbook.
+    /// A workbook consists of one or more chapters.
+    /// Each chapter, in turn, holds one or more Variation Tree.
+    /// </summary>
+    public class Workbook
+    {
+    }
+}
