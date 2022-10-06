07f57f02723dd50a716f15d7891616c31e27c013
czbar <czbar@users.noreply.github.com>
2022-10-06 22:24:58 +1100

    Implemented ChaptersView

---
+++ ChessForge/ChaptersView.cs
@@ -0,0 +1,190 @@
+﻿using GameTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows;
+using System.Windows.Documents;
+using ChessPosition;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+namespace ChessForge
+{
+    /// <summary>
+    /// Manages text and events in the Chaptesr view.
+    /// The view is hosted in a RichTextBox.
+    /// </summary>
+    public class ChaptersView : RichTextBuilder
+    {
+        // Application's Main Window
+        private MainWindow _mainWin;
+
+        /// <summary>
+        /// RichTextPara dictionary accessor
+        /// </summary>
+        override internal Dictionary<string, RichTextPara> RichTextParas { get { return _richTextParas; } }
+
+        private static readonly string STYLE_WORKBOOK_TITLE = "workbook_title";
+        private static readonly string STYLE_CHAPTER_TITLE = "chapter_title";
+        private static readonly string STYLE_STUDY_TREE = "study_tree";
+        private static readonly string STYLE_MODEL_GAME = "model_game";
+        private static readonly string STYLE_EXERCISE = "exercise";
+
+        /// <summary>
+        /// Layout definitions for paragrahs at different levels.
+        /// </summary>
+        private Dictionary<string, RichTextPara> _richTextParas = new Dictionary<string, RichTextPara>()
+        {
+            [STYLE_WORKBOOK_TITLE] = new RichTextPara(0, 10, 18, FontWeights.Bold, null, TextAlignment.Left),
+            [STYLE_CHAPTER_TITLE] = new RichTextPara(10, 10, 16, FontWeights.Normal, null, TextAlignment.Left),
+            [STYLE_STUDY_TREE] = new RichTextPara(40, 10, 14, FontWeights.Normal, null, TextAlignment.Left),
+            [STYLE_MODEL_GAME] = new RichTextPara(70, 5, 14, FontWeights.Normal, null, TextAlignment.Left),
+            [STYLE_EXERCISE] = new RichTextPara(90, 5, 14, FontWeights.Normal, null, TextAlignment.Left),
+            ["default"] = new RichTextPara(140, 5, 11, FontWeights.Normal, null, TextAlignment.Left),
+        };
+
+        /// <summary>
+        /// Maps chapter Ids to Chapter objects
+        /// </summary>
+        private Dictionary<int, Paragraph> _dictChapterParas = new Dictionary<int, Paragraph>();
+
+        /// <summary>
+        /// Names and prefixes for the Runs.
+        /// They will identify the element of the Workbook structure being shown.
+        /// In the run names they will be followed by the element's index in their
+        /// respective list.
+        /// </summary>
+        private readonly string _run_chapter_expand_char_ = "_run_chapter_expand_char_";
+        private readonly string _run_chapter_ = "_run_chapter_";
+        private readonly string _run_study_tree_ = "study_tree_";
+        private readonly string _run_model_game_ = "model_game_";
+        private readonly string _run_exercise_ = "exercise_";
+
+        /// <summary>
+        /// Names and prefixes for the Paragraphs.
+        /// </summary>
+        private readonly string _par_workbook_title_ = "par_workbook_title_";
+        private readonly string _par_chapter_ = "par_chapter_";
+
+        /// <summary>
+        /// Constructor. Sets a reference to the 
+        /// FlowDocument for the RichTextBox control, via
+        /// a call to the base class's constructor.
+        /// </summary>
+        /// <param name="doc"></param>
+        public ChaptersView(FlowDocument doc, MainWindow mainWin) : base(doc)
+        {
+            _mainWin = mainWin;
+        }
+
+        /// <summary>
+        /// Builds a list of paragraphs to show the chapters in this Workbook and their content.
+        /// </summary>
+        public void BuildFlowDocumentForChaptersView()
+        {
+            Document.Blocks.Clear();
+            _dictChapterParas.Clear();
+
+            AddNewParagraphToDoc(STYLE_WORKBOOK_TITLE, WorkbookManager.SessionWorkbook.Title);
+
+            foreach (Chapter chapter in WorkbookManager.SessionWorkbook.Chapters)
+            {
+                Paragraph para = BuildChapterParagraph(chapter);
+                Document.Blocks.Add(para);
+                _dictChapterParas[chapter.Id] = para;
+            }
+        }
+
+        /// <summary>
+        /// Builds a paragraph for a single chapter
+        /// </summary>
+        /// <param name="chapter"></param>
+        /// <param name="para"></param>
+        /// <returns></returns>
+        private Paragraph BuildChapterParagraph(Chapter chapter, Paragraph para = null)
+        {
+            try
+            {
+                if (para == null)
+                {
+                    para = AddNewParagraphToDoc(STYLE_CHAPTER_TITLE, "");
+                }
+                else
+                {
+                    para.Inlines.Clear();
+                }
+
+                para.Name = _par_chapter_ + chapter.Id.ToString();
+
+                char expandCollapse = chapter.IsViewExpanded ? Constants.CharCollapse : Constants.CharExpand;
+                Run rExpandChar = CreateRun(STYLE_CHAPTER_TITLE, expandCollapse.ToString() + " ");
+                rExpandChar.Name = _run_chapter_expand_char_ + chapter.Id.ToString();
+                rExpandChar.MouseDown += EventExpandSymbolClicked;
+                para.Inlines.Add(rExpandChar);
+
+                Run rTitle = CreateRun(STYLE_CHAPTER_TITLE, chapter.Title);
+                rTitle.Name = _run_chapter_ + chapter.Id.ToString();
+                para.Inlines.Add(rTitle);
+
+                para.Inlines.Add(new Run("\n"));
+
+                if (chapter.IsViewExpanded)
+                {
+                    Run rStudy = CreateRun(STYLE_STUDY_TREE, "        Study Tree");
+                    rStudy.Name = _run_study_tree_ + "1";
+                    para.Inlines.Add(rStudy);
+                }
+
+                return para;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Event handler invoked when a Run was clicked.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void EventRunClicked(object sender, MouseButtonEventArgs e)
+        {
+        }
+
+        /// <summary>
+        /// An expand/collapse character was clicked.
+        /// Establish which chapter this is for, check its expand/collapse status and flip it.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void EventExpandSymbolClicked(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                Run r = (Run)e.Source;
+                int chapterId = GetNodeIdFromRunName(r.Name, _run_chapter_expand_char_);
+                Chapter chapter = WorkbookManager.SessionWorkbook.GetChapter(chapterId);
+                bool? isExpanded = chapter.IsViewExpanded;
+
+                if (isExpanded == true)
+                {
+                    chapter.IsViewExpanded = false;
+                }
+                else if (isExpanded == false)
+                {
+                    chapter.IsViewExpanded = true;
+                }
+
+                BuildChapterParagraph(chapter, _dictChapterParas[chapter.Id]);
+            }
+            catch (Exception ex)
+            {
+                AppLog.Message("Exception in EventExpandSymbolClicked(): " + ex.Message);
+            }
+        }
+    }
+}
