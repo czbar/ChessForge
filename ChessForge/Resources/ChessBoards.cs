d6edc6ede72d2212d65a283c8f12ec51c597438e
czbar <czbar@users.noreply.github.com>
2022-10-13 20:42:09 +1100

    Added small board image

--- ChessForge/Resources/ChessBoards.cs
+++ ChessForge/Resources/ChessBoards.cs
@@ -13,6 +13,7 @@ namespace ChessForge
     public class ChessBoards
     {
         public static BitmapImage ChessBoardBlue = new BitmapImage(new Uri("pack://application:,,,/Resources/Images/ChessBoardBlue.png", UriKind.RelativeOrAbsolute));
+        public static BitmapImage ChessBoardBlueSmall = new BitmapImage(new Uri("pack://application:,,,/Resources/Images/ChessBoardBlueSmall.png", UriKind.RelativeOrAbsolute));
         public static BitmapImage ChessBoardBrown = new BitmapImage(new Uri("pack://application:,,,/Resources/Images/ChessBoardBrown.png", UriKind.RelativeOrAbsolute));
         public static BitmapImage ChessBoardGreen = new BitmapImage(new Uri("pack://application:,,,/Resources/Images/ChessBoardGreen.png", UriKind.RelativeOrAbsolute));
     }
