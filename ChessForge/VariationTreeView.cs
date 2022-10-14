0eaa36966ff6255bf60430be73939405c1e59eb9
czbar <czbar@users.noreply.github.com>
2022-10-14 19:18:31 +1100

    Improved handling of ContentType

--- ChessForge/VariationTreeView.cs
+++ ChessForge/VariationTreeView.cs
@@ -273,7 +273,7 @@ namespace ChessForge
                         _dictNodeToRun[nd.NodeId].Background = _brushRegularBkg;
                     }
                     else if (Configuration.DebugMode != 0)
-                    {  //we should always have this key, so show deubug message if not
+                    {  //we should always have this key, so show debug message if not
                         DebugUtils.ShowDebugMessage("WorkbookView:SelectLineAndMove()-brushRegularBkg nodeId=" + nd.NodeId.ToString() + " not in _dictNodeToRun");
                     }
                 }
@@ -394,12 +394,12 @@ namespace ChessForge
 
             if (_variationTree != null)
             {
-                string contentType = _variationTree.Header.GetContentType(out _);
+                GameMetadata.ContentType contentType = _variationTree.Header.GetContentType(out _);
                 
                 switch (contentType)
                 {
-                    case PgnHeaders.VALUE_MODEL_GAME:
-                    case PgnHeaders.VALUE_EXERCISE:
+                    case GameMetadata.ContentType.MODEL_GAME:
+                    case GameMetadata.ContentType.EXERCISE:
                         string whitePlayer = _variationTree.Header.GetWhitePlayer(out _);
                         string blackPlayer = _variationTree.Header.GetBlackPlayer(out _);
 
@@ -451,7 +451,7 @@ namespace ChessForge
         /// <returns></returns>
         private Paragraph BuildExercisesChessboardParagraph()
         {
-            if (_variationTree != null && _variationTree.Header.GetContentType(out _) == PgnHeaders.VALUE_EXERCISE)
+            if (_variationTree != null && _variationTree.Header.GetContentType(out _) == GameMetadata.ContentType.EXERCISE)
             {
                 Paragraph para = CreateParagraph("2");
                 para.Margin = new Thickness(0,0,0,40);
