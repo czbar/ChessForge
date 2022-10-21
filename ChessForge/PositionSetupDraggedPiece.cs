daf1f540d17e22b3535360340b8b1527c5866af5
czbar <czbar@users.noreply.github.com>
2022-10-21 20:36:31 +1100

    Added PositionSetupDraggedPiece

---
+++ ChessForge/PositionSetupDraggedPiece.cs
@@ -0,0 +1,28 @@
+﻿using ChessPosition;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+
+namespace ChessForge
+{
+    public class PositionSetupDraggedPiece
+    {
+        /// <summary>
+        /// Image control being dragged.
+        /// </summary>
+        public Image ImageControl;
+
+        public PieceType Type;
+
+        public PieceColor Color;
+
+        /// <summary>
+        /// Whether we are currently dragging a piece
+        /// </summary>
+        public bool IsDragInProgress = false;
+    }
+}
