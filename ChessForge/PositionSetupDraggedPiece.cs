6e4954fe8b1547c4b1bce81cd1f3a5570602ec37
czbar <czbar@users.noreply.github.com>
2022-10-22 18:13:45 +1100

    Added functionality

--- ChessForge/PositionSetupDraggedPiece.cs
+++ ChessForge/PositionSetupDraggedPiece.cs
@@ -11,18 +11,48 @@ namespace ChessForge
 {
     public class PositionSetupDraggedPiece
     {
+        // whether there is a drag process in progress
+        private bool _isDragInProgress;
+
+        /// <summary>
+        /// Whether we are currently dragging a piece
+        /// </summary>
+        public bool IsDragInProgress
+        {
+            get { return _isDragInProgress; }
+            set { _isDragInProgress = value; }
+        }
+
+        /// <summary>
+        /// Resets all properties.
+        /// </summary>
+        public void Clear()
+        {
+            OriginSquare = null;
+            Piece = PieceType.None;
+            Color = PieceColor.None;
+            ImageControl = null;
+        }
+
         /// <summary>
         /// Image control being dragged.
         /// </summary>
         public Image ImageControl;
 
-        public PieceType Type;
+        /// <summary>
+        /// The type of piece being dragged.
+        /// </summary>
+        public PieceType Piece;
 
+        /// <summary>
+        /// The color of the piece being dragged.
+        /// </summary>
         public PieceColor Color;
 
         /// <summary>
-        /// Whether we are currently dragging a piece
+        /// Coordinates from which the piece was dragging.
+        /// Null if this was an off-board piece.
         /// </summary>
-        public bool IsDragInProgress = false;
+        public SquareCoords OriginSquare;
     }
 }
