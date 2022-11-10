a0291110de9d3cb6cf9da9c56fd5579da7a19bda
czbar <czbar@users.noreply.github.com>
2022-11-10 17:35:43 +1100

    Dialog shown after new chapter created from variation

---
+++ ChessForge/ChapterFromLineDialog.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ChessForge
+{
+    /// <summary>
+    /// Interaction logic for ChapterFromLineDialog.xaml
+    /// </summary>
+    public partial class ChapterFromLineDialog : Window
+    {
+        /// <summary>
+        /// True if the user pushed Yes or No to exit.
+        /// </summary>
+        public bool ExitOK = false;
+
+        /// <summary>
+        /// Whether the user chose to go to the new chapter
+        /// after creating it.
+        /// </summary>
+        public bool GoToNewChapter = false;
+
+        /// <summary>
+        /// Whether to delete the subtree of the new chapter
+        /// from the origin Study.
+        /// </summary>
+        public bool DeleteOriginal = false;
+
+        /// <summary>
+        /// Creates the dialog.
+        /// Show the Chapter's title.
+        /// </summary>
+        /// <param name="chapter"></param>
+        public ChapterFromLineDialog(Chapter chapter)
+        {
+            InitializeComponent();
+            UiLblChapterTitle.Content = chapter.GetTitle();
+        }
+
+        /// <summary>
+        /// Gets data from the controls.
+        /// </summary>
+        private void CollectResponses()
+        {
+            GoToNewChapter = UiCbGoToNew.IsChecked == true;
+            ExitOK = true;
+        }
+
+        /// <summary>
+        /// User chose to remove the variation from the origin.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void UiBtnYes_Click(object sender, RoutedEventArgs e)
+        {
+            DeleteOriginal = true;
+            CollectResponses();
+            Close();
+        }
+
+        /// <summary>
+        /// User chose to keep the variation in the origin.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void UiBtnNo_Click(object sender, RoutedEventArgs e)
+        {
+            DeleteOriginal = false;
+            CollectResponses();
+            Close();
+        }
+    }
+}
