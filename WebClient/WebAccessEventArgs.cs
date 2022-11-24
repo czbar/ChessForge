91f13e68238605e2d56b3f0d08f1f9ed92b8b920
czbar <czbar@users.noreply.github.com>
2022-11-24 16:13:49 +1100

    Create WebAccessEventArgs.cs

---
+++ WebClient/WebAccessEventArgs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAccess
+{
+    /// <summary>
+    /// EventArgs for the Web Access events
+    /// </summary>
+    public class WebAccessEventArgs : EventArgs
+    {
+        public bool Sucess { get; set; }
+    }
+}
