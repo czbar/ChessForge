de5594667b31e4ef5f9f6180f60612981f5496a8
czbar <czbar@users.noreply.github.com>
2022-11-24 18:55:24 +1100

    Create WebAccessOpeniningStatsState.cs

---
+++ ChessForge/WebAccessOpeniningStatsState.cs
@@ -0,0 +1,36 @@
+﻿using GameTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessForge
+{
+    /// <summary>
+    /// Holds state of OpeningStats query
+    /// </summary>
+    public class WebAccessOpeniningStatsState
+    {
+        /// <summary>
+        /// A node that had to be queued while another request
+        /// was in porogress
+        /// </summary>
+        public static TreeNode QueuedNode = null;
+
+        /// <summary>
+        /// Id of the Tree to which the queued node belongs.
+        /// </summary>
+        public static int QueuedNodeTreeId = 0;
+
+        /// <summary>
+        /// Whether there is an OpeningStats query in progress,
+        /// </summary>
+        public static bool IsOpeningStatsRequestInProgress = false;
+
+        /// <summary>
+        /// Whether the OpeningStats handlers have been initialized
+        /// </summary>
+        public static bool IsOpeningStatsInitialized = false;
+    }
+}
