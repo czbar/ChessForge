1c2ae76639d2836d1b445df3b1f82ee3b4b48830
czbar <czbar@users.noreply.github.com>
2022-12-09 14:22:10 +1100

    Implemented Game Cache

--- WebAccess/GameDownload.cs
+++ WebAccess/GameDownload.cs
@@ -20,6 +20,21 @@ namespace WebAccess
     public class GameDownload
     {
         /// <summary>
+        /// Max number of games to keep in cache.
+        /// </summary>
+        private static int GAME_CACHE_SIZE = 100;
+
+        /// <summary>
+        /// Ids of the currently cached games
+        /// </summary>
+        private static Queue<string> _queueGameIds = new Queue<string>();
+
+        /// <summary>
+        /// Cached games.
+        /// </summary>
+        private static Dictionary<string, string> _dictCachedGames = new Dictionary<string, string>();
+
+        /// <summary>
         /// Handler for the DataReceived event
         /// </summary>
         public static event EventHandler<WebAccessEventArgs> GameReceived;
@@ -30,6 +45,15 @@ namespace WebAccess
         public static string GameText;
 
         /// <summary>
+        /// Clears the cache queue and dictionary
+        /// </summary>
+        public void CleareGameCache()
+        {
+            _queueGameIds.Clear();
+            _dictCachedGames.Clear();
+        }
+
+        /// <summary>
         /// Gets a game from lichess.org.
         /// </summary>
         /// <param name="gameId"></param>
@@ -38,14 +62,22 @@ namespace WebAccess
             WebAccessEventArgs eventArgs = new WebAccessEventArgs();
             try
             {
-                HttpClient client = new HttpClient();
-                var response = await RestApiRequest.Client.GetAsync("https://lichess.org/game/export/" + gameId);
-                using (var fs = new MemoryStream())
+                if (_dictCachedGames.ContainsKey(gameId))
                 {
-                    await response.Content.CopyToAsync(fs);
-                    fs.Position = 0;
-                    StreamReader sr = new StreamReader(fs);
-                    GameText = sr.ReadToEnd();
+                    GameText = _dictCachedGames[gameId];
+                }
+                else
+                {
+                    HttpClient client = new HttpClient();
+                    var response = await RestApiRequest.Client.GetAsync("https://lichess.org/game/export/" + gameId);
+                    using (var fs = new MemoryStream())
+                    {
+                        await response.Content.CopyToAsync(fs);
+                        fs.Position = 0;
+                        StreamReader sr = new StreamReader(fs);
+                        GameText = sr.ReadToEnd();
+                        AddGameToCache(gameId, GameText);
+                    }
                 }
                 eventArgs.Success = true;
                 GameReceived?.Invoke(null, eventArgs);
@@ -54,9 +86,32 @@ namespace WebAccess
             catch (Exception ex)
             {
                 eventArgs.Success = true;
-                eventArgs.Message= ex.Message;
+                eventArgs.Message = ex.Message;
                 GameReceived?.Invoke(null, eventArgs);
                 return "";
+            }
+        }
+
+        /// <summary>
+        /// Add game to cache.
+        /// Delete the oldest game if the cache is full.
+        /// </summary>
+        /// <param name="gameId"></param>
+        /// <param name="gameText"></param>
+        private static void AddGameToCache(string gameId, string gameText)
+        {
+            try
+            {
+                if (_queueGameIds.Count >= GAME_CACHE_SIZE)
+                {
+                    string dequeuedGameId = _queueGameIds.Dequeue();
+                    _dictCachedGames.Remove(dequeuedGameId);
+                }
+                _queueGameIds.Enqueue(gameId);
+                _dictCachedGames[gameId] = gameText;
+            }
+            catch
+            {
             }
         }
 
