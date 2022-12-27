83af0a828492a3b8ad3e3bd2ca7877e55f906514
czbar <czbar@users.noreply.github.com>
2022-12-27 19:12:04 +1100

    Implemented TreeManager

---
+++ ChessPosition/GameTree/TreeManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessForge
+{
+    /// <summary>
+    /// Manages Variation Trees within a Workbook.
+    /// </summary>
+    public class TreeManager
+    {
+        private static int _maxTreeId = 0;
+
+        public static int GetNewTreeId()
+        {
+            _maxTreeId++;
+            return _maxTreeId;
+        }
+
+        public static void Reset()
+        {
+            _maxTreeId = 0;
+        }
+    }
+}
