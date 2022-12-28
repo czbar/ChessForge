353546de08300544cf0a4f281544e78853402db5
czbar <czbar@users.noreply.github.com>
2022-12-28 19:04:16 +1100

    Update EditOperation.cs

--- ChessPosition/GameTree/EditOperation.cs
+++ ChessPosition/GameTree/EditOperation.cs
@@ -21,6 +21,27 @@ namespace GameTree
         }
 
         /// <summary>
+        /// List of nodes to operate on.
+        /// </summary>
+        public List<TreeNode> NodeList { get { return _nodeList; } }
+
+        /// <summary>
+        /// The TreeNode operated no.
+        /// </summary>
+        public TreeNode Node { get { return _node; } }
+
+        /// <summary>
+        /// Child index in its parent's children list that will be required e.g. when promoting
+        /// a line it will be the original index from which the line was promoted.
+        /// </summary>
+        public int ChildIndex { get { return _childIndex; } }
+
+        /// <summary>
+        /// Operation type.
+        /// </summary>
+        public EditType OpType { get { return _opType; } }
+
+        /// <summary>
         /// Type of this operation.
         /// </summary>
         private EditType _opType;
@@ -31,29 +52,22 @@ namespace GameTree
         private List<TreeNode> _nodeList;
 
         /// <summary>
-        /// The node oparated on
+        /// The node operated on
         /// </summary>
         private TreeNode _node;
 
-        /// <summary>
-        /// Child index will be required e.g. when promoting
-        /// a line it will be the original index from which the line was promoted.
-        /// </summary>
+        // Child index 
         private int _childIndex;
-
-        /// <summary>
-        /// Operation type.
-        /// </summary>
-        public EditType OpType{ get { return _opType; } }
 
         /// <summary>
         /// Constructor for DELETE_LINE.
         /// </summary>
-        public EditOperation(EditType tp, TreeNode deletionRoot, List<TreeNode> deletedNodes) : base()
+        public EditOperation(EditType tp, TreeNode deletionRoot, List<TreeNode> deletedNodes, int childIndex) : base()
         {
             _opType = tp;
             _node = deletionRoot;
             _nodeList = deletedNodes;
+            _childIndex = childIndex;
         }
 
         /// <summary>
