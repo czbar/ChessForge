6c153897aa077c6415f3878644a7aa4a6d6e4a1a
czbar <czbar@users.noreply.github.com>
2022-12-30 19:24:20 +1100

    Update Operation.cs

--- ChessPosition/GameTree/Operation.cs
+++ ChessPosition/GameTree/Operation.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using static GameTree.EditOperation;
 
 namespace GameTree
 {
@@ -20,6 +22,16 @@ namespace GameTree
         /// This operation's creation time.
         /// </summary>
         public long Timestamp {get { return _timestamp; } }
+
+        /// <summary>
+        /// Returns Operation's data.
+        /// </summary>
+        public object OpData_1 { get { return _opData_1; } }
+
+        /// <summary>
+        /// Returns Operation's data.
+        /// </summary>
+        public object OpData_2 { get { return _opData_2; } }
 
         /// <summary>
         /// Operation's data
