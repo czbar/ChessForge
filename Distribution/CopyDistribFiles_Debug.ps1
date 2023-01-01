d83c2ceb38dd3819738d8fd851d055c0d713ec55
czbar <czbar@users.noreply.github.com>
2023-01-01 17:58:20 +1100

    Create CopyDistribFiles_Debug.ps1

---
+++ Distribution/CopyDistribFiles_Debug.ps1
@@ -0,0 +1,50 @@
+# Remove previous release files (supress errors)
+Remove-Item -Recurse -Force .\Temp 2>$null
+Remove-Item -Recurse -Force .\ReleaseDebug 2>$null
+
+# Create required directory structure
+New-Item -Type dir .\Temp\Resources\Sounds
+New-Item -Type dir .\ReleaseDebug
+
+# Copy all release items from the list fo files
+foreach($line in Get-Content ".\List of Files.txt") 
+{
+	if (![string]::IsNullOrEmpty($line))
+	{
+		write $line
+		Copy-Item ..\ChessForge\bin\Debug\$line -Destination .\Temp\$line
+	}
+}
+
+# Copy all distribution Workbooks
+Copy-Item ..\Workbooks\Distribution\* -Destination .\Temp
+
+# Copy readme's
+Copy-Item ..\LICENSE.MD -Destination .\Temp
+Copy-Item ..\README.md -Destination .\Temp
+
+Set-Location .\Temp 
+# Create the FULL zip file
+Compress-Archive * -DestinationPath ..\ReleaseDebug\ChessForgeFullDebug.zip
+
+Set-Location .. 
+
+# Delete files meant for full release only
+foreach($line in Get-Content ".\Files in Full Zip Only.txt") 
+{
+	if (![string]::IsNullOrEmpty($line))
+	{
+		write "removing: " + $line
+		Remove-Item .\Temp\$line
+	}
+}
+
+Set-Location .\Temp 
+
+# Create the zip file without Srockfish
+Compress-Archive * -DestinationPath ..\ReleaseDebug\ChessForgeDebug.zip
+
+Set-Location .. 
+
+# Clear Temp directory
+Remove-Item -Recurse -Force .\Temp
