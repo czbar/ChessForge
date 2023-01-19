f7fdb5644de2bfc0f8659dfedadfcd6a8d889a07
czbar <czbar@users.noreply.github.com>
2023-01-19 22:24:31 +1100

    Implemented FullNodeId

---
+++ ChessPosition/GameTree/FullNodeId.cs
@@ -0,0 +1,19 @@
+﻿
+namespace GameTree
+{
+    /// <summary>
+    /// Identifies a Node by its Id 
+    /// and holding Tree's Id.
+    /// </summary>
+    public class FullNodeId
+    {
+        public int TreeId;
+        public int NodeId;
+
+        public FullNodeId(int treeId, int nodeId)
+        {
+            TreeId = treeId;
+            NodeId = nodeId;
+        }
+    }
+}
