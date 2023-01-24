9110920cb555d8c8e4931a682c14e0e71076cfdf
czbar <czbar@users.noreply.github.com>
2023-01-24 15:54:36 +1100

    Allow showing engine lines in Training Game

--- ChessForge/EngineLinesBox.cs
+++ ChessForge/EngineLinesBox.cs
@@ -66,7 +66,7 @@ namespace ChessForge
         {
             try
             {
-                if (EvaluationManager.CurrentMode != EvaluationManager.Mode.ENGINE_GAME && EngineMessageProcessor.LastMessageNode != null)
+                if (AppStateManager.ShowEvaluationLines() && EngineMessageProcessor.LastMessageNode != null)
                 {
                     Lines.Clear();
                     lock (EngineMessageProcessor.MoveCandidatesLock)
