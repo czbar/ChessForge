33a9975cd4ed108a831d56bf56d993396c10eed6
czbar <czbar@users.noreply.github.com>
2023-02-10 17:43:01 +1100

    Update Program.cs

--- StockfishTest/Program.cs
+++ StockfishTest/Program.cs
@@ -10,43 +10,90 @@ using EngineService;
 
 namespace StockfishTest
 {
+    /// <summary>
+    /// Use this to test engine behavior under different circumstances. 
+    /// </summary>
     class Program
     {
-        private static StringBuilder txtMessageFile = new StringBuilder();
-
         private static EngineService.EngineProcess engine;
-        private static MoveEval[] mev = new MoveEval[5];
         private static bool IsEngineReady = false;
-
         private static bool IsMultiPvMode = true;
-        private static bool AnalysisInProgress = false;
 
-        private static StringBuilder sbAllText = new StringBuilder();
-
-        private static string BestMove;
-
+        /// <summary>
+        /// Entry point.
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
+            Console.WriteLine("Starting engine...");
             engine = new EngineService.EngineProcess();
             engine.EngineMessage += EngineMessage;
 
+            // start the engine
             StartStockfishEngine(engine);
 
+            Console.WriteLine("Engine Ready");
+
+            // Send a few requests one after another and check the responses in the log files
             AnalyzePosition("5rk1/2p1bppp/p1p1pn2/PQ6/3P4/6P1/NP2PP1P/2BR2K1 b k - 0 18");
+            Console.WriteLine("StartsSleep");
+            Thread.Sleep(500);
+            Console.WriteLine("EndSleep");
 
-            //EvaluatePgnGame();
+            AnalyzePosition("rnbq1rk1/pp2bppp/5n2/2pp2B1/3P4/3B1Q1P/PPP2PP1/RN2K1NR w KQ c6 0 7");
+            Console.WriteLine("StartsSleep");
+            Thread.Sleep(500);
+            Console.WriteLine("EndSleep");
 
-            EngineLog.Dump(null);
+            AnalyzePosition("rnbq1rk1/pp2bppp/5n2/2Pp2B1/8/3B1Q1P/PPP2PP1/RN2K1NR b KQ - 0 8");
+            Console.WriteLine("StartsSleep");
+            Thread.Sleep(500);
+            Console.WriteLine("EndSleep");
 
-            //Console.WriteLine(sbAllText.ToString());
+            AnalyzePosition("5rk1/2p1bppp/p1p1pn2/PQ6/3P4/6P1/NP2PP1P/2BR2K1 b k - 0 18");
+            Console.WriteLine("StartsSleep");
+            Thread.Sleep(500);
+            Console.WriteLine("EndSleep");
 
-            //File.WriteAllText("EngineMessages.txt", txtMessageFile.ToString());
+            AnalyzePosition("r1bq1rk1/pp1nbppp/5n2/2Pp2B1/8/3B1Q1P/PPP2PP1/RN2K1NR w KQ - 1 8");
+            Console.WriteLine("StartsSleep");
+            Thread.Sleep(500);
+            Console.WriteLine("EndSleep");
 
+            AnalyzePosition("5rk1/2p1bppp/p1p1pn2/PQ6/3P4/6P1/NP2PP1P/2BR2K1 b k - 0 18");
+            Console.WriteLine("StartsSleep");
+            Thread.Sleep(500);
+
+            Console.WriteLine("Final Sleep");
+            Thread.Sleep(1000);
+
+            EngineLog.Dump("enginelog.txt");
+
+            engine.StopEngine();
+
+            Console.WriteLine("Finished");
+
+            Environment.Exit(0);
         }
 
+        /// <summary>
+        /// Sends FEN for analysis
+        /// </summary>
+        /// <param name="fen"></param>
+        private static void AnalyzePosition(string fen)
+        {
+            engine.DebugSendCommand("stop");
+            engine.SendCommand("position fen " + fen);
+            engine.SendCommand("go");
+        }
+
+        /// <summary>
+        /// Starts the engine and waits for the reaydok message
+        /// </summary>
+        /// <param name="engine"></param>
         private static void StartStockfishEngine(EngineService.EngineProcess engine)
         {
-            engine.StartEngine("stockfish_15_x64_avx2.exe");
+            engine.StartEngine("stockfish_15_x86_64.exe");
 
             // wait for "readyok"
             while (true)
@@ -66,121 +113,12 @@ namespace StockfishTest
             }
         }
 
-        private static void EvaluateTreeLine(TreeNode nd, bool includeNumber = false)
-        {
-            while (true)
-            {
-                // if the node has no children,
-                // print it and return 
-                if (nd.Children.Count == 0)
-                {
-                    return;
-                }
-
-                // if the node has 1 child, print it,
-                // call this method on the child
-                if (nd.Children.Count == 1)
-                {
-                    EvaluateNode(nd.Children[0], includeNumber);
-                    EvaluateTreeLine(nd.Children[0]);
-                    return;
-                }
-
-                // if the node has more than 1 child
-                // call this method on each sibling except
-                // the first one, before calling it on the 
-                // first one.
-                if (nd.Children.Count > 1)
-                {
-                    EvaluateNode(nd.Children[0], includeNumber);
-                    for (int i = 1; i < nd.Children.Count; i++)
-                    {
-                        sbAllText.Append(" (");
-                        EvaluateNode(nd.Children[i], true);
-                        EvaluateTreeLine(nd.Children[i]);
-                        sbAllText.Append(") ");
-                    }
-                    EvaluateTreeLine(nd.Children[0], true);
-                    return;
-                }
-            }
-        }
-        private static void EvaluateNode(TreeNode nd, bool includeNumber)
-        {
-            if (nd.Position.ColorToMove == PieceColor.Black)
-            {
-                if (!includeNumber && nd.Position.MoveNumber != 1)
-                {
-                    sbAllText.Append(" ");
-                }
-                sbAllText.Append(nd.Position.MoveNumber.ToString() + ".");
-            }
-
-            if (nd.Position.ColorToMove == PieceColor.White && includeNumber)
-            {
-                sbAllText.Append(nd.Position.MoveNumber.ToString() + "...");
-            }
-
-            sbAllText.Append(" " + nd.LastMoveAlgebraicNotation);
-
-            AnalyzePosition(FenParser.GenerateFenFromPosition(nd.Position));
-        }
-
-        private static void AnalyzePosition(string fen)
-        {
-            AnalysisInProgress = true;
-            //fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
-            txtMessageFile.Append(Environment.NewLine);
-            txtMessageFile.Append("FEN: " + fen + Environment.NewLine);
-            engine.SendCommand("position fen " + fen);
-            engine.SendCommand("go movetime 1000");
-
-            Console.WriteLine("Position: " + fen);
-
-            while (AnalysisInProgress)
-            {
-                Thread.Sleep(10);
-            }
-
-            sbAllText.Append(" [eval: " + mev[0].CpEval.ToString() + " best move: " + mev[0].Move + "]");
-
-            //Console.WriteLine(" [eval: " + mev[0].CpEval.ToString() + " best move: " + mev[0].Move + "]");
-            //Console.WriteLine("");
-
- //           engine.Kill();
- //           engine.StartEngine();
-
-        }
-
-        private static void CheckEngineReady()
-        {
-            IsEngineReady = false;
-            engine.SendCommand("isready");
-            while (true)
-            {
-                if (IsEngineReady)
-                    break;
-
-                Thread.Sleep(100);
-            }
-        }
-
         /// <summary>
-        /// Process messages giving us the scores
+        /// Sets IsEngineReady if the message is "readyok"
         /// </summary>
         /// <param name="message"></param>
         private static void EngineMessage(string message)
         {
-            int pvIndex = 0;
-            int cpScore = 0;
-            string move = "";
-
-            if (message != null)
-            {
-                txtMessageFile.Append(Environment.NewLine);
-                txtMessageFile.Append("Engine: " + message + Environment.NewLine);
-            }
-
             if (!IsEngineReady)
             {
                 if (message.Contains("readyok"))
@@ -188,88 +126,7 @@ namespace StockfishTest
                     IsEngineReady = true;
                 }
             }
-
-            if (message.Contains("score")) // Message is in the form: "info depth 1 seldepth 1 multipv 1 score cp 127 nodes 169 nps 84500 tbhits 0 time 2 pv d1d4"
-            {
-                string[] tokens = message.Split(' ');
-                if (IsMultiPvMode)
-                {
-                    for (int i = 0; i < tokens.Length; i++)
-                    {
-                        if (tokens[i] == "multipv")
-                        {
-                            pvIndex = int.Parse(tokens[i + 1]);
-                        }
-                        else if (tokens[i] == "score")
-                        {
-                            cpScore = int.Parse(tokens[i + 2]);
-                        }
-                        else if (tokens[i] == "pv")
-                        {
-                            move = tokens[i + 1];
-                            mev[pvIndex - 1].Move = move;
-                            mev[pvIndex - 1].CpEval = cpScore;
-                        }
-                    }
-                }
-                else
-                {
-                    for (int i = 0; i < tokens.Length; i++)
-                    {
-                        if (tokens[i] == "score")
-                        {
-                            // the next token MUST be cp
-                            // TODO handle other tokens esp. "mate"!
-                            if (tokens[i + 1] == "cp")
-                            {
-                                cpScore = int.Parse(tokens[i + 2]);
-                            }
-                        }
-                        else if (tokens[i] == "pv")
-                        {
-                            move = tokens[i + 1];
-                            mev[0].Move = move;
-                            mev[0].CpEval = -1 * cpScore;
-                        }
-                    }
-                }
-            }
-            else if (message.Contains("bestmove"))
-            {
-                // we are done
-                string[] tokens = message.Split(' ');
-                BestMove = tokens[1];
-
-                if (BestMove != mev[0].Move)
-                {
-//                    throw new Exception("best move != pv move");
-                }
-
-                AnalysisInProgress = false;
-            }
         }
-
-        static public void SendMovesToEngine(string moves, short time)
-        {
-            var command = UciCommands.ENG_POSITION_STARTPOS + " " + moves;
-            engine.SendCommand(command);
-            command = UciCommands.ENG_GO_MOVE_TIME + " " + time.ToString();
-            engine.SendCommand(command);
-        }
-
-
-    }
-
-    public struct MoveEval
-    {
-        //public MoveEval(string move, int cpEval)
-        //{
-        //    Move = move;
-        //    CpEval = cpEval;
-        //}
-
-        public string Move;
-        public int CpEval;
     }
 
 }
