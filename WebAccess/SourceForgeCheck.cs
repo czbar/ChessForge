20cc9221a18c3769f43c550df762bebb79370247
czbar <czbar@users.noreply.github.com>
2023-02-23 13:34:20 +1100

    Use RegEx

--- WebAccess/SourceForgeCheck.cs
+++ WebAccess/SourceForgeCheck.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Newtonsoft.Json;
 using ChessPosition;
 using System.Linq.Expressions;
+using System.Text.RegularExpressions;
 
 namespace WebAccess
 {
@@ -14,7 +15,7 @@ namespace WebAccess
         /// Version of ChessForge's download package on SourceForge.
         /// </summary>
         public static Version ChessForgeVersion = null;
-        
+
         /// <summary>
         /// Gets the version number of Chess Forge currently available from Source Forge.
         /// </summary>
@@ -29,11 +30,17 @@ namespace WebAccess
                     var json = await client.GetStringAsync("https://sourceforge.net/projects/ChessForge/best_release.json");
 
                     dynamic obj = JsonConvert.DeserializeObject<dynamic>(json);
-                    string sVer = obj.platform_releases.windows.filename;
+                    string latestFileName = obj.platform_releases.windows.filename;
 
-                    bool result = TextUtils.GetVersionNumbers(sVer, out int major, out int minor, out int patch);
-                    ChessForgeVersion = new Version(major, minor, patch);
-
+                    string versionString = ExtractVersionString(latestFileName);
+                    if (!string.IsNullOrEmpty(versionString))
+                    {
+                        bool result = TextUtils.GetVersionNumbers(versionString, out int major, out int minor, out int patch);
+                        if (result)
+                        {
+                            ChessForgeVersion = new Version(major, minor, patch);
+                        }
+                    }
                     return json;
                 }
             }
@@ -41,6 +48,26 @@ namespace WebAccess
             {
                 return "";
             }
+        }
+
+        /// <summary>
+        /// Extracts the version number in the form of n.n.n from the passed string.
+        /// </summary>
+        /// <param name="inputString"></param>
+        /// <returns></returns>
+        private static string ExtractVersionString(string inputString)
+        {
+            string pattern = @"\b\d+\.\d+\.\d+\b";
+
+            Match match = Regex.Match(inputString, pattern);
+
+            string substring = "";
+            if (match.Success)
+            {
+                substring = match.Value;
+            }
+
+            return substring;
         }
     }
 }
