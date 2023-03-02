d59a40aac242aaed9672214294763664ed890c40
czbar <czbar@users.noreply.github.com>
2023-03-02 23:07:00 +1100

    Implement split of Comments into Parts

---
+++ ChessPosition/Utils/CommentPart.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessPosition.Utils
+{
+    /// <summary>
+    /// Types of comment parts
+    /// </summary>
+    public enum CommentPartType
+    {
+        TEXT,
+        URL,
+        THUMBNAIL_SYMBOL,
+        QUIZ_POINTS,
+    }
+
+    /// <summary>
+    /// Represents a single part of a comment
+    /// </summary>
+    public class CommentPart
+    {
+        /// <summary>
+        /// Type of the part.
+        /// </summary>
+        public CommentPartType Type;
+
+        /// <summary>
+        /// Text of the part.
+        /// </summary>
+        public string Text;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="text"></param>
+        public CommentPart(CommentPartType type, string text)
+        {
+            Type = type;
+            Text = text;
+        }
+    }
+}
