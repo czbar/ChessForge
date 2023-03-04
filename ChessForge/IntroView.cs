dee138cb9510ea32c39f23c2e96be43ef95c1f41
czbar <czbar@users.noreply.github.com>
2023-03-04 21:00:02 +1100

    Update IntroView.cs

--- ChessForge/IntroView.cs
+++ ChessForge/IntroView.cs
@@ -7,6 +7,7 @@ using System.Windows.Documents;
 using System.Windows.Markup;
 using System.Windows;
 using ChessPosition;
+using System.Windows.Controls;
 
 namespace ChessForge
 {
@@ -15,12 +16,34 @@ namespace ChessForge
     /// </summary>
     public class IntroView
     {
+        // flag to use to prevent unnecessary saving after the load.
+        private bool _ignoreTextChange = false;
+
         /// <summary>
-        /// Constructor
+        /// Constructor. Builds the content if not empty.
         /// </summary>
         public IntroView(Chapter parentChapter)
         {
+            AppState.MainWin.UiRtbIntroView.Document.Blocks.Clear();
             ParentChapter = parentChapter;
+
+            // set the event handler we loaded the document.
+            AppState.MainWin.UiRtbIntroView.TextChanged += UiRtbIntroView_TextChanged;
+            if (!string.IsNullOrEmpty(Intro.Tree.RootNode.Data))
+            {
+                Intro.CodedContent = Intro.Tree.RootNode.Data;
+                _ignoreTextChange = true;
+                LoadXAMLContent();
+            }
+        }
+
+        /// <summary>
+        /// Clear the content of the RTB
+        /// </summary>
+        public void Clear()
+        {
+            _ignoreTextChange = true;
+            AppState.MainWin.UiRtbIntroView.Document.Blocks.Clear();
         }
 
         /// <summary>
@@ -41,8 +64,28 @@ namespace ChessForge
         /// </summary>
         public void LoadXAMLContent()
         {
-            string xaml = EncodingUtils.Base64Decode(Intro.CodedContent);
-            AppState.MainWin.UiRtbIntroView.Document = StringToFlowDocument(xaml);
+            if (!string.IsNullOrEmpty(Intro.CodedContent))
+            {
+                string xaml = EncodingUtils.Base64Decode(Intro.CodedContent);
+                AppState.MainWin.UiRtbIntroView.Document = StringToFlowDocument(xaml);
+            }
+        }
+
+        /// <summary>
+        /// Handles the Text Change event. Sets the Workbook's dirty flag.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void UiRtbIntroView_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (_ignoreTextChange)
+            {
+                _ignoreTextChange = false;
+            }
+            else
+            {
+                AppState.IsDirty = true;
+            }
         }
 
         /// <summary>
