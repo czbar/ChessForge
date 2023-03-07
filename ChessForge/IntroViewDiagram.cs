2ebae4c3ce70e0b214d98e6f6094b23d7787b508
czbar <czbar@users.noreply.github.com>
2023-03-07 20:36:31 +1100

    Update IntroViewDiagram.cs

--- ChessForge/IntroViewDiagram.cs
+++ ChessForge/IntroViewDiagram.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GameTree;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,9 +7,24 @@ using System.Threading.Tasks;
 
 namespace ChessForge
 {
+    /// <summary>
+    /// Encapsulates info about a diagram within the Intro view.
+    /// </summary>
     public class IntroViewDiagram
     {
+        /// <summary>
+        /// The Chessboard object.
+        /// </summary>
         public ChessBoardSmall Chessboard;
+
+        /// <summary>
+        /// Name of the paragraph the diagram is in.
+        /// </summary>
         public string ParagraphName;
+
+        /// <summary>
+        /// The position represented by the diagram.
+        /// </summary>
+        public TreeNode Node;
     }
 }
