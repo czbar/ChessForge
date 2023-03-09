649ef3e3bad964c72a03178200dcd43cb6aeaec4
czbar <czbar@users.noreply.github.com>
2023-03-09 14:06:20 +1100

    Added XAML keys

--- ChessPosition/GameTree/ChfCommands.cs
+++ ChessPosition/GameTree/ChfCommands.cs
@@ -13,6 +13,13 @@ namespace GameTree
     public class ChfCommands
     {
         /// <summary>
+        /// Strings to use when encoding the XAML view.
+        /// </summary>
+        public const string XAML_NODE_ID = "node_id";
+        public const string XAML_MOVE_TEXT = "text";
+        public const string XAML_FEN = "fen";
+
+        /// <summary>
         /// Command IDs
         /// </summary>
         public enum Command
