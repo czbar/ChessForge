6bc618a7033c04180d9c4b25c3889ba4b168f31f
czbar <czbar@users.noreply.github.com>
2023-04-07 20:42:15 +1000

    Put IntroViewClipboardElement in a separate file

--- ChessForge/IntroViewClipboard.cs
+++ ChessForge/IntroViewClipboard.cs
@@ -105,39 +105,4 @@ namespace ChessForge
         }
     }
 
-    /// <summary>
-    /// A class for elements to store in the IntroViewClipboard
-    /// </summary>
-    public class IntroViewClipboardElement
-    {
-        /// <summary>
-        /// Constructs an element of a specified type.
-        /// </summary>
-        /// <param name="type"></param>
-        public IntroViewClipboardElement(IntroViewClipboard.ElementType type)
-        {
-            Type = type;
-        }
-
-        /// <summary>
-        /// Type of the element.
-        /// </summary>
-        public IntroViewClipboard.ElementType Type = IntroViewClipboard.ElementType.None;
-
-        /// <summary>
-        /// Node id that will be used by some elements.
-        /// </summary>
-        public int NodeId = -1;
-
-        /// <summary>
-        /// An object with data of the type appropriate for the type of the element.
-        /// </summary>
-        public object DataObject = null;
-
-        /// <summary>
-        /// A boolean value to use for the elements that require it.
-        /// </summary>
-        public bool? BoolState;
-    }
-
 }
