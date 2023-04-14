cad3aefc750429d84092f2e31bea3b70ab9e57ca
czbar <czbar@users.noreply.github.com>
2023-04-15 00:44:00 +1000

    Continued implementation.

--- ChessForge/DownloadWebGamesDialog.xaml.cs
+++ ChessForge/DownloadWebGamesDialog.xaml.cs
@@ -1,18 +1,14 @@
-﻿using GameTree;
+﻿using ChessPosition;
+using GameTree;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 using WebAccess;
 
 namespace ChessForge
@@ -30,6 +26,19 @@ namespace ChessForge
             InitializeComponent();
             GameDownload.UserGamesReceived += UserGamesReceived;
             EnableControls(false);
+
+            SetControlValues();
+
+            UiCmbSite.Items.Add(Constants.LichessNameId);
+            UiCmbSite.Items.Add(Constants.ChesscomNameId);
+
+            UiCmbSite.SelectedItem = Configuration.WebGamesSite;
+            if (UiCmbSite.SelectedItem == null)
+            {
+                UiCmbSite.SelectedItem = Constants.LichessNameId;
+            }
+
+            SetUserName();
         }
 
         /// <summary>
@@ -68,6 +77,35 @@ namespace ChessForge
         }
 
         /// <summary>
+        /// Set user name per currently selected web site.
+        /// </summary>
+        private void SetUserName()
+        {
+            if ((string)UiCmbSite.SelectedItem == Constants.ChesscomNameId)
+            {
+                UiTbUserName.Text = Configuration.WebGamesChesscomUser;
+            }
+            else
+            {
+                UiTbUserName.Text = Configuration.WebGamesLichessUser;
+            }
+        }
+
+        /// <summary>
+        /// Set values in the controls per configuration items.
+        /// </summary>
+        private void SetControlValues()
+        {
+            UiTbMaxGames.Text = Math.Max(Configuration.WebGamesMaxCount, 1).ToString();
+
+            EnableDateControls(!Configuration.WebGamesMostRecent);
+            UiCbOnlyNew.IsChecked = Configuration.WebGamesMostRecent;
+
+            UiDtStartDate.SelectedDate = Configuration.WebGamesStartDate;
+            UiDtEndDate.SelectedDate = Configuration.WebGamesEndDate;
+        }
+
+        /// <summary>
         /// Invokes the dialog to select games for import.
         /// </summary>
         /// <param name="games"></param>
@@ -79,7 +117,7 @@ namespace ChessForge
                 games[i].OrderNo = (i + 1).ToString();
             }
 
-            SelectGamesDialog dlg = new SelectGamesDialog(ref games, SelectGamesDialog.Mode.DOWNLOAD_WEB_GAMES )
+            SelectGamesDialog dlg = new SelectGamesDialog(ref games, SelectGamesDialog.Mode.DOWNLOAD_WEB_GAMES)
             {
                 Left = AppState.MainWin.ChessForgeMain.Left + 100,
                 Top = AppState.MainWin.ChessForgeMain.Top + 100,
@@ -99,11 +137,11 @@ namespace ChessForge
 
             UiBtnDownload.IsEnabled = !isDownloading;
             UiCbOnlyNew.IsEnabled = !isDownloading;
-            UiCbUseStartDate.IsEnabled = !isDownloading;
             UiCmbSite.IsEnabled = !isDownloading;
             UiTbMaxGames.IsEnabled = !isDownloading;
             UiTbUserName.IsEnabled = !isDownloading;
             UiDtStartDate.IsEnabled = !isDownloading;
+            UiDtEndDate.IsEnabled = !isDownloading;
         }
 
         /// <summary>
@@ -120,6 +158,8 @@ namespace ChessForge
             }
             else
             {
+                UpdateConfiguration();
+
                 EnableControls(true);
                 GamesFilter filter = new GamesFilter();
                 filter.User = UiTbUserName.Text;
@@ -136,5 +176,80 @@ namespace ChessForge
             }
         }
 
+        /// <summary>
+        /// Update WebGames configuaration items.
+        /// </summary>
+        private void UpdateConfiguration()
+        {
+            string site = (string)UiCmbSite.SelectedItem;
+            Configuration.WebGamesSite = site;
+            if (site == Constants.LichessNameId)
+            {
+                Configuration.WebGamesLichessUser = UiTbUserName.Text;
+            }
+            else if (site == Constants.ChesscomNameId)
+            {
+                Configuration.WebGamesChesscomUser = UiTbUserName.Text;
+            }
+
+            int.TryParse(UiTbMaxGames.Text, out Configuration.WebGamesMaxCount);
+
+            Configuration.WebGamesMostRecent = UiCbOnlyNew.IsChecked == true;
+            Configuration.WebGamesStartDate = UiDtStartDate.SelectedDate;
+            Configuration.WebGamesEndDate = UiDtEndDate.SelectedDate;
+        }
+
+        /// <summary>
+        /// Enable/disable date controls.
+        /// </summary>
+        /// <param name="enable"></param>
+        private void EnableDateControls(bool enable)
+        {
+            UiDtStartDate.IsEnabled = enable;
+            UiDtEndDate.IsEnabled = enable;
+        }
+
+        /// <summary>
+        /// Checkbox for "Recent Games only" chnaged.
+        /// Disable date controls.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void UiCbOnlyNew_Checked(object sender, RoutedEventArgs e)
+        {
+            EnableDateControls(false);
+        }
+
+        /// <summary>
+        /// Checkbox for "Recent Games only" chnaged.
+        /// Enable date controls.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void UiCbOnlyNew_Unchecked(object sender, RoutedEventArgs e)
+        {
+            EnableDateControls(true);
+        }
+
+        /// <summary>
+        /// Web site selection changed so change the user name accordingly
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void UiCmbSite_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            SetUserName();
+        }
+
+        /// <summary>
+        /// Remove handler subscription.
+        /// Otherwise, the it will be called twice when the dialog is called again.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            GameDownload.UserGamesReceived -= UserGamesReceived;
+        }
     }
 }
