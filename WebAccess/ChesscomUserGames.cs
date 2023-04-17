9c6106000799ff2229b0338b3339f43574d41682
czbar <czbar@users.noreply.github.com>
2023-04-17 15:49:40 +1000

    temp update

--- WebAccess/ChesscomUserGames.cs
+++ WebAccess/ChesscomUserGames.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Diagnostics.Tracing;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
+using System.Security.Cryptography;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using ChessPosition;
+using GameTree;
 using static System.Net.Mime.MediaTypeNames;
 using static System.Net.WebRequestMethods;
 
@@ -34,32 +37,23 @@ namespace WebAccess
             WebAccessEventArgs eventArgs = new WebAccessEventArgs();
             try
             {
-                // Stage 1 get list of possible archives
+                // STAGE 1: get list of possible archives
                 List<string> urlArchivesList = null;
                 string urlArchivesCommand = GetArchiveListUrl(filter.User);
                 string text = await ExecuteHttpCall(urlArchivesCommand);
                 eventArgs.TextData = text.Replace(',', ' ');
                 urlArchivesList = TextUtils.MatchUrls(eventArgs.TextData);
 
-                if (filter.StartDate != null || filter.EndDate != null)
-                {
-                    urlArchivesList = TrimUrlList(filter, urlArchivesList);
-                }
+                List<uint> lstYearMonth = BuildYearMonthList(filter, urlArchivesList);
 
-                if (urlArchivesList.Count == 0)
+                if (lstYearMonth.Count == 0)
                 {
                     eventArgs.TextData = "";
                 }
                 else
                 {
-                    // start loading archive after archive and retrieving games until we get the required number
-                    // if start date is not null we start from there, otherwise, we go back from the latest archive.
-                    if (filter.StartDate.HasValue)
-                    {
-                    }
-                    else
-                    {
-                    }
+                    // STAGE 2: start loading archive after archive and retrieving games until we get the required number
+                    ReadGamesFromArchives(lstYearMonth, filter);
                 }
                 eventArgs.Success = true;
                 UserGamesReceived?.Invoke(null, eventArgs);
@@ -71,6 +65,37 @@ namespace WebAccess
                 eventArgs.Message = ex.Message;
                 UserGamesReceived?.Invoke(null, eventArgs);
                 return "";
+            }
+        }
+
+        private async static void ReadGamesFromArchives(List<uint> lstYearMonths, GamesFilter filter)
+        {
+            int totalGames = 0;
+
+            // if start date is not null we start from there, otherwise, we go back from the EndDate or latest.
+            if (!filter.StartDate.HasValue)
+            {
+                lstYearMonths.Reverse();
+            }
+
+            StringBuilder allGames = new StringBuilder();
+
+            foreach (uint yearMonth in lstYearMonths)
+            {
+                // get month and year
+                DecodeYearMonth(yearMonth, out uint year, out uint month);
+                string sYear = year.ToString("0000");
+                string sMonth = month.ToString("00");
+                string url = string.Format("https://api.chess.com/pub/player/{0}/games/{1}/{2}/pgn", filter.User, sYear, sMonth);
+                string text = await ExecuteHttpCall(url);
+                allGames.AppendLine(text);
+
+                ObservableCollection<GameData> games = new ObservableCollection<GameData>();
+                totalGames += PgnMultiGameParser.ParsePgnMultiGameText(text, ref games);
+                if (totalGames >= filter.MaxGames || totalGames == 0)
+                {
+                    break;
+                }
             }
         }
 
@@ -103,9 +128,9 @@ namespace WebAccess
         /// <param name="filter"></param>
         /// <param name="urlList"></param>
         /// <returns></returns>
-        private static List<string> TrimUrlList(GamesFilter filter, List<string> urlList)
+        private static List<uint> BuildYearMonthList(GamesFilter filter, List<string> urlList)
         {
-            List<string> trimmedList = new List<string>();
+            List<uint> trimmedList = new List<uint>();
 
             Regex urlRegex = new Regex(@"\/\d{4}\/\d{2}$", RegexOptions.Compiled);
             foreach (string url in urlList)
@@ -114,11 +139,11 @@ namespace WebAccess
                 // first 4 digits is year followed by slash and 2 digits for month
                 try
                 {
-                    int year = int.Parse(match.Value.Substring(1, 4));
-                    int month = int.Parse(match.Value.Substring(6, 2));
+                    uint year = uint.Parse(match.Value.Substring(1, 4));
+                    uint month = uint.Parse(match.Value.Substring(6, 2));
                     if (IsDateToYearMonthGood(true, filter.StartDate, year, month) && IsDateToYearMonthGood(false, filter.EndDate, year, month))
                     {
-                        trimmedList.Add(url);
+                        trimmedList.Add(EncodeYearMonth(year, month));
                     }
                 }
                 catch
@@ -126,6 +151,7 @@ namespace WebAccess
                 }
             }
 
+            trimmedList.Sort();
             return trimmedList;
         }
 
@@ -138,7 +164,7 @@ namespace WebAccess
         /// <param name="year"></param>
         /// <param name="month"></param>
         /// <returns></returns>
-        private static bool IsDateToYearMonthGood(bool startDate, DateTime? date, int year, int month)
+        private static bool IsDateToYearMonthGood(bool startDate, DateTime? date, uint year, uint month)
         {
             if (!date.HasValue)
             {
@@ -173,5 +199,17 @@ namespace WebAccess
             string url = string.Format("https://api.chess.com/pub/player/{0}/games/archives", user);
             return url;
         }
+
+        private static uint EncodeYearMonth(uint year, uint month)
+        {
+            return year << 4 | month;
+        }
+
+        private static void DecodeYearMonth(uint encoded, out uint year, out uint month)
+        {
+            month = encoded & 0x0F;
+            year = encoded >> 4;
+        }
+
     }
 }
