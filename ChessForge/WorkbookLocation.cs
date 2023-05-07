d0d559f028749844e139a979f62e12efaa222973
czbar <czbar@users.noreply.github.com>
2023-05-07 23:39:43 +0200

    Update WorkbookLocation.cs

--- ChessForge/WorkbookLocation.cs
+++ ChessForge/WorkbookLocation.cs
@@ -12,19 +12,57 @@ namespace ChessForge
     /// </summary>
     public class WorkbookLocation
     {
+        // Guid of the chapter
+        private string _chapterGuid;
+
+        // Type of the view
+        private WorkbookManager.TabViewType _viewType;
+
+        // Guid of the article where applicable
+        private string _articleGuid;
+
         /// <summary>
         /// Guid of the chapter
         /// </summary>
-        public string ChapterGuid;
-
-        /// <summary>
-        /// Type of the view
-        /// </summary>
-        public WorkbookManager.TabViewType ViewType;
+        public string ChapterGuid
+        {
+            get { return _chapterGuid; }
+        }
 
         /// <summary>
         /// Guid of the article where applicable
         /// </summary>
-        public string ArticleGuid;
+        public string ArticleGuid
+        {
+            get
+            {
+                return _articleGuid;
+            }
+        }
+
+        /// <summary>
+        /// Type of the view
+        /// </summary>
+        public WorkbookManager.TabViewType ViewType
+        {
+            get
+            {
+                return _viewType;
+            }
+        }
+
+        /// <summary>
+        /// Constructs the location object.
+        /// </summary>
+        /// <param name="chapterGuid"></param>
+        /// <param name="viewType"></param>
+        /// <param name="articleGuid"></param>
+        public WorkbookLocation(string chapterGuid, WorkbookManager.TabViewType viewType, string articleGuid)
+        {
+            _chapterGuid = chapterGuid;
+            _viewType = viewType;
+            _articleGuid = articleGuid;
+        }
+
     }
 }
