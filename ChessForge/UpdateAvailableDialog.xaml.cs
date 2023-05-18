f841f1c08c258945c744fe1bf307cf9decbd2ca0
czbar <czbar@users.noreply.github.com>
2023-05-18 16:45:24 +0200

    Handle 2 potential download sites

--- ChessForge/UpdateAvailableDialog.xaml.cs
+++ ChessForge/UpdateAvailableDialog.xaml.cs
@@ -19,25 +19,51 @@ namespace ChessForge
     /// </summary>
     public partial class UpdateAvailableDialog : Window
     {
-        Version _ver;
+        // version to report
+        private Version _ver;
 
-        public UpdateAvailableDialog(Version ver)
+        // source of the new version: 1 if MS, -1 if SourceForge
+        private int _updateSource;
+
+        /// <summary>
+        /// Constructor. Takes the new available version number
+        /// as the argument
+        /// </summary>
+        /// <param name="ver"></param>
+        public UpdateAvailableDialog(Version ver, int updSource)
         {
             _ver = ver;
+            _updateSource = updSource;
             InitializeComponent();
             string s = Properties.Resources.NewVersionAvailable;
             s = s.Replace("$0", ver.ToString());
             UiLblPreamble.Content = s;
+
+            if (updSource == -1)
+            {
+                UiTbDownloadLink.Text = Properties.Resources.SourceForgeSite;
+            }
+            else
+            {
+                UiTbDownloadLink.Text = Properties.Resources.MicrosoftAppStore;
+            }
         }
 
         /// <summary>
-        /// Points the host's browser to the downalod page.
+        /// Points the host's browser to the download page.
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
         private void UiLblDownloadLink_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            System.Diagnostics.Process.Start("https://sourceforge.net/projects/chessforge/");
+            if (_updateSource == -1)
+            {
+                System.Diagnostics.Process.Start("https://sourceforge.net/projects/chessforge/");
+            }
+            else
+            {
+                System.Diagnostics.Process.Start("https://apps.microsoft.com/store/detail/chess-forge/XPDC18VV71LM34");
+            }
         }
 
         /// <summary>
