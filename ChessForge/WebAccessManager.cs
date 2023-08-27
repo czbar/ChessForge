51f479f8f774e6962fa83a0c37c4586e45d04838
czbar <czbar@users.noreply.github.com>
2023-08-27 08:59:26 -0600

    Completed refactor

--- ChessForge/WebAccessManager.cs
+++ ChessForge/WebAccessManager.cs
@@ -21,6 +21,9 @@ namespace ChessForge
         // counts pulse events since last web access 
         private static int _webAccessCounter;
 
+        // lock to prevent clashing of UpdateWebProcessing and RequestWebAccess
+        private static object _lockWebUpdate = new object();
+
         /// <summary>
         /// This method is called by the client requesting Explorer data from a web site.
         /// It will be checked against the current state and executed, queued or discarded.
@@ -44,12 +47,27 @@ namespace ChessForge
         {
             _webAccessCounter++;
 
+            lock (_lockWebUpdate)
+            {
+                // if there a new queued request, and we are not in "mandatory delay", slot it in for immediate execution,
+                // we are no longer interested in the current one
+                if (WebAccessState.HasQueuedRequest && !WebAccessState.IsMandatoryDelayOn)
+                {
+                    WebAccessState.WaitingNode = WebAccessState.QueuedNode;
+                    WebAccessState.WaitingNodeTreeId = WebAccessState.QueuedNodeTreeId;
+                    WebAccessState.HasQueuedRequest = false;
+                    WebAccessState.HasWaitingRequest = true;
+                    WebAccessState.IsWaitingForResults = true;
+                }
+            }
+
+            // if we have a request in progress check for results
             if (WebAccessState.IsWaitingForResults)
             {
                 LichessOpeningsStats stats = CheckResults();
                 if (stats != null)
                 {
-                    AppLog.Message(2, "Received Web Data for :" + WebAccessState.WaitingNode.LastMoveAlgebraicNotation);
+                    AppLog.Message(2, "Received Web Data for: " + WebAccessState.WaitingNode.LastMoveAlgebraicNotation);
                     WebAccessState.IsWaitingForResults = false;
                     AppState.MainWin.Dispatcher.Invoke(() =>
                     {
@@ -59,18 +77,20 @@ namespace ChessForge
                 }
             }
 
+            // reset the "mandatory delay" period, if it is time to do so
             if (_webAccessCounter > MANDATORY_DELAY_PULSES_COUNT)
             {
                 WebAccessState.IsMandatoryDelayOn = false;
             }
 
+            // if we are not in the "mandatory delay", check if we have a request to execute
             if (!WebAccessState.IsMandatoryDelayOn)
             {
                 _webAccessCounter = 0;
                 if (WebAccessState.HasWaitingRequest)
                 {
                     WebAccessState.IsMandatoryDelayOn = true;
-                    AppLog.Message(2, "Execute Web Request for :" + WebAccessState.WaitingNode.LastMoveAlgebraicNotation);
+                    AppLog.Message(2, "Execute Web Request for: " + WebAccessState.WaitingNode.LastMoveAlgebraicNotation);
                     ExecuteRequest(WebAccessState.WaitingNodeTreeId, WebAccessState.WaitingNode);
                     WebAccessState.HasWaitingRequest = false;
                 }
@@ -78,12 +98,38 @@ namespace ChessForge
         }
 
         /// <summary>
-        /// Checks if the results are already in the cache.
+        /// Sets a queued request that will be picked up later on by UpdateWebAccess()
         /// </summary>
+        /// <param name="node"></param>
         /// <returns></returns>
-        private static LichessOpeningsStats CheckResults()
+        public static bool RequestWebAccess(int treeId, TreeNode nd, bool force = false)
         {
-            return WebAccess.OpeningExplorer.GetOpeningStats(WebAccessState.WaitingNode);
+            lock (_lockWebUpdate)
+            {
+                if (force || nd != WebAccessState.WaitingNode)
+                {
+                    AppLog.Message(2, "Requesting Web Access for:" + nd.LastMoveAlgebraicNotation);
+
+                    if (WebAccessState.WaitingNode != null)
+                    {
+                        AppLog.Message(2, "Cancelling Web Access for:" + WebAccessState.WaitingNode.LastMoveAlgebraicNotation);
+                    }
+
+                    WebAccessState.QueuedNode = nd;
+                    WebAccessState.QueuedNodeTreeId = treeId;
+                    WebAccessState.HasQueuedRequest = true;
+
+                    if (WebAccessState.IsMandatoryDelayOn)
+                    {
+                        // reset the "mandatory delay" counter.
+                        // this is to prevent processing of requests when too many come in a short
+                        // period of time e.g. when the user keep the right arrow depressed.
+                        _webAccessCounter = 0;
+                    }
+                }
+            }
+
+            return true;
         }
 
         /// <summary>
@@ -96,35 +142,17 @@ namespace ChessForge
         }
 
         /// <summary>
-        /// Replaces the State attributes, effectively cancelling the one that
-        /// was in progress if any.
+        /// Checks if the results are already in the cache.
         /// </summary>
-        /// <param name="node"></param>
         /// <returns></returns>
-        public static bool RequestWebAccess(int treeId, TreeNode nd, bool force = false)
+        private static LichessOpeningsStats CheckResults()
         {
-            if (force || nd != WebAccessState.WaitingNode)
-            {
-                AppLog.Message(2, "Requesting Web Access for:" + nd.LastMoveAlgebraicNotation);
-
-                if (WebAccessState.WaitingNode != null)
-                {
-                    AppLog.Message(2, "Cancelling Web Access for:" + WebAccessState.WaitingNode.LastMoveAlgebraicNotation);
-                }
-
-                WebAccessState.IsWaitingForResults = false;
-                WebAccessState.WaitingNode = nd;
-                WebAccessState.WaitingNodeTreeId = treeId;
-                WebAccessState.HasWaitingRequest = true;
-            }
-
-            return true;
+            return WebAccess.OpeningExplorer.GetOpeningStats(WebAccessState.WaitingNode);
         }
 
-
         /// <summary>
-        /// Execute a Web request.
-        /// This will be invoked from PulseManager once web query
+        /// Executes a Web request.
+        /// This will be invoked from PulseManager once the web query
         /// is greenlit for execution.
         /// </summary>
         /// <param name="treeId"></param>
@@ -159,23 +187,6 @@ namespace ChessForge
                     });
                 }
                 catch { }
-            }
-        }
-
-        /// <summary>
-        /// Delegate listening to the Explorer request completed events.
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private static void ExplorerRequestCompleted(object sender, WebAccessEventArgs e)
-        {
-            WebAccessState.IsWaitingForResults = false;
-
-            // check if we have anything queued and if so run it
-            if (WebAccessState.WaitingNode != null)
-            {
-                ExplorerRequest(0, WebAccessState.WaitingNode);
-                WebAccessState.WaitingNode = null;
             }
         }
     }
