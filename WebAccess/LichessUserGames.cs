db446c73e907ca3c37741b79dab9be477dc86127
czbar <czbar@users.noreply.github.com>
2023-09-09 19:57:19 -0600

    Removed junk refs

--- WebAccess/LichessUserGames.cs
+++ WebAccess/LichessUserGames.cs
@@ -5,10 +5,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
-using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace WebAccess
 {
