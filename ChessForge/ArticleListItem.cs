60a908b43c77f570368b5d0557e499c79e7d03e2
czbar <czbar@users.noreply.github.com>
2023-09-20 16:47:01 -0600

    Changed how to display chapter title

--- ChessForge/ArticleListItem.cs
+++ ChessForge/ArticleListItem.cs
@@ -155,7 +155,7 @@ namespace ChessForge
                 }
                 else if (_chapter != null)
                 {
-                    header = Properties.Resources.Chapter.ToUpper() + ": " + _chapter.Title;
+                    header = "[" + Chapter.Index.ToString() + ".] " + _chapter.Title;
                 }
 
                 string prefix = string.Empty;
