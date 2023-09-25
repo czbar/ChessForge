b77af4e7e8b4bb365efcd10813a1e55c25a12f67
czbar <czbar@users.noreply.github.com>
2023-09-25 15:50:37 -0600

    Created EngineGameView.cs

---
+++ ChessForge/EngineGameView.cs
@@ -0,0 +1,171 @@
+﻿using ChessPosition;
+using GameTree;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Media;
+
+namespace ChessForge
+{
+    public class EngineGameView : RichTextBuilder
+    {
+        /// <summary>
+        /// Types of paragraphs used in this document.
+        /// There can only be one, or none, paragraph of a given type in the document
+        /// </summary>
+        private enum ParaType
+        {
+            INTRO,
+            GAME_MOVES
+        }
+
+        /// <summary>
+        /// Maps paragraph types to Paragraph objects
+        /// </summary>
+        private Dictionary<ParaType, Paragraph> _dictParas = new Dictionary<ParaType, Paragraph>()
+        {
+            [ParaType.INTRO] = null,
+            [ParaType.GAME_MOVES] = null,
+        };
+
+        /// <summary>
+        /// Creates an instance of this class and sets reference 
+        /// to the FlowDocument managed by the object.
+        /// </summary>
+        /// <param name="doc"></param>
+        public EngineGameView(FlowDocument doc) : base(doc)
+        {
+        }
+
+
+        /// <summary>
+        /// Property referencing definitions of Paragraphs 
+        /// </summary>
+        override internal Dictionary<string, RichTextPara> RichTextParas { get { return _richTextParas; } }
+
+        private static readonly string STYLE_INTRO = "intro";
+        private static readonly string STYLE_GAME_MOVES = "moves_main";
+        private static readonly string STYLE_DEFAULT = "default";
+
+        private readonly string _run_ = "run_";
+
+        /// <summary>
+        /// Layout definitions for paragraphs at different levels.
+        /// </summary>
+        internal Dictionary<string, RichTextPara> _richTextParas = new Dictionary<string, RichTextPara>()
+        {
+            [STYLE_INTRO] = new RichTextPara(0, 0, 12, FontWeights.Normal, new SolidColorBrush(Color.FromRgb(0, 0, 0)), TextAlignment.Left),
+            [STYLE_GAME_MOVES] = new RichTextPara(10, 5, 16, FontWeights.Bold, new SolidColorBrush(Color.FromRgb(120, 61, 172)), TextAlignment.Left),
+            [STYLE_DEFAULT] = new RichTextPara(10, 5, 12, FontWeights.Normal, new SolidColorBrush(Color.FromRgb(128, 98, 63)), TextAlignment.Left),
+        };
+
+        /// <summary>
+        /// Builds the FlowDocument representing the current GameLine.
+        /// Inserts dummy (no text) run for the starting position (NodeId == 0)
+        /// </summary>
+        public void BuildFlowDocumentForGameLine()
+        {
+            Document.Blocks.Clear();
+
+            Document.Blocks.Add(BuildDummyPararaph());
+            Document.Blocks.Add(BuildGameMovesParagraph());
+
+//            Paragraph movePromptPara = CreateOrUpdateMovePrompt();
+        }
+
+        private Paragraph BuildGameMovesParagraph()
+        {
+            Paragraph para = new Paragraph();
+            para.Margin = new Thickness(10, 0, 0, 0);
+            para.Inlines.Add(new Run(""));
+
+            ObservableCollection<TreeNode> gameMoves = EngineGame.Line.NodeList;
+            for (int i = 1; i < EngineGame.Line.NodeList.Count; i++)
+            {
+                BuildNodeTextAndAddToPara(EngineGame.Line.NodeList[i], true, para);
+            }
+
+            return para;
+        }
+
+        /// <summary>
+        /// Builds text of an individual node (ply),
+        /// creates a new Run and adds it to the paragraph.
+        /// </summary>
+        /// <param name="nd"></param>
+        /// <param name="includeNumber"></param>
+        private void BuildNodeTextAndAddToPara(TreeNode nd, bool includeNumber, Paragraph para)
+        {
+            string nodeText = BuildNodeText(nd, includeNumber);
+            Run r = AddRunToParagraph(nd, para, nodeText);
+        }
+
+        private Run AddRunToParagraph(TreeNode nd, Paragraph para, string text)
+        {
+            Run r = null;
+
+            try
+            {
+                r = new Run(text.ToString());
+                r.Name = _run_ + nd.NodeId.ToString();
+                //r.PreviewMouseDown += EventRunClicked;
+
+            }
+            catch (Exception ex)
+            {
+                AppLog.Message("AddRunToParagraph()", ex);
+            }
+
+            return r;
+        }
+
+        /// <summary>
+        /// Builds text for the passed Node.
+        /// </summary>
+        /// <param name="nd"></param>
+        /// <param name="includeNumber"></param>
+        /// <returns></returns>
+        private string BuildNodeText(TreeNode nd, bool includeNumber)
+        {
+            if (nd.NodeId == 0)
+            {
+                return "";
+            }
+
+            StringBuilder sb = new StringBuilder();
+
+            if (nd.Position.ColorToMove == PieceColor.Black)
+            {
+                sb.Append(nd.Position.MoveNumber.ToString() + ".");
+            }
+
+            if (nd.Position.ColorToMove == PieceColor.White && includeNumber)
+            {
+                sb.Append(nd.Position.MoveNumber.ToString() + "...");
+            }
+
+            sb.Append(nd.GetGuiPlyText(true));
+            sb.Append(" ");
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Creates a dummy paragraph to use for spacing before
+        /// the first "real" paragraph. 
+        /// </summary>
+        /// <returns></returns>
+        private Paragraph BuildDummyPararaph()
+        {
+            Paragraph dummy = new Paragraph();
+            dummy.Margin = new Thickness(0, 0, 0, 0);
+            dummy.Inlines.Add(new Run(""));
+            return dummy;
+        }
+
+    }
+}
