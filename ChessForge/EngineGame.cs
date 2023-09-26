c8ba9ff8bb00333eda1ed901c2e4c5c8dea481e0
czbar <czbar@users.noreply.github.com>
2023-09-26 16:36:46 -0600

    Implemented EngineColor

--- ChessForge/EngineGame.cs
+++ ChessForge/EngineGame.cs
@@ -37,6 +37,22 @@ namespace ChessForge
         /// </summary>
         public static GameState CurrentState { get => _gameState; }
 
+        // color that the engine plays with
+        private static PieceColor _engineColor = PieceColor.None;
+
+        /// <summary>
+        /// The color with which the engine plays.
+        /// This is not controlled here but is just a convenient
+        /// place holder for use by clients.
+        /// In particular it may not be set at all.
+        /// </summary>
+        public static PieceColor EngineColor
+        {
+            get => _engineColor;
+            set => _engineColor = value;
+        }
+
+
         /// <summary>
         /// Position from which the game started.
         /// This will be a reference to a Node in the Workbook Tree.
