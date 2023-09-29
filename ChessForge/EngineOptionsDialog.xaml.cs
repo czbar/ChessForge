64839be2ce3186d0b1474d5fa6e780bad8775c3a
czbar <czbar@users.noreply.github.com>
2023-09-28 18:25:30 -0600

    Integrated Engine Options dialog

--- ChessForge/EngineOptionsDialog.xaml.cs
+++ ChessForge/EngineOptionsDialog.xaml.cs
@@ -195,7 +195,7 @@ namespace ChessForge
         /// <param name="e"></param>
         private void UiBtnHelp_Click(object sender, RoutedEventArgs e)
         {
-           // System.Diagnostics.Process.Start("https://github.com/czbar/ChessForge/wiki/Application-Options-Dialog");
+           System.Diagnostics.Process.Start("https://github.com/czbar/ChessForge/wiki/Engine-Options-Dialog");
         }
     }
 }
