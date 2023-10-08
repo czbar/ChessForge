434e7e1386a1d5d102501b4236a190c81e571955
czbar <czbar@users.noreply.github.com>
2023-10-08 22:34:28 +1100

    Added constructor

--- ChessForge/WorkbookOperation.cs
+++ ChessForge/WorkbookOperation.cs
@@ -27,6 +27,7 @@ namespace ChessForge
         EDIT_EXERCISE_HEADER,
         MOVE_ARTICLES,
         COPY_ARTICLES,
+        DELETE_COMMENTS
     }
 
     /// <summary>
@@ -132,6 +133,21 @@ namespace ChessForge
             _articleIndex = articleIndex;
             _opData_1 = data1;
             _opData_2 = data2;
+        }
+
+        /// <summary>
+        /// Constructor for actions:
+        ///    DELETE_COMMENTS
+        ///    DELETE_ENGINE_EVALS
+        /// The data parameter is a dictionary with Articles as keys
+        /// and deleted data as values.
+        /// </summary>
+        /// <param name="tp"></param>
+        /// <param name="data"></param>
+        public WorkbookOperation(WorkbookOperationType tp, object data) : base()
+        {
+            _opType = tp;
+            _opData_1 = data;
         }
     }
 }
