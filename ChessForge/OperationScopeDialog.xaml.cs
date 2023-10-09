ea4c3b164a66dfbbdc29a3e5ef7bc1d5d7cfc6f9
czbar <czbar@users.noreply.github.com>
2023-10-09 20:34:56 +1100

    Reworked the behavior on Radio Button checks

--- ChessForge/OperationScopeDialog.xaml.cs
+++ ChessForge/OperationScopeDialog.xaml.cs
@@ -36,6 +36,9 @@ namespace ChessForge
         /// </summary>
         public bool ApplyToExercises { get; set; }
 
+        // flags change in selection logic after the first radio button check
+        private bool firstTimeCheck = true;
+
         /// <summary>
         /// Constructor. Sets the title of the dialog
         /// and checks the controls as per the current state
@@ -70,25 +73,71 @@ namespace ChessForge
         /// the one that corresponds to the passed itemType.
         /// </summary>
         /// <param name="itemType"></param>
-        private void ShowItemType(GameData.ContentType itemType)
+        private void ShowItemType(GameData.ContentType itemType, bool enabled)
         {
             UiCbStudy.IsChecked = false;
             UiCbGames.IsChecked = false;
             UiCbExercises.IsChecked = false;
 
+            CheckBox cb = null;
             switch (itemType)
             {
                 case GameData.ContentType.STUDY_TREE:
-                    UiCbStudy.IsChecked = true;
+                    cb = UiCbStudy;
                     break;
                 case GameData.ContentType.MODEL_GAME:
-                    UiCbGames.IsChecked = true;
+                    cb = UiCbGames;
                     break;
                 case GameData.ContentType.EXERCISE:
-                    UiCbExercises.IsChecked = true;
+                    cb = UiCbExercises;
                     break;
             }
+
+            if (cb != null)
+            {
+                cb.IsChecked = true;
+                cb.Visibility = Visibility.Visible;
+                cb.IsEnabled = enabled ? true : false;
+            }
         }
+
+        /// <summary>
+        /// Enables all itmes, shows or hides them,
+        /// sets singular or plural lable for games/exercises.
+        /// </summary>
+        /// <param name="showHide"></param>
+        /// <param name="plural"></param>
+        private void ShowEnableAllItemTypes(bool showHide, bool plural)
+        {
+            UiCbStudy.IsEnabled = true;
+            UiCbGames.IsEnabled = true;
+            UiCbExercises.IsEnabled = true;
+            
+            UiCbStudy.Visibility = showHide ? Visibility.Visible : Visibility.Hidden;
+            UiCbGames.Visibility = showHide ? Visibility.Visible : Visibility.Hidden;
+            UiCbExercises.Visibility = showHide ? Visibility.Visible : Visibility.Hidden;
+
+            UiCbGames.Content = plural ? Properties.Resources.Games : Properties.Resources.Game;
+            UiCbExercises.Content = plural ? Properties.Resources.Exercises : Properties.Resources.Exercise;
+        }
+
+        /// <summary>
+        /// Checks or unchecks all check boxes.
+        /// </summary>
+        /// <param name="isChecked"></param>
+        private void CheckAll(bool isChecked)
+        {
+            UiCbStudy.IsChecked = isChecked;
+            UiCbGames.IsChecked = isChecked;
+            UiCbExercises.IsChecked = isChecked;
+        }
+
+
+        //####################################################
+        //
+        // Radio button checked
+        //
+        //####################################################
 
         /// <summary>
         /// "Current View" radio button selected.
@@ -99,11 +148,13 @@ namespace ChessForge
         {
             if (AppState.MainWin.ActiveVariationTree == null)
             {
-                ShowItemType(GameData.ContentType.NONE);
+                ShowItemType(GameData.ContentType.NONE, false);
+                ShowEnableAllItemTypes(false, true);
             }
             else
             {
-                ShowItemType(AppState.MainWin.ActiveVariationTree.ContentType);
+                ShowEnableAllItemTypes(false, false);
+                ShowItemType(AppState.MainWin.ActiveVariationTree.ContentType, false);
             }
             UiCbStudy.Content = Properties.Resources.Study;
         }
@@ -115,7 +166,13 @@ namespace ChessForge
         /// <param name="e"></param>
         private void UiRbCurrentChapter_Checked(object sender, RoutedEventArgs e)
         {
+            ShowEnableAllItemTypes(true, true);
             UiCbStudy.Content = Properties.Resources.Study;
+            if (firstTimeCheck)
+            {
+                CheckAll(true);
+                firstTimeCheck = false;
+            }
         }
 
         /// <summary>
@@ -125,8 +182,20 @@ namespace ChessForge
         /// <param name="e"></param>
         private void UiRbWorkbook_Checked(object sender, RoutedEventArgs e)
         {
+            ShowEnableAllItemTypes(true, true);
             UiCbStudy.Content = (AppState.Workbook != null && AppState.Workbook.GetChapterCount() > 1) ? Properties.Resources.Studies : Properties.Resources.Study;
+            if (firstTimeCheck)
+            {
+                CheckAll(true);
+                firstTimeCheck = false;
+            }
         }
+
+        //####################################################
+        //
+        // Exit
+        //
+        //####################################################
 
         /// <summary>
         /// Collects the controls states and converts them to scope. 
