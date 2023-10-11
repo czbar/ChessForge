000eda4a9c61e15a3349be01a023f7de553934c2
czbar <czbar@users.noreply.github.com>
2023-10-11 12:52:27 +1100

    Created OnlineLibrary.cs

---
+++ WebAccess/OnlineLibrary.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAccess
+{
+    /// <summary>
+    /// Access to the online library.
+    /// </summary>
+    public class OnlineLibrary
+    {
+        /// <summary>
+        /// Gets and deserializes the library content from ChessForge's web site.
+        /// </summary>
+        /// <returns></returns>
+        public static LibraryContent GetLibraryContent()
+        {
+            LibraryContent library = null;
+
+            string urlQuery = "https://chessforge.sourceforge.io/LibraryContent.json";
+            var request = WebRequest.CreateHttp(urlQuery);
+
+            request.Method = "GET";
+
+            using (var response = (HttpWebResponse)request.GetResponse())
+            {
+                using (var responseStream = response.GetResponseStream())
+                {
+                    using (var myStreamReader = new StreamReader(responseStream, Encoding.UTF8))
+                    {
+                        var json = myStreamReader.ReadToEnd();
+                        library = JsonConvert.DeserializeObject<LibraryContent>(json);
+                    }
+                }
+            }
+
+            return library;
+        }
+
+    }
+}
