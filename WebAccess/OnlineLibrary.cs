f0f6ef9c2f60614af7e069fd801ff04865637414
czbar <czbar@users.noreply.github.com>
2023-10-12 16:16:56 +1100

    Implemented GetWorkbookText()

--- WebAccess/OnlineLibrary.cs
+++ WebAccess/OnlineLibrary.cs
@@ -16,6 +16,12 @@ namespace WebAccess
     /// </summary>
     public class OnlineLibrary
     {
+        // root URL for the library
+        private static string LIBRARY_URL = "https://chessforge.sourceforge.io/Library/";
+
+        // name of the json content file at the root of the library
+        private static string LIBRARY_CONTENT_FILE = "LibraryContent.json";
+
         /// <summary>
         /// Gets and deserializes the library content from ChessForge's web site.
         /// </summary>
@@ -27,7 +33,7 @@ namespace WebAccess
             LibraryContent library = null;
             try
             {
-                string urlQuery = "https://chessforge.sourceforge.io/LibraryContent.json";
+                string urlQuery = LIBRARY_URL + LIBRARY_CONTENT_FILE;
                 var request = WebRequest.CreateHttp(urlQuery);
                 request.Method = "GET";
 
@@ -52,5 +58,42 @@ namespace WebAccess
             return library;
         }
 
+        /// <summary>
+        /// Reads the content of the workbook file at the specified URL path.
+        /// </summary>
+        /// <param name="bookPath"></param>
+        /// <param name="error"></param>
+        /// <returns></returns>
+        public static string GetWorkbookText(string bookPath, out string error)
+        {
+            error = string.Empty;
+
+            string bookText = null;
+
+            try
+            {
+                string urlQuery = LIBRARY_URL + bookPath;
+                var request = WebRequest.CreateHttp(urlQuery);
+                request.Method = "GET";
+
+                using (var response = (HttpWebResponse)request.GetResponse())
+                {
+                    using (var responseStream = response.GetResponseStream())
+                    {
+                        using (var myStreamReader = new StreamReader(responseStream, Encoding.UTF8))
+                        {
+                            bookText = myStreamReader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                error = ex.Message;
+                AppLog.Message("GetWorkbookText()", ex);
+            }
+
+            return bookText;
+        }
     }
 }
