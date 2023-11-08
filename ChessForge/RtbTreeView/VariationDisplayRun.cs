38eb230b45c96cf6412de576f155b884e92468db
czbar <czbar@users.noreply.github.com>
2023-11-08 16:14:32 +1100

    Added classes for refactored Study View rendering

---
+++ ChessForge/RtbTreeView/VariationDisplayRun.cs
@@ -0,0 +1,22 @@
+﻿using GameTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+
+namespace ChessForge
+{
+    /// <summary>
+    /// Combines a Run shown in the Variation view with associated objects.
+    /// </summary>
+    public class VariationDisplayRun
+    {
+        // the Run to show
+        Run _run;
+
+        // the Node that this object represents.
+        TreeNode _node;
+    }
+}
---
+++ ChessForge/RtbTreeView/VariationSingleLine.cs
@@ -0,0 +1,40 @@
+﻿using GameTree;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChessForge
+{
+    /// <summary>
+    /// A list of nodes forming a single path from one node to another
+    /// without any branches included.
+    /// </summary>
+    public class VariationSingleLine
+    {
+        // the list of nodes forming a single linear path.
+        private List<TreeNode> _nodes = new List<TreeNode>();
+
+        // whether the line has any forks
+        private bool _hasForks;
+
+        /// <summary>
+        /// The list of nodes in this line.
+        /// </summary>
+        public List<TreeNode> Nodes
+        {
+            get => _nodes;
+            set => _nodes = value;
+        }
+
+        /// <summary>
+        /// Whether any node in this line is a fork.
+        /// If not, it may be aggregated into a single VariationDisplayLine
+        /// with other VariationSingleLines.
+        /// </summary>
+        public bool HasForks
+        {
+            get => _hasForks;
+            set => _hasForks = value;
+        }
+    }
+}
