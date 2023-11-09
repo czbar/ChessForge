ae27e01c1f14fa0d80da2319627578fa698d8657
czbar <czbar@users.noreply.github.com>
2023-11-09 15:34:15 +1100

    Persists control values in session

--- ChessForge/ArticleSearchCriteriaDialog.xaml.cs
+++ ChessForge/ArticleSearchCriteriaDialog.xaml.cs
@@ -13,14 +13,86 @@ namespace ChessForge
     /// </summary>
     public partial class ArticleSearchCriteriaDialog : Window
     {
+        //**** values persisted between closing and re-opening the dialog. ***
+
+        private static string _whiteName = "";
+        private static string _blackName = "";
+
+        private static bool? _ignoreColors = false;
+
+        private static string _minMoves = "";
+        private static string _maxMoves = "";
+        private static string _minYear = "";
+        private static string _maxYear = "";
+        private static bool? _includeEmptyYear = false;
+
+        private static string _minECO = "";
+        private static string _maxECO = "";
+
+        private static bool? _resultWhiteWin = false;
+        private static bool? _resultBlackWin = false;
+        private static bool? _resultDraw = false;
+        private static bool? _resultNone = false;
+
+
         /// <summary>
         /// Constructor.
         /// </summary>
         public ArticleSearchCriteriaDialog()
         {
             InitializeComponent();
-
+            RestoreLastValues();
             UiTbWhite.Focus();
+        }
+
+        /// <summary>
+        /// Set initial values as per the last session
+        /// </summary>
+        private void RestoreLastValues()
+        {
+            UiTbWhite.Text = _whiteName;
+            UiTbBlack.Text = _blackName;
+
+            UiCbIgnoreColors.IsChecked = _ignoreColors;
+
+            UiTbMinMoves.Text = _minMoves;
+            UiTbMaxMoves.Text = _maxMoves;
+            UiTbMinYear.Text = _minYear;
+            UiTbMaxYear.Text = _maxYear;
+            UiCbEmptyYear.IsChecked = _includeEmptyYear;
+
+            UiTbMinEco.Text = _minECO;
+            UiTbMaxEco.Text = _maxECO;
+
+            UiCbWhiteWin.IsChecked = _resultWhiteWin;
+            UiCbWhiteLoss.IsChecked = _resultBlackWin;
+            UiCbDraw.IsChecked = _resultDraw;
+            UiCbNoResult.IsChecked = _resultNone;
+        }
+
+        /// <summary>
+        /// Saves values on exit to use when initializing the next time.
+        /// </summary>
+        private void SaveValuesOnExit()
+        {
+            _whiteName = UiTbWhite.Text;
+            _blackName = UiTbBlack.Text;
+
+            UiCbIgnoreColors.IsChecked = _ignoreColors;
+
+            _minMoves = UiTbMinMoves.Text;
+            _maxMoves = UiTbMaxMoves.Text;
+            _minYear = UiTbMinYear.Text;
+            _maxYear = UiTbMaxYear.Text;
+            _includeEmptyYear = UiCbEmptyYear.IsChecked;
+
+            _minECO = UiTbMinEco.Text;
+            _maxECO = UiTbMaxEco.Text;
+
+            _resultWhiteWin = UiCbWhiteWin.IsChecked;
+            _resultBlackWin = UiCbWhiteLoss.IsChecked;
+            _resultDraw = UiCbDraw.IsChecked;
+            _resultNone = UiCbNoResult.IsChecked;
         }
 
         /// <summary>
@@ -30,6 +102,7 @@ namespace ChessForge
         /// <param name="e"></param>
         private void UiBtnOk_Click(object sender, RoutedEventArgs e)
         {
+            SaveValuesOnExit();
             DialogResult = true;
         }
     }
