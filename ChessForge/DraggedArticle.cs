1212f37c33f7c45a3151d2dfea590d2c111ba2cc
czbar <czbar@users.noreply.github.com>
2023-11-23 20:33:43 +1100

    Implemented IsChapterDragged()

--- ChessForge/DraggedArticle.cs
+++ ChessForge/DraggedArticle.cs
@@ -32,6 +32,15 @@ namespace ChessForge
         public static int ArticleIndex { get; set; }
 
         /// <summary>
+        /// Return true if the dragged object is chapter
+        /// </summary>
+        /// <returns></returns>
+        public static bool IsChapterDragged()
+        {
+            return ContentType == GameData.ContentType.NONE && ArticleIndex == -1 && ChapterIndex >= 0;
+        }
+
+        /// <summary>
         /// Set the variables for the started drag operation.
         /// </summary>
         /// <param name="chapter"></param>
