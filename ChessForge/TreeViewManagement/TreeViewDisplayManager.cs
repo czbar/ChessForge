f756219d6ed5e0bad7341c97ac6231e37b11e0b9
czbar <czbar@users.noreply.github.com>
2023-12-30 22:00:26 +1100

    Update TreeViewDisplayManager.cs

--- ChessForge/TreeViewManagement/TreeViewDisplayManager.cs
+++ ChessForge/TreeViewManagement/TreeViewDisplayManager.cs
@@ -18,6 +18,8 @@ namespace ChessForge
         // the tree holding all LineSectors.
         public LineSectorsTree SectorsTree;
 
+        public List<DisplaySector> DisplaySectors;
+
         /// <summary>
         /// The VariationTree whose lines are managed by this objects
         /// </summary>
@@ -63,14 +65,14 @@ namespace ChessForge
         /// The method calls itself recursively to build the complete set of clean lines.
         /// </summary>
         /// <param name="nd"></param>
-        public void BuildLineSector(LineSector parent, TreeNode nd, int level)
+        private void BuildLineSector(LineSector parent, TreeNode nd, int level)
         {
             _runningSectorId++;
             LineSector sector = new LineSector();
             sector.LineSectorId = _runningSectorId;
             SectorsTree.LineSectors.Add(sector);
 
-            sector.DisplayLevel = level;
+            sector.BranchLevel = level;
             sector.Nodes.Add(nd);
 
             // add all leaf nodes that follow
@@ -100,6 +102,103 @@ namespace ChessForge
                 // we reached the end of the branch so return
                 sector.SectorType = LineSectorType.LEAF;
                 parent.AddChild(sector);
+            }
+        }
+
+        /// <summary>
+        /// Recursively builds all DisplaySectors
+        /// </summary>
+        public void BuildDisplaySectors()
+        {
+            DisplaySectors = new List<DisplaySector>();
+            BuildDisplaySector(SectorsTree.Root, 1, false);
+        }
+
+        /// <summary>
+        /// Builds a DisplaySector that will be shown in a single paragraph.
+        /// Calls itself recursively to build a sub-tree of DisplaySectors.
+        /// </summary>
+        /// <param name="lineSector"></param>
+        /// <param name="displayLevel"></param>
+        /// <param name="isSectionTitle"></param>
+        private LineSector BuildDisplaySector(LineSector lineSector, int displayLevel, bool isSectionTitle)
+        {
+            LineSector lastProcessedSector = lineSector;
+
+            DisplaySector displaySector = new DisplaySector();
+            displaySector.DisplayLevel = displayLevel;
+            DisplaySectors.Add(displaySector);
+
+            // copy TreeNodes from the lineSector
+            foreach (TreeNode node in lineSector.Nodes)
+            {
+                displaySector.Nodes.Add(node);
+            }
+
+            if (isSectionTitle)
+            {
+                // finish here and self-invoke to continue down the tree
+                foreach (LineSector sector in lineSector.Children)
+                {
+                    BuildDisplaySector(sector, displayLevel++, isSectionTitle);
+                }
+            }
+            else
+            {
+                if (lineSector.SectorType == LineSectorType.LEAF)
+                {
+                    // all done so just return
+                }
+                else if (lineSector.SectorType == LineSectorType.FORKING)
+                {
+                    // not a section title line so the main line takes the first move of the first child
+                    displaySector.Nodes.Add(lineSector.Children[0].Nodes[0]);
+                    // if we have just 2 children and the second is of type LEAF, add it as a sub-sector
+                    // and keep going with the first child as if it was the same sector
+                    lastProcessedSector = HandleSubSectors(displaySector, lineSector);
+
+                    foreach (LineSector sector in lastProcessedSector.Children)
+                    {
+                        BuildDisplaySector(sector, displayLevel++, isSectionTitle);
+                    }
+                }
+            }
+
+            return lastProcessedSector;
+        }
+
+        private LineSector HandleSubSectors(DisplaySector displaySector, LineSector lineSector)
+        {
+            LineSector subSector = GetSubSector(lineSector);
+            {
+                while (subSector != null)
+                {
+                    displaySector.SubSectors.Add(subSector);
+                    foreach (TreeNode node in lineSector.Children[0].Nodes)
+                    {
+                        displaySector.Nodes.Add(node);
+                    }
+                    subSector = HandleSubSectors(displaySector, subSector);
+                }
+            }
+
+            return subSector;
+        }
+
+        /// <summary>
+        /// Checks if there is a child LineSector meeting the sub-sector criteria.
+        /// </summary>
+        /// <param name="lineSector"></param>
+        /// <returns></returns>
+        private LineSector GetSubSector(LineSector lineSector)
+        {
+            if (lineSector.Children.Count == 2 && lineSector.Children[1].SectorType == LineSectorType.LEAF)
+            {
+                return lineSector.Children[1];
+            }
+            else
+            {
+                return null;
             }
         }
     }
