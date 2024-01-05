a90e6f60c439c51b6b7a7cdc467024eef0b2c834
czbar <czbar@users.noreply.github.com>
2024-01-05 18:03:52 +1100

    Implemented SortExercises()

--- ChessForge/ChapterUtils.cs
+++ ChessForge/ChapterUtils.cs
@@ -508,6 +508,9 @@ namespace ChessForge
 
         /// <summary>
         /// Sorts games in the chapter per specified criteria.
+        /// Keep the original order if items cannot be separated by those criteria.
+        /// This method is superior to the standard QuickSort because it does keep 
+        /// the original order.
         /// </summary>
         /// <param name="chapter"></param>
         /// <param name="sortBy"></param>
@@ -516,6 +519,29 @@ namespace ChessForge
         {
             IComparer<Article> comparer = new ArticleComparer(sortBy, direction);
             chapter.ModelGames = chapter.ModelGames
+                .Select((item, index) => new { item, index })
+                .OrderBy(z => z.item, comparer)
+                .ThenBy(z => z.index)
+                .Select(z => z.item)
+                .ToList();
+
+            AppState.MainWin.ChaptersView.IsDirty = true;
+            AppState.IsDirty = true;
+        }
+
+        /// <summary>
+        /// Sorts exercises in the chapter per specified criteria.
+        /// Keep the original order if items cannot be separated by those criteria.
+        /// This method is superior to the standard QuickSort because it does keep 
+        /// the original order.
+        /// </summary>
+        /// <param name="chapter"></param>
+        /// <param name="sortBy"></param>
+        /// <param name="direction"></param>
+        private static void SortExercises(Chapter chapter, GameSortCriterion.SortItem sortBy, GameSortCriterion.SortItem direction)
+        {
+            IComparer<Article> comparer = new ArticleComparer(sortBy, direction);
+            chapter.Exercises = chapter.Exercises
                 .Select((item, index) => new { item, index })
                 .OrderBy(z => z.item, comparer)
                 .ThenBy(z => z.index)
