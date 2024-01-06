1830a53edff81ea399596712a58581ab08728ecf
czbar <czbar@users.noreply.github.com>
2024-01-06 11:39:25 +1100

    Created Tools.cs

---
+++ ChessForge/Tools.cs
@@ -0,0 +1,113 @@
+﻿using GameTree;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace ChessForge
+{
+    public class Tools
+    {
+        /// <summary>
+        /// Invokes the scope dialog and works out ECOs for articles
+        /// in the selected scope.
+        /// </summary>
+        public static bool AssignEcoToArticles()
+        {
+            bool anyUpdated = false;
+
+            try
+            {
+                OperationScopeDialog dlg = new OperationScopeDialog(Properties.Resources.ECO);
+                GuiUtilities.PositionDialog(dlg, AppState.MainWin, 100);
+
+                if (dlg.ShowDialog() == true)
+                {
+                    Mouse.SetCursor(Cursors.Wait);
+                    switch (dlg.ApplyScope)
+                    {
+                        case ChessPosition.OperationScope.ACTIVE_ITEM:
+                            anyUpdated = AssignEcoToArticle(AppState.Workbook.ActiveArticle);
+                            break;
+                        case ChessPosition.OperationScope.CHAPTER:
+                            anyUpdated = AssignEcosInChapter(AppState.ActiveChapter);
+                            break;
+                        case ChessPosition.OperationScope.WORKBOOK:
+                            anyUpdated = AssignEcosInWorbook(AppState.Workbook);
+                            break;
+                    }
+                }
+            }
+            catch
+            {
+            }
+
+            Mouse.SetCursor(Cursors.Arrow);
+            return anyUpdated;
+        }
+
+        /// <summary>
+        /// Assign ECO to an article.
+        /// </summary>
+        /// <param name="article"></param>
+        /// <returns>true if an ECO was found and it was different to the current one.</returns>
+        private static bool AssignEcoToArticle(Article article)
+        {
+            bool res = false;
+
+            EcoUtils.GetArticleEcoFromDictionary(article, out string eco);
+            if (!string.IsNullOrEmpty(eco))
+            {
+                string oldEco = article.Tree.Header.GetECO(out _);
+                if (oldEco != eco)
+                {
+                    article.Tree.Header.SetHeaderValue(PgnHeaders.KEY_ECO, eco);
+                    res = true;
+                }
+            }
+
+            return res;
+        }
+
+        /// <summary>
+        /// Assign a ECO to every article in the Chapter.
+        /// </summary>
+        /// <param name="chapter"></param>
+        /// <returns></returns>
+        private static bool AssignEcosInChapter(Chapter chapter)
+        {
+            bool anyUpdated = false;
+
+            anyUpdated = AssignEcoToArticle(chapter.StudyTree) ? true : anyUpdated;
+
+            foreach (Article article in chapter.ModelGames)
+            {
+                anyUpdated = AssignEcoToArticle(article) ? true : anyUpdated;
+            }
+
+            foreach (Article article in chapter.Exercises)
+            {
+                anyUpdated = AssignEcoToArticle(article) ? true : anyUpdated;
+            }
+
+            return anyUpdated;
+        }
+
+        /// <summary>
+        /// Assign ECO to each article in the passed workbook.
+        /// </summary>
+        /// <param name="workbook"></param>
+        /// <returns></returns>
+        private static bool AssignEcosInWorbook(Workbook workbook)
+        {
+            bool anyUpdated = false;
+
+            foreach (Chapter chapter in workbook.Chapters)
+            {
+                anyUpdated = AssignEcosInChapter(chapter) ? true : anyUpdated;
+            }
+
+            return anyUpdated;
+        }
+    }
+}
