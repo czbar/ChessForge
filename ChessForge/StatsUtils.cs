6bc96f791837cc10b3863b8089d9b804c19af671
czbar <czbar@users.noreply.github.com>
2024-01-13 22:21:29 +1100

    Updated logic

--- ChessForge/StatsUtils.cs
+++ ChessForge/StatsUtils.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using ChessPosition;
+using Newtonsoft.Json.Linq;
 
 namespace ChessForge
 {
@@ -17,8 +18,12 @@ namespace ChessForge
         /// <param name="scope"></param>
         public static void ReportStats(OperationScope scope)
         {
+            Dictionary<string, PlayerAggregatedStats> _dictPlayersStats = new Dictionary<string, PlayerAggregatedStats>();
             StatsData stats = new StatsData();
-            GetResultsStats(scope, stats);
+            GetResultsStats(scope, stats, _dictPlayersStats);
+
+            // report in the dialog
+            // identify the common player(s)
         }
 
         /// <summary>
@@ -33,13 +38,13 @@ namespace ChessForge
         /// <param name="draws"></param>
         /// <param name="commonPlayer_1"></param>
         /// <param name="commonPlayer_2"></param>
-        public static void GetResultsStats(OperationScope scope, StatsData stats)
+        public static void GetResultsStats(OperationScope scope, StatsData stats, Dictionary<string, PlayerAggregatedStats> dictPlayersStats)
         {
             if (scope == OperationScope.CHAPTER)
             {
                 if (AppState.ActiveChapter != null)
                 {
-                    GetChapterResultsStats(AppState.ActiveChapter, stats);
+                    GetChapterResultsStats(AppState.ActiveChapter, stats, dictPlayersStats);
 
                     stats.ChapterCount = 1;
                     stats.GameCount = AppState.ActiveChapter.GetModelGameCount();
@@ -48,11 +53,6 @@ namespace ChessForge
             }
             else if (scope == OperationScope.WORKBOOK && AppState.Workbook != null)
             {
-                bool hasCommonPlayer_1 = true;
-                bool hasCommonPlayer_2 = true;
-
-                bool firstChapter = true;
-
                 foreach (Chapter chapter in AppState.Workbook.Chapters)
                 {
                     stats.ChapterCount++;
@@ -60,32 +60,11 @@ namespace ChessForge
                     stats.ExerciseCount += chapter.GetExerciseCount();
 
                     StatsData chapterStats = new StatsData();
-                    if (GetChapterResultsStats(chapter, chapterStats))
+                    if (GetChapterResultsStats(chapter, chapterStats, dictPlayersStats))
                     {
                         stats.WhiteWins += chapterStats.WhiteWins;
                         stats.BlackWins += chapterStats.BlackWins;
                         stats.Draws += chapterStats.Draws;
-
-                        if (firstChapter)
-                        {
-                            stats.CommonPlayer_1 = chapterStats.CommonPlayer_1;
-                            stats.CommonPlayer_2 = chapterStats.CommonPlayer_2;
-                            firstChapter = false;
-                        }
-                        else
-                        {
-                            // check if we keep the current "common players"
-                            if (hasCommonPlayer_1 && stats.CommonPlayer_1 != chapterStats.CommonPlayer_1 && stats.CommonPlayer_1 != chapterStats.CommonPlayer_2)
-                            {
-                                stats.CommonPlayer_1 = null;
-                                hasCommonPlayer_1 = false;
-                            }
-                            if (hasCommonPlayer_2 && stats.CommonPlayer_2 != chapterStats.CommonPlayer_1 && stats.CommonPlayer_2 != chapterStats.CommonPlayer_2)
-                            {
-                                stats.CommonPlayer_2 = null;
-                                hasCommonPlayer_2 = false;
-                            }
-                        }
                     }
                 }
             }
@@ -101,184 +80,112 @@ namespace ChessForge
         /// <param name="commonPlayer_1"></param>
         /// <param name="commonPlayer_2"></param>
         /// <returns>true if any games were processed</returns>
-        private static bool GetChapterResultsStats(Chapter chapter, StatsData stats)
+        private static bool GetChapterResultsStats(Chapter chapter, StatsData stats, Dictionary<string, PlayerAggregatedStats> dictPlayersStats)
         {
-            Dictionary<string, int> playersGameCounts = new Dictionary<string, int>();
-
             int gamesCount = chapter.ModelGames.Count;
 
             try
             {
                 foreach (Article article in chapter.ModelGames)
                 {
-                    UpdatePlayerGamesDictionary(playersGameCounts, article);
                     GetResultFromString(article.Tree.Header.GetResult(out _), out bool whiteWin, out bool blackWin, out bool draw);
-                    if (whiteWin)
+                    stats.WhiteWins += whiteWin ? 1 : 0;
+                    stats.BlackWins += blackWin ? 1 : 0;
+                    stats.Draws += draw ? 1 : 0;
+
+                    string whitePlayer = article.Tree.Header.GetWhitePlayer(out _).Trim();
+                    string blackPlayer = article.Tree.Header.GetBlackPlayer(out _).Trim();
+
+                    UpdatePlayerScoresStats(whitePlayer, true, whiteWin, blackWin, draw, dictPlayersStats);
+                    UpdatePlayerScoresStats(blackPlayer, false, whiteWin, blackWin, draw, dictPlayersStats);
+
+                    // adjust total games if white and black have the same name, to avoid incorrect games total for the player
+                    if (whitePlayer == blackPlayer && !string.IsNullOrEmpty(whitePlayer))
                     {
-                        stats.WhiteWins++;
-                    }
-                    else if (blackWin)
-                    {
-                        stats.BlackWins++;
-                    }
-                    else if (draw)
-                    {
-                        stats.Draws++;
+                        ReduceTotalGamesCount(whitePlayer, dictPlayersStats);
                     }
                 }
             }
             catch
             {
             }
-
-            stats.CommonPlayer_1 = null;
-            stats.CommonPlayer_2 = null;
-
-            // find any players that were found in all games, they can only be 0, 1 or 2 such players
-            // they appearance number must be exactly gamesCount 'coz we avoided dupes in the logic above
-            foreach (KeyValuePair<string, int> keyValue in playersGameCounts)
-            {
-                if (keyValue.Value == gamesCount)
-                {
-                    if (string.IsNullOrEmpty(stats.CommonPlayer_1))
-                    {
-                        stats.CommonPlayer_1 = keyValue.Key;
-                    }
-                    else
-                    {
-                        stats.CommonPlayer_2 = keyValue.Key;
-                    }
-                }
-            }
-
-            if (!string.IsNullOrEmpty(stats.CommonPlayer_1))
-            {
-                GetPlayerResultsStats(chapter, stats.CommonPlayer_1, stats.Player_1_Stats);
-            }
-
-            if (!string.IsNullOrEmpty(stats.CommonPlayer_2))
-            {
-                GetPlayerResultsStats(chapter, stats.CommonPlayer_2, stats.Player_2_Stats);
-            }
-
             return gamesCount > 0;
         }
 
         /// <summary>
-        /// Updates the dictionary holding th enumber of games played by each player.
+        /// Decrements the total number of games for a player.
         /// </summary>
-        /// <param name="playersGameCounts"></param>
-        /// <param name="article"></param>
-        private static void UpdatePlayerGamesDictionary(Dictionary<string, int> playersGameCounts, Article article)
-        {
-            string whitePlayer = article.Tree.Header.GetWhitePlayer(out _).Trim();
-            string blackPlayer = article.Tree.Header.GetBlackPlayer(out _).Trim();
-
-            // update player stats
-            if (!playersGameCounts.ContainsKey(whitePlayer))
-            {
-                playersGameCounts.Add(whitePlayer, 1);
-            }
-            else
-            {
-                playersGameCounts[whitePlayer]++;
-            }
-
-            // handle the second player only if different than the previous one,
-            // otherwise we may get into some unwanted cases when a player plays vs themselves
-            if (!playersGameCounts.ContainsKey(blackPlayer))
-            {
-                playersGameCounts.Add(blackPlayer, 1);
-            }
-            else
-            {
-                playersGameCounts[blackPlayer]++;
-            }
-
-        }
-
-        /// <summary>
-        /// Collect results for the specified player in the passed chapter.
-        /// </summary>
-        /// <param name="chapter"></param>
         /// <param name="player"></param>
-        /// <param name="stats"></param>
-        private static void GetPlayerResultsStats(Chapter chapter, string player, PlayerScoresStats stats)
+        /// <param name="dictPlayersStats"></param>
+        private static void ReduceTotalGamesCount(string player, Dictionary<string, PlayerAggregatedStats> dictPlayersStats)
         {
-            foreach (Article article in chapter.ModelGames)
+            if (dictPlayersStats.ContainsKey(player))
             {
-                string whitePlayer = article.Tree.Header.GetWhitePlayer(out _).Trim();
-                string blackPlayer = article.Tree.Header.GetBlackPlayer(out _).Trim();
-
-                GetResultFromString(article.Tree.Header.GetResult(out _), out bool whiteWin, out bool blackWin, out bool draw);
-
-                if (whitePlayer == player)
-                {
-                    UpdatePlayerStats(stats, true, whiteWin, blackWin, draw);
-                }
-
-                if (blackPlayer == player)
-                {
-                    UpdatePlayerStats(stats, false, whiteWin, blackWin, draw);
-                    if (whitePlayer == blackPlayer)
-                    {
-                        // this is an anomaly when we have the same name as white and black
-                        // we will adjust down the total for this case
-                        stats.GameCount--;
-                    }
-                }
+                dictPlayersStats[player].TotalStats.GameCount--;
             }
         }
 
         /// <summary>
-        /// Updates player stats with a single result
+        /// Updates individual player stats.
         /// </summary>
-        /// <param name="stats"></param>
-        /// <param name="isWhite"></param>
+        /// <param name="player"></param>
+        /// <param name="playedWhite"></param>
         /// <param name="whiteWin"></param>
         /// <param name="blackWin"></param>
         /// <param name="draw"></param>
-        private static void UpdatePlayerStats(PlayerScoresStats stats, bool isWhite, bool whiteWin, bool blackWin, bool draw)
+        /// <param name="dictPlayersStats"></param>
+        private static void UpdatePlayerScoresStats(string player,
+                                                    bool playedWhite,
+                                                    bool whiteWin,
+                                                    bool blackWin,
+                                                    bool draw,
+                                                    Dictionary<string, PlayerAggregatedStats> dictPlayersStats)
         {
-            if (isWhite)
+            if (!string.IsNullOrEmpty(player))
             {
-                stats.WhiteScoreStats.GameCount++;
-                if (whiteWin)
+                if (!dictPlayersStats.ContainsKey(player))
                 {
-                    stats.WhiteScoreStats.Wins++;
+                    dictPlayersStats.Add(player, new PlayerAggregatedStats());
                 }
-                else if (blackWin)
-                {
-                    stats.WhiteScoreStats.Losses++;
-                }
-                else if (draw)
-                {
-                    stats.WhiteScoreStats.Draws++;
-                }
-            }
-            else
-            {
-                stats.BlackScoreStats.GameCount++;
-                if (blackWin)
-                {
-                    stats.BlackScoreStats.Wins++;
-                }
-                else if (blackWin)
-                {
-                    stats.BlackScoreStats.Losses++;
-                }
-                else if (draw)
-                {
-                    stats.BlackScoreStats.Draws++;
-                }
-            }
 
-            stats.SumupColorTotals();
+                PlayerAggregatedStats aggreg = dictPlayersStats[player];
+                aggreg.TotalStats.GameCount++;
+                if (playedWhite)
+                {
+                    UpdateColorStats(aggreg.WhiteStats, whiteWin, blackWin, draw);
+                }
+                else
+                {
+                    UpdateColorStats(aggreg.BlackStats, blackWin, whiteWin, draw);
+                }
+
+                aggreg.UpdateTotalScores();
+            }
+        }
+
+        /// <summary>
+        /// Updates player stats in the passed stats object.
+        /// This will be called to update player stats 
+        /// for one color. 
+        /// </summary>
+        /// <param name="stats"></param>
+        /// <param name="win"></param>
+        /// <param name="loss"></param>
+        /// <param name="draw"></param>
+        private static void UpdateColorStats(PlayerStats stats,
+                                             bool win,
+                                             bool loss,
+                                             bool draw)
+        {
+            stats.GameCount++;
+            stats.Wins += win ? 1 : 0;
+            stats.Losses += loss ? 1 : 0;
+            stats.Draws += draw ? 1 : 0;
         }
 
         /// <summary>
         /// Checks the type of the result as encoded in the passed string.
-        /// Returns true if this white win, black win or draw.
+        /// Returns true if this is white win, black win or draw.
         /// Otherwise returns false.
         /// </summary>
         /// <param name="sResult"></param>
