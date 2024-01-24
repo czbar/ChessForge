0f657a9a11e059e40bae85ee98a50094400057b4
czbar <czbar@users.noreply.github.com>
2024-01-24 22:27:10 +1100

    Completed implementation

--- ChessForge/SelectDuplicatesDialog.xaml.cs
+++ ChessForge/SelectDuplicatesDialog.xaml.cs
@@ -10,12 +10,13 @@ using System.Windows.Data;
 namespace ChessForge
 {
     /// <summary>
-    /// Interaction logic for SelectDuplicates.xaml
+    /// Identifies all duplicates of the passed article
+    /// and allows the user to determine which ones to keep and which ones to delete.
     /// </summary>
     public partial class SelectDuplicatesDialog : Window
     {
         // Collection bound to the ListView control
-        private ObservableCollection<ArticleListItem> _articleListItemsSource = new ObservableCollection<ArticleListItem>();
+        public ObservableCollection<ArticleListItem> DuplicateArticles = new ObservableCollection<ArticleListItem>();
 
         /// <summary>
         /// Creates the dialog, builds the list of items to display
@@ -31,7 +32,7 @@ namespace ChessForge
 
             Title = Properties.Resources.DuplicatesOf + ": " + BuildNamesString(article);
 
-            UiLvArticles.ItemsSource = _articleListItemsSource;
+            UiLvArticles.ItemsSource = DuplicateArticles;
         }
 
         /// <summary>
@@ -41,13 +42,16 @@ namespace ChessForge
         /// <returns></returns>
         public void BuildArticleItemList(List<Article> dupes)
         {
-            _articleListItemsSource = new ObservableCollection<ArticleListItem>();
+            DuplicateArticles = new ObservableCollection<ArticleListItem>();
 
+            bool isFirst = true;
             foreach (Article article in dupes)
             {
                 Article art = AppState.Workbook.GetArticleByGuid(article.Guid, out int chapterIndex, out int articleIndex);
                 ArticleListItem item = new ArticleListItem(AppState.Workbook.Chapters[chapterIndex], chapterIndex, art, articleIndex);
-                _articleListItemsSource.Add(item);
+                item.IsSelected = !isFirst;
+                DuplicateArticles.Add(item);
+                isFirst = false;
             }
         }
 
@@ -60,6 +64,41 @@ namespace ChessForge
         private string BuildNamesString(Article article)
         {
             return (article.Tree.Header.GetWhitePlayer(out _) ?? "NN") + " - " + (article.Tree.Header.GetBlackPlayer(out _) ?? "NN");
+        }
+
+        /// <summary>
+        /// Close the dialog on OK
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void UiBtnOk_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (ArticleListItem item in DuplicateArticles)
+            {
+                item.Article.Data = item.IsSelected;
+            }
+
+            DialogResult = true;
+        }
+
+        /// <summary>
+        /// Close the dialog on cancel.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void UiBtnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+
+        /// <summary>
+        /// Links to the relevant Wiki page.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void UiBtnHelp_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/czbar/ChessForge/wiki/Select-Duplicates-Dialog");
         }
     }
 }
