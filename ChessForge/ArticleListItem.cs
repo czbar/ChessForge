3a542a34c74d8654689536c3533462d0df009485
czbar <czbar@users.noreply.github.com>
2024-01-25 17:44:42 +1100

    Cleanup

--- ChessForge/ArticleListItem.cs
+++ ChessForge/ArticleListItem.cs
@@ -3,11 +3,8 @@ using GameTree;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace ChessForge
 {
