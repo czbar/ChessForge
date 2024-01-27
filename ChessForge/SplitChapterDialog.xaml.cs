1eb1adcc1852efcf5116175ae3796bba931fdc54
czbar <czbar@users.noreply.github.com>
2024-01-27 22:53:52 +1100

    CheckBox for moving games

--- ChessForge/SplitChapterDialog.xaml.cs
+++ ChessForge/SplitChapterDialog.xaml.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
-using System.Windows.Controls;
 
 namespace ChessForge
 {
@@ -36,6 +35,12 @@ namespace ChessForge
     public partial class SplitChapterDialog : Window
     {
         /// <summary>
+        /// Whether the Move to Chapters per ECO
+        /// option was selected.
+        /// </summary>
+        public bool MoveToChaptersPerECO;
+
+        /// <summary>
         /// Last used split by field
         /// </summary>
         public static SplitBy LastSplitBy = SplitBy.ECO;
@@ -55,6 +60,11 @@ namespace ChessForge
             SetSplitBySelection(LastSplitBy);
             SetDefaultCriterionButton(LastSplitBy);
             SetActiveCriterionButton(LastSplitBy);
+
+            if (AppState.Workbook.Chapters.Count <= 1)
+            {
+                UiCbDistribByEco.IsEnabled = false;
+            }
         }
 
         /// <summary>
@@ -84,6 +94,58 @@ namespace ChessForge
                     UiGbDateCrit.Visibility = Visibility.Collapsed;
                     UiGbRoundCrit.Visibility = Visibility.Visible;
                     break;
+            }
+        }
+
+        /// <summary>
+        /// Retrieves the current SplitBy selection.
+        /// </summary>
+        /// <returns></returns>
+        private SplitBy CurrentSplitBy()
+        {
+            SplitBy splitBy;
+
+            if (UiRbSplitByEco.IsChecked == true)
+            {
+                splitBy = SplitBy.ECO;
+            }
+            else if (UiRbSplitByDate.IsChecked == true)
+            {
+                splitBy = SplitBy.DATE;
+            }
+            else
+            {
+                splitBy = SplitBy.ROUND;
+            }
+
+            return splitBy;
+        }
+
+        /// <summary>
+        /// Show or hide Split by and Spli Criteria group boxes
+        /// in response to the chnaging state of the Move Games check box
+        /// </summary>
+        /// <param name="show"></param>
+        private void ShowSplitByGroups(bool show)
+        {
+            if (show)
+            {
+                SetSplitBySelection(CurrentSplitBy());
+                UiGbSplitBy.Foreground = Brushes.Black;
+                UiRbSplitByDate.Visibility = Visibility.Visible;
+                UiRbSplitByEco.Visibility = Visibility.Visible;
+                UiRbSplitByRound.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                UiGbEcoCrit.Visibility = Visibility.Hidden;
+                UiGbDateCrit.Visibility = Visibility.Hidden;
+                UiGbRoundCrit.Visibility = Visibility.Visible;
+
+                UiGbSplitBy.Foreground = Brushes.LightGray;
+                UiRbSplitByDate.Visibility = Visibility.Hidden;
+                UiRbSplitByEco.Visibility = Visibility.Hidden;
+                UiRbSplitByRound.Visibility = Visibility.Hidden;
             }
         }
 
@@ -129,7 +191,7 @@ namespace ChessForge
                     UiRbCritYear.IsChecked = true;
                     break;
                 case SplitBy.DATE:
-                    UiRbCritAtoE.IsChecked= true;
+                    UiRbCritAtoE.IsChecked = true;
                     break;
                 case SplitBy.ROUND:
                     UiRbCritYear.IsChecked = true;
@@ -175,6 +237,8 @@ namespace ChessForge
         /// <param name="e"></param>
         private void UiBtnOk_Click(object sender, RoutedEventArgs e)
         {
+            MoveToChaptersPerECO = UiCbDistribByEco.IsChecked == true;
+
             if (UiRbSplitByEco.IsChecked == true)
             {
                 LastSplitBy = SplitBy.ECO;
@@ -212,6 +276,7 @@ namespace ChessForge
                 LastSplitBy = SplitBy.ROUND;
             }
 
+
             DialogResult = true;
         }
 
@@ -223,6 +288,26 @@ namespace ChessForge
         private void UiBtnHelp_Click(object sender, RoutedEventArgs e)
         {
             System.Diagnostics.Process.Start("https://github.com/czbar/ChessForge/wiki/Split-Chapter-Dialog");
+        }
+
+        /// <summary>
+        /// The ditribute by ECO box was checked.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void UiCbDistribByEco_Checked(object sender, RoutedEventArgs e)
+        {
+            ShowSplitByGroups(false);
+        }
+
+        /// <summary>
+        /// The distribute by ECO box was unchecked.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void UiCbDistribByEco_Unchecked(object sender, RoutedEventArgs e)
+        {
+            ShowSplitByGroups(true);
         }
     }
 }
