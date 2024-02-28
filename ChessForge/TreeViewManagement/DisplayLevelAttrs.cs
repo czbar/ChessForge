59284af9b03ac4efb4a91242c72311330428164f
czbar <czbar@users.noreply.github.com>
2024-02-28 21:13:21 +1100

    Implemented AdjustParagraphMargin()

--- ChessForge/TreeViewManagement/DisplayLevelAttrs.cs
+++ ChessForge/TreeViewManagement/DisplayLevelAttrs.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows;
+using System.Windows.Documents;
 using System.Windows.Media;
 
 namespace ChessForge
@@ -11,6 +13,9 @@ namespace ChessForge
     /// </summary>
     public class DisplayLevelAttrs
     {
+        // additional margin to add to the top/bottom for the first/last paragraph at a given level
+        private static int EXTRA_MARGIN = 10;
+
         /// <summary>
         /// Color for the last node at the given level. 
         /// </summary>
@@ -42,6 +47,24 @@ namespace ChessForge
             }
 
             return brush;
+        }
+
+        /// <summary>
+        /// Increases the top or bottom margin of a paragraph.
+        /// </summary>
+        /// <param name="para"></param>
+        /// <param name="topBottom"></param>
+        public static void AdjustParagraphMargin(Paragraph para, bool topBottom)
+        {
+            Thickness margin = para.Margin;
+            if (topBottom)
+            {
+                para.Margin = new Thickness(margin.Left, margin.Top + EXTRA_MARGIN, margin.Right, margin.Bottom);
+            }
+            else
+            {
+                para.Margin = new Thickness(margin.Left, margin.Top, margin.Right, margin.Bottom + EXTRA_MARGIN);
+            }
         }
     }
 }
