b9fe20cfa22a55f5965bbdde666015555b5c26a9
czbar <czbar@users.noreply.github.com>
2024-02-29 15:10:38 +1100

    Implemented CreateStudyParagraph()

--- ChessForge/TreeViewManagement/DisplayLevelAttrs.cs
+++ ChessForge/TreeViewManagement/DisplayLevelAttrs.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ChessPosition;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
@@ -13,19 +14,52 @@ namespace ChessForge
     /// </summary>
     public class DisplayLevelAttrs
     {
-        // additional margin to add to the top/bottom for the first/last paragraph at a given level
-        private static int EXTRA_MARGIN = 10;
+        /// <summary>
+        /// Extra margin to add to the top/bottom for the first/last paragraph at a given level
+        /// </summary>
+        public static int EXTRA_MARGIN = 10;
+
+        /// <summary>
+        /// Creates a paragraph for the main Study View area.
+        /// </summary>
+        /// <param name="displayLevel"></param>
+        /// <param name="topMarginExtra"></param>
+        /// <param name="bottomMarginExtra"></param>
+        /// <returns></returns>
+        public static Paragraph CreateStudyParagraph(int displayLevel, int topMarginExtra = 0, int bottomMarginExtra = 0)
+        {
+            // figure out the font size.
+            int fontSize = Constants.BASE_FIXED_FONT_SIZE;
+            if (!Configuration.UseFixedFont)
+            {
+                fontSize = GetVariableFontSizeForLevel(displayLevel);
+            }
+            fontSize = GuiUtilities.AdjustFontSize(fontSize);
+
+            // create the paragraph.
+            Paragraph para = new Paragraph
+            {
+                Margin = new Thickness(20 * displayLevel, topMarginExtra, 0, 5 + bottomMarginExtra),
+                FontSize = fontSize,
+                FontWeight = displayLevel == 0 ? FontWeights.DemiBold : FontWeights.Normal,
+                TextAlignment = TextAlignment.Left,
+                Foreground = Brushes.Black
+            };
+
+            return para;
+        }
 
         /// <summary>
         /// Color for the last node at the given level. 
         /// </summary>
         /// <param name="level"></param>
+        /// <param name="levelGroup"></param>
         /// <returns></returns>
-        public static Brush GetBrushForLastMove(int level)
+        public static Brush GetBrushForLastMove(int level, int levelGroup)
         {
             Brush brush;
 
-            int modLevel = level % 4;
+            int modLevel = (level + levelGroup) % 4;
 
             switch (modLevel)
             {
@@ -33,13 +67,13 @@ namespace ChessForge
                     brush = Brushes.Blue;
                     break;
                 case 1:
-                    brush = Brushes.Green;
+                    brush = Brushes.DarkGreen;
                     break;
                 case 2:
                     brush = Brushes.Purple;
                     break;
                 case 3:
-                    brush = Brushes.Chocolate;
+                    brush = Brushes.Firebrick;
                     break;
                 default:
                     brush = Brushes.Black;
@@ -65,6 +99,36 @@ namespace ChessForge
             {
                 para.Margin = new Thickness(margin.Left, margin.Top, margin.Right, margin.Bottom + EXTRA_MARGIN);
             }
+        }
+
+        /// <summary>
+        /// Returns the font size for a given display level
+        /// (if using variable font size).
+        /// </summary>
+        /// <param name="displayLevel"></param>
+        /// <returns></returns>
+        private static int GetVariableFontSizeForLevel(int displayLevel)
+        {
+            int fontSize;
+
+            if (displayLevel <= 1)
+            {
+                fontSize = 16;
+            }
+            else if (displayLevel == 2)
+            {
+                fontSize = 14;
+            }
+            else if (displayLevel <= 4)
+            {
+                fontSize = 14;
+            }
+            else
+            {
+                fontSize = 11;
+            }
+
+            return fontSize;
         }
     }
 }
