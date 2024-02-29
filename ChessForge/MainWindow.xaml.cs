b711ac942422d86a947fcf652129d7acf3bd0a22
czbar <czbar@users.noreply.github.com>
2024-02-29 23:02:50 +1100

    Renamed DisplayManager to LineManager

--- ChessForge/MainWindow.xaml.cs
+++ ChessForge/MainWindow.xaml.cs
@@ -2088,7 +2088,7 @@ namespace ChessForge
                 AppLog.DumpVariationTree(DebugUtils.BuildLogFileName(App.AppPath, "wktree", distinct), ActiveVariationTree);
                 if (_studyTreeView != null)
                 {
-                    AppLog.DumpLineSectorTree(DebugUtils.BuildLogFileName(App.AppPath, "lstree", distinct), _studyTreeView.DisplayManager.LineSectors);
+                    AppLog.DumpLineSectorTree(DebugUtils.BuildLogFileName(App.AppPath, "lstree", distinct), _studyTreeView.LineManager.LineSectors);
                 }
                 DebugDumps.DumpStatesAndTimers(DebugUtils.BuildLogFileName(App.AppPath, "timest", distinct));
             }
