7810530ce8772d4e0105f96cecebc2547e6c88a3
czbar <czbar@users.noreply.github.com>
2024-03-01 19:03:30 +1100

    Update

--- ChessForge/Properties/Resources.Designer.cs
+++ ChessForge/Properties/Resources.Designer.cs
@@ -3968,6 +3968,15 @@ namespace ChessForge.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Positions not found. Edit search?.
+        /// </summary>
+        public static string MsgEditPositionSearch {
+            get {
+                return ResourceManager.GetString("MsgEditPositionSearch", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Enter valid Url or leave empty to delete link. .
         /// </summary>
         public static string MsgNeedValidUrlOrEmpty {
@@ -4063,6 +4072,15 @@ namespace ChessForge.Properties {
         public static string MsgThisOverwritesStudy {
             get {
                 return ResourceManager.GetString("MsgThisOverwritesStudy", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Position Search.
+        /// </summary>
+        public static string MsgTitlePositionSearch {
+            get {
+                return ResourceManager.GetString("MsgTitlePositionSearch", resourceCulture);
             }
         }
         
