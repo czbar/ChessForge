34304e0e9ef473be015cbb68a24c0fd829b073fe
czbar <czbar@users.noreply.github.com>
2024-03-22 20:49:01 +1100

    Created SelectLibraryDialog

---
+++ ChessForge/SelectLibraryDialog.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ChessForge
+{
+    /// <summary>
+    /// Interaction logic for SelectLibraryDialog.xaml
+    /// </summary>
+    public partial class SelectLibraryDialog : Window
+    {
+        public SelectLibraryDialog()
+        {
+            InitializeComponent();
+
+            UiBtnAddLibrary.Content = Properties.Resources.Add + "...";
+        }
+    }
+}
