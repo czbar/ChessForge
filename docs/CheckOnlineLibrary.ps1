f1aca36b37b6e825251c5b3047df65a25feeefe2
czbar <czbar@users.noreply.github.com>
2024-03-27 23:37:58 +1100

    Copied WebSite to docs

---
+++ docs/CheckOnlineLibrary.ps1
@@ -0,0 +1,4 @@
+
+# Run the utility checking if the library subdirectories have content declared in the library.txt file 
+../Utilities/OnlineLibraryCheck/bin/Debug/OnlineLibraryCheck.exe
+Read-Host -Prompt "Press Enter to exit"
