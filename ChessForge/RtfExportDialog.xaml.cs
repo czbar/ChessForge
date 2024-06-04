17d99c2d684315e02c649e028b6f8425f8da732d
czbar <czbar@users.noreply.github.com>
2024-06-04 21:06:10 +1000

    Created RtfExportDialog

---
+++ ChessForge/RtfExportDialog.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace ChessForge
+{
+    /// <summary>
+    /// Interaction logic for RtfExportDialog.xaml
+    /// </summary>
+    public partial class RtfExportDialog : Window
+    {
+        /// <summary>
+        /// Scope of export selected by the user.
+        /// </summary>
+        public static PrintScope Scope = PrintScope.ARTICLE;
+        
+        /// <summary>
+        /// Initializes the data.
+        /// </summary>
+        public RtfExportDialog()
+        {
+            InitializeComponent();
+
+            switch (Scope)
+            {
+                case PrintScope.ARTICLE:
+                    UiRbCurrentItem.IsChecked = true;
+                    break;
+                case PrintScope.CHAPTER:
+                    UiRbCurrentChapter.IsChecked = true;
+                    break;
+                case PrintScope.WORKBOOK:
+                    UiRbWorkbook.IsChecked = true;
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Proceed with the export as configured.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void UiBtnOk_Click(object sender, RoutedEventArgs e)
+        {
+            Chapter chapter = AppState.ActiveChapter;
+            Article article = null;
+
+            if (UiRbCurrentItem.IsChecked == true)
+            {
+                Scope = PrintScope.ARTICLE;
+                if (chapter != null)
+                {
+                    article = chapter.ActiveArticle;
+                }
+            }
+            else if (UiRbCurrentChapter.IsChecked == true)
+            {
+                Scope = PrintScope.CHAPTER;
+            }
+            else if (UiRbWorkbook.IsChecked == true)
+            {
+                Scope = PrintScope.WORKBOOK;
+            }
+
+            Mouse.SetCursor(Cursors.Wait);
+            RtfWriter.WriteRtf(Scope, chapter, article);
+            Mouse.SetCursor(Cursors.Arrow);
+            
+            DialogResult = true; 
+        }
+
+    }
+}
