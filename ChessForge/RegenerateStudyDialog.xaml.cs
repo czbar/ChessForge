1c2f5ae094ecce3cfd648cad35ac70d40725de14
czbar <czbar@users.noreply.github.com>
2024-06-11 19:51:43 +1000

    Created RegenerateStudyDialog

---
+++ ChessForge/RegenerateStudyDialog.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System.Windows;
+
+namespace ChessForge
+{
+    /// <summary>
+    /// Interaction logic for ManageChapterDialog.xaml
+    /// </summary>
+    public partial class RegenerateStudyDialog : Window
+    {
+        /// <summary>
+        /// Whether the Sort command should be applied to all chapters.
+        /// </summary>
+        public bool ApplyToAllChapters = false;
+
+        /// <summary>
+        /// Constructors. Initializes the list box values.
+        /// </summary>
+        public RegenerateStudyDialog(Chapter chapter)
+        {
+            InitializeComponent();
+
+            UiLabelChapterTitle.Content = Properties.Resources.Chapter + ": " + chapter.GetTitle();
+
+            if (Configuration.AutogenTreeDepth == 0)
+            {
+                UiTbLastTreeMoveNo.Text = "";
+            }
+            else
+            {
+                UiTbLastTreeMoveNo.Text = Configuration.AutogenTreeDepth.ToString();
+            }
+        }
+
+        /// <summary>
+        /// Collect the states and exit.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void UiBtnOk_Click(object sender, RoutedEventArgs e)
+        {
+            uint treeDepth;
+            if (!uint.TryParse(UiTbLastTreeMoveNo.Text, out treeDepth))
+            {
+                treeDepth = 0;
+            }
+            Configuration.AutogenTreeDepth = treeDepth;
+
+            ApplyToAllChapters = UiCbAllChapters.IsChecked == true;
+
+            DialogResult = true;
+        }
+
+        /// <summary>
+        /// Links to the relevant Wiki page.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void UiBtnHelp_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/czbar/ChessForge/wiki/Managing-Chapter");
+        }
+    }
+
+}
