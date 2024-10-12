10e12f373ddfa3d8afe86f10c788a8c9e7e29031
czbar <czbar@users.noreply.github.com>
2024-10-12 11:57:07 +0200

    Cleaned up usings

--- ChessForge/ActiveLineManager.cs
+++ ChessForge/ActiveLineManager.cs
@@ -1,11 +1,9 @@
-﻿using System;
+﻿using ChessPosition;
+using GameTree;
+using System;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
 using System.Windows.Controls;
 using System.Windows.Input;
-using ChessPosition;
-using GameTree;
 
 namespace ChessForge
 {
