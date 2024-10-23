1bad6e556cd5d7286ded6800fac7d2599aacd488
czbar <czbar@users.noreply.github.com>
2024-10-23 02:28:21 +0200

    Fixed restoring the selected node

--- ChessPosition/GameTree/EditOperationsManager.cs
+++ ChessPosition/GameTree/EditOperationsManager.cs
@@ -75,15 +75,19 @@ namespace GameTree
                         break;
                     case EditOperation.EditType.INSERT_DIAGRAM:
                         _owningTree.UndoInsertDiagram(op.Node);
+                        selectedNodeId = op.Node.NodeId;
                         break;
                     case EditOperation.EditType.DELETE_DIAGRAM:
                         _owningTree.UndoDeleteDiagram(op.Node);
+                        selectedNodeId = op.Node.NodeId;
                         break;
                     case EditOperation.EditType.SWAP_DIAGRAM_COMMENT:
                         _owningTree.UndoSwapDiagramComment(op.Node);
+                        selectedNodeId = op.Node.NodeId;
                         break;
                     case EditOperation.EditType.MARK_THUMBNAIL:
                         _owningTree.UndoMarkThumbnail(op.OpData_1, op.OpData_2);
+                        selectedNodeId = op.Node.NodeId;
                         break;
                 }
             }
