5fba7e96bb3f0be562b0ce3ed258c1bf4acb7f66
czbar <czbar@users.noreply.github.com>
2024-11-06 22:35:02 +0100

    Added accessors

--- ChessPosition/GameTree/Operation.cs
+++ ChessPosition/GameTree/Operation.cs
@@ -21,17 +21,25 @@ namespace GameTree
         /// <summary>
         /// This operation's creation time.
         /// </summary>
-        public long Timestamp {get { return _timestamp; } }
+        public long Timestamp { get { return _timestamp; } }
 
         /// <summary>
         /// Returns Operation's data.
         /// </summary>
-        public object OpData_1 { get { return _opData_1; } }
+        public object OpData_1
+        {
+            get { return _opData_1; }
+            set { _opData_1 = value; }
+        }
 
         /// <summary>
         /// Returns Operation's data.
         /// </summary>
-        public object OpData_2 { get { return _opData_2; } }
+        public object OpData_2
+        {
+            get { return _opData_2; }
+            set { _opData_2 = value; }
+        }
 
         /// <summary>
         /// Operation's data
