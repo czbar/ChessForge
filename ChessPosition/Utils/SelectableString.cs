30fc5ddc273a67c502a9547c8217db7300f934ae
czbar <czbar@users.noreply.github.com>
2025-01-25 18:09:59 +1100

    Create SelectableString.cs

---
+++ ChessPosition/Utils/SelectableString.cs
@@ -0,0 +1,29 @@
+﻿namespace ChessPosition
+{
+    /// <summary>
+    /// Encapsulates a string and a boolean value.
+    /// </summary>
+    public class SelectableString
+    {
+        /// <summary>
+        /// The string.
+        /// </summary>
+        public string Text { get; set; }
+
+        /// <summary>
+        /// The boolean value.
+        /// </summary>
+        public bool IsSelected { get; set; }
+
+        /// <summary>
+        /// Creates a new instance of SelectableString.
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="isSelected"></param>
+        public SelectableString(string text, bool isSelected)
+        {
+            Text = text;
+            IsSelected = isSelected;
+        }
+    }
+}
