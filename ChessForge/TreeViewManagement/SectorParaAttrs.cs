1b6c042e9eae34a37e3955ca33472d45eeaa6f9d
czbar <czbar@users.noreply.github.com>
2025-02-02 19:28:41 +1100

    Created SectorParaAttrs.cs

---
+++ ChessForge/TreeViewManagement/SectorParaAttrs.cs
@@ -0,0 +1,55 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace ChessForge.TreeViewManagement
+{
+    /// <summary>
+    /// Attributes of a paragraph represented by the Sector.
+    /// </summary>
+    public class SectorParaAttrs
+    {
+        /// <summary>
+        /// Font size of the paragraph.
+        /// </summary>
+        public double FontSize { get; set; }
+
+        /// <summary>
+        /// Font weight of the paragraph.
+        /// </summary>
+        public FontWeight FontWeight { get; set; }
+
+        /// <summary>
+        /// Text alignment of the paragraph.
+        /// </summary>
+        public TextAlignment TextAlignment { get; set; }
+
+        /// <summary>
+        /// Foreground color of the paragraph.
+        /// </summary>
+        public Brush Foreground { get; set; }
+        
+        public string Background { get; set; }
+
+        /// <summary>
+        /// Margin of the paragraph.
+        /// </summary>
+        public Thickness Margin { get; set; }
+
+        /// <summary>
+        /// Extra margin to add to the top for the first/last paragraph at a given level
+        /// </summary>
+        public int TopMarginExtra { get; set; }
+
+        /// <summary>
+        /// Extra margin to add to the bottom for the first/last paragraph at a given level
+        /// </summary>
+        public int BottomMarginExtra { get; set; }
+
+        /// <summary>
+        /// Creates a new instance of SectorParaAttrs.
+        /// </summary>
+        public SectorParaAttrs()
+        {
+        }
+    }
+}
