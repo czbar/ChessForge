d6fbf110b55cd8e748ff69e9a679aed67865e4e3
czbar <czbar@users.noreply.github.com>
2025-02-02 19:29:13 +1100

    Create VariationTreeViewUtils.cs

---
+++ ChessForge/VariationTreeViewUtils.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessForge
+{
+    public class VariationTreeViewUtils
+    {
+        /// <summary>
+        /// Safe accessor to the chapter's variation index depth.
+        /// </summary>
+        /// <returns></returns>
+        public static int VariationIndexDepth
+        {
+            get { return AppState.ActiveChapter == null ? Configuration.DefaultIndexDepth : AppState.ActiveChapter.VariationIndexDepth.Value; }
+        }
+
+    }
+}
