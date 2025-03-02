7e6296422fbff0990e11036f044643d6630e11fa
czbar <czbar@users.noreply.github.com>
2025-03-02 17:11:27 +1100

    Implemented HasCollapsedAncestor()

--- ChessForge/VariationTreeViewUtils.cs
+++ ChessForge/VariationTreeViewUtils.cs
@@ -1,13 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using GameTree;
 
 namespace ChessForge
 {
     public class VariationTreeViewUtils
     {
+        /// <summary>
+        /// Finds out if the move has a collapsed ancestor
+        /// </summary>
+        /// <param name="nd"></param>
+        /// <returns>true if the uncollapse was required</returns>
+        public static bool HasCollapsedAncestor(TreeNode nd)
+        {
+            bool result = false;
+
+            if (nd != null)
+            {
+                while (nd.Parent != null)
+                {
+                    if (nd.Parent.IsCollapsed)
+                    {
+                        result = true;
+                        break;
+                    }
+                    nd = nd.Parent;
+                }
+            }
+
+            return result;
+        }
+
         /// <summary>
         /// Safe accessor to the chapter's variation index depth.
         /// </summary>
