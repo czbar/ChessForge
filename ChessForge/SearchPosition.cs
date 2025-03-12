be1230bbbd0a2e20c3fb86bae9de948f929acdf7
czbar <czbar@users.noreply.github.com>
2025-03-12 21:38:38 +1100

    Implemented FilterPositions()

--- ChessForge/SearchPosition.cs
+++ ChessForge/SearchPosition.cs
@@ -58,6 +58,67 @@ namespace ChessForge
         }
 
         /// <summary>
+        /// Finds nodes matching the passed "filter" position.
+        /// </summary>
+        /// <param name="tree"></param>
+        /// <param name="filter"></param>
+        /// <param name="checkSideToMove"></param>
+        /// <param name="checkEnpassant"></param>
+        /// <param name="checkCastleRights"></param>
+        /// <returns></returns>
+        public static List<TreeNode> FilterPositions(VariationTree tree, BoardPosition filter, bool checkSideToMove, bool checkEnpassant, bool checkCastleRights)
+        {
+            List<TreeNode> nodeList = new List<TreeNode>();
+
+            foreach (TreeNode nd in tree.Nodes)
+            {
+                if (CompareByteArrays(filter.Board, nd.Position.Board))
+                {
+                    if ((!checkEnpassant || IsSameEnpassantPossibilities(filter, nd.Position))
+                        && (!checkSideToMove || filter.ColorToMove == nd.Position.ColorToMove)
+                        && (!checkCastleRights || filter.CastlingRights == nd.Position.CastlingRights))
+                    {
+                        if (nodeList == null)
+                        {
+                            nodeList = new List<TreeNode>();
+                        }
+                        nodeList.Add(nd);
+                    }
+                }
+            }
+
+            return nodeList;
+        }
+
+
+        /// <summary>
+        /// Compares two byte arrays representing chess positions.
+        /// The position argument is the one that is being checked.
+        /// The filter argument is the one that is used as a filter.
+        /// If the filter has a non-zero value in a square, the position must have the same piece in that square.
+        /// If the filter has a 0xFF value in a square, the position must have an empty square in that position.
+        /// </summary>
+        /// <param name="filter"></param>
+        /// <param name="position"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException"></exception>
+        private static bool CompareByteArrays(byte[,] filter, byte[,] position)
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    if (filter[i, j] != 0 && filter[i, j] != position[i, j]
+                        || filter[i, j] == 0xFF && position[i, j] != 0)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        /// <summary>
         /// Checks if possible enpassant moves are same in both positions. 
         /// </summary>
         /// <param name="pos1"></param>
