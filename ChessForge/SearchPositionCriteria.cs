72c96f366d8690908b547121be29b96f17fd7317
czbar <czbar@users.noreply.github.com>
2025-03-17 20:51:49 +1100

    Update SearchPositionCriteria.cs

--- ChessForge/SearchPositionCriteria.cs
+++ ChessForge/SearchPositionCriteria.cs
@@ -3,14 +3,34 @@ using GameTree;
 
 namespace ChessForge
 {
-    class SearchPositionCriteria
+    public class SearchPositionCriteria
     {
+        /// <summary>
+        /// The position to search for.
+        /// </summary>
+        private TreeNode _searchNode;
+
+        /// <summary>
+        /// The position to search for.
+        /// </summary>
+        private BoardPosition _searchPosition;
+
+        /// <summary>
+        /// Whether to show a message if no position was found.
+        /// </summary>
+        private bool _reportNotFound = true;
+
+        /// <summary>
+        /// Whether to return only one matching position.
+        /// </summary>
+        private bool _findFirstOnly = false;
+
         /// <summary>
         /// Constructor
         /// </summary>
         public SearchPositionCriteria(TreeNode node)
         {
-            SearchPosition = node.Position;
+            _searchNode = node;
         }
 
         /// <summary>
@@ -23,9 +43,53 @@ namespace ChessForge
         }
 
         /// <summary>
+        /// Find Position mode.
+        /// We only use the FIND_AND_REPORT mode.
+        /// The other mode CHECK_IF_ANY was invented to search dynamically for identical positions
+        /// while editing. This proved to be too slow and was abandoned.
+        /// </summary>
+        public FindIdenticalPositions.Mode FindMode { get; set; }
+
+        /// <summary>
+        /// The node whose position to search for.
+        /// </summary>
+        public TreeNode SearchNode
+        {
+            get => _searchNode;
+            set => _searchNode = value;
+        }
+
+        /// <summary>
+        /// Whether to return only one matching position.
+        /// </summary>
+        public bool FindFirstOnly
+        {
+            get => _findFirstOnly;
+            set => _findFirstOnly = value;
+        }
+
+        /// <summary>
         /// The position to search for.
         /// </summary>
-        public BoardPosition SearchPosition { get; set; }
+        public BoardPosition SearchPosition
+        {
+            get
+            {
+                if (_searchPosition != null)
+                {
+                    return _searchPosition;
+                }
+                else if (_searchNode != null)
+                {
+                    return _searchNode.Position;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            set => _searchPosition = value;
+        }
 
         /// <summary>
         /// Whether to perform a partial search.
@@ -35,7 +99,11 @@ namespace ChessForge
         /// <summary>
         /// Whether to show a message if no position was found.
         /// </summary>
-        public bool ReportNoFind { get; set; }
+        public bool ReportNoFind
+        {
+            get => _reportNotFound;
+            set => _reportNotFound = value;
+        }
 
         /// <summary>
         /// Whether to exclude the passed node from the search.
@@ -46,11 +114,20 @@ namespace ChessForge
         /// Whether to check the dynamic properties (castling rights, e.p. whose move).
         /// </summary>
         /// <param name="check"></param>
-        public void CheckDynamicAttrs(bool check)
+        public void SetCheckDynamicAttrs(bool check)
         {
             CheckSideToMove = check;
             CheckEnpassant = check;
             CheckCastleRights = check;
+        }
+
+        /// <summary>
+        /// Whether to check the dynamic properties (castling rights, e.p. whose move).
+        /// </summary>
+        /// <returns></returns>
+        public bool IsCheckDynamicAttrs()
+        {
+            return CheckSideToMove && CheckEnpassant && CheckCastleRights;
         }
 
         /// <summary>
