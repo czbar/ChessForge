4974f7ef49e7a494fc6b03e17fa6d086c67c5330
czbar <czbar@users.noreply.github.com>
2025-03-25 21:45:50 +0100

    Created new

---
+++ ChessForge/DiagramImageSizeDialog.xaml.cs
@@ -0,0 +1,51 @@
+﻿using ChessPosition;
+using System;
+using System.Windows;
+
+namespace ChessForge
+{
+    /// <summary>
+    /// Interaction logic for DiagramImageSizeDialog.xaml
+    /// </summary>
+    public partial class DiagramImageSize : Window
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DiagramImageSize"/> class.
+        /// </summary>
+        public DiagramImageSize()
+        {
+            InitializeComponent();
+
+            Title = TextUtils.RemoveTrailingElipsis(Title);
+            UiTbSideSize.Text = Configuration.DiagramImageSideSize.ToString();
+            UiCbDoNotAskAgain.IsChecked = Configuration.DoNotAskDiagramImageSideSize;
+        }
+
+        /// <summary>
+        /// Handles the click event for the save button.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void UiBtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            int.TryParse(UiTbSideSize.Text, out int sideSize);
+            sideSize = Math.Max(sideSize, 120);
+            sideSize = Math.Min(sideSize, 480);
+            
+            Configuration.DiagramImageSideSize = sideSize;
+            Configuration.DoNotAskDiagramImageSideSize = UiCbDoNotAskAgain.IsChecked == true;
+            
+            DialogResult = true;
+        }
+
+        /// <summary>
+        /// Handles the click event for the cancel button.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void UiBtnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+    }
+}
---
+++ ChessForge/SaveDiagram.cs
@@ -0,0 +1,169 @@
+﻿using GameTree;
+using Microsoft.Win32;
+using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows;
+
+namespace ChessForge
+{
+    public class SaveDiagram
+    {
+        /// <summary>
+        /// Asks the user to select to field to save the diagram to.
+        /// Once selected, generates a png image for the passed position and saves it.
+        /// </summary>
+        /// <param name="node"></param>
+        /// <param name="isFlipped"></param>
+        /// <returns></returns>
+        public static bool SaveAsImage(TreeNode node, bool isFlipped)
+        {
+            bool result = true;
+
+            try
+            {
+                // invokes the SaveFileDialog to let the user choose the file name
+                string filePath = GetUserSelectedFileName();
+
+                if (!string.IsNullOrEmpty(filePath))
+                {
+                    Configuration.LastPngFile = filePath;
+
+                    int sideSize = GetUserSelectedSideSize(out bool cancel);
+
+                    if (!cancel)
+                    {
+                        sideSize = VerifySideSize(sideSize);
+                        Configuration.DiagramImageSideSize = sideSize;
+                        
+                        // at this point we have all we need in the Configuration class
+                        PositionImageGenerator.SaveDiagramAsImage(node, isFlipped, Configuration.LastPngFile, Configuration.DiagramImageSideSize);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, Properties.Resources.Error, MessageBoxButton.OK, MessageBoxImage.Error);
+                result = false;
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Verifies that the side size is within the allowed range.
+        /// </summary>
+        /// <param name="sideSize"></param>
+        /// <returns></returns>
+        public static int VerifySideSize(int sideSize)
+        {
+            sideSize = Math.Max(sideSize, 120);
+            sideSize = Math.Min(sideSize, 480);
+            return sideSize;
+        }
+
+        /// <summary>
+        /// Asks the user to select a file name to save the diagram to.
+        /// The dialog will be pre-populated with the last file name used.
+        /// If the file with that name already exisist, the initial value will be the same with a number appended.
+        /// </summary>
+        /// <returns></returns>
+        private static string GetUserSelectedFileName()
+        {
+            string filePath = Configuration.LastPngFile;
+
+            if (!string.IsNullOrEmpty(filePath) && File.Exists(filePath))
+            {
+                string directory = Path.GetDirectoryName(filePath);
+                string fileName = Path.GetFileNameWithoutExtension(filePath);
+                string baseName = ExtractBaseName(fileName);
+                fileName = GetNextAvailableFileName(directory, baseName);
+                filePath = Path.Combine(directory, fileName);
+            }
+
+            SaveFileDialog saveDlg = new SaveFileDialog
+            {
+                Filter = Properties.Resources.ImageFiles + " (*.png)|*.png"
+            };
+
+            saveDlg.OverwritePrompt = true;
+            saveDlg.Title = " " + Properties.Resources.SaveDiagramAsImage;
+
+            saveDlg.FileName = filePath;
+
+            if (saveDlg.ShowDialog() == true)
+            {
+                return saveDlg.FileName;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Asks the user to select the size of the diagram image
+        /// and retruns the value.
+        /// -1 indicates that the user cancelled the dialog.
+        /// </summary>
+        /// <returns></returns>
+        private static int GetUserSelectedSideSize(out bool cancel)
+        {
+            cancel = false;
+
+            if (!Configuration.DoNotAskDiagramImageSideSize)
+            {
+                DiagramImageSize dlg = new DiagramImageSize();
+                GuiUtilities.PositionDialog(dlg, AppState.MainWin, 100);
+
+                if (dlg.ShowDialog() != true)
+                {
+                    cancel = true;
+                }
+            }
+
+            return Configuration.DiagramImageSideSize;
+        }
+
+        /// <summary>
+        /// Extracts the base name from a file name.
+        /// The base name is the file name without the trailing number.
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
+        private static string ExtractBaseName(string fileName)
+        {
+            Match match = Regex.Match(fileName, @"^(.*?)(?:\s+\d+)?$", RegexOptions.IgnoreCase);
+            return match.Success ? match.Groups[1].Value : fileName;
+        }
+
+        /// <summary>
+        /// Returns the next available file name in the directory
+        /// in the format of base name followed by a number.
+        /// </summary>
+        /// <param name="directoryPath"></param>
+        /// <param name="baseName"></param>
+        /// <returns></returns>
+        private static string GetNextAvailableFileName(string directoryPath, string baseName)
+        {
+            string pattern = $"^{Regex.Escape(baseName)}\\s*(\\d+)\\.png$";
+            Regex regex = new Regex(pattern);
+
+            var existingNumbers = Directory.EnumerateFiles(directoryPath, "*.png")
+                .Select(Path.GetFileName)
+                .Select(fileName => regex.Match(fileName))
+                .Where(match => match.Success)
+                .Select(match => int.Parse(match.Groups[1].Value))
+                .ToHashSet();
+
+            int M = 1;
+            while (existingNumbers.Contains(M))
+            {
+                M++;
+            }
+
+            return $"{baseName} {M}.png";
+        }
+    }
+}
