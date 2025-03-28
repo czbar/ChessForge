baaee1536a8b40a492cb90adeb67404542b69a9b
czbar <czbar@users.noreply.github.com>
2025-03-28 21:17:46 +0100

    Added dpi=96

--- Utilities/LichessSvg/ConvertToPgn.bat
+++ Utilities/LichessSvg/ConvertToPgn.bat
@@ -1,15 +1,15 @@
-"C:\Program Files\Inkscape\bin\inkscape" l.svg --export-filename=WhiteCircle_%1.png --export-width=%1 --export-height=%1
-"C:\Program Files\Inkscape\bin\inkscape" r.svg --export-filename=WhiteRook_%1.png --export-width=%1 --export-height=%1
-"C:\Program Files\Inkscape\bin\inkscape" n.svg --export-filename=WhiteKnight_%1.png --export-width=%1 --export-height=%1
-"C:\Program Files\Inkscape\bin\inkscape" b.svg --export-filename=WhiteBishop_%1.png --export-width=%1 --export-height=%1
-"C:\Program Files\Inkscape\bin\inkscape" q.svg --export-filename=WhiteQueen_%1.png --export-width=%1 --export-height=%1
-"C:\Program Files\Inkscape\bin\inkscape" k.svg --export-filename=WhiteKing_%1.png --export-width=%1 --export-height=%1
-"C:\Program Files\Inkscape\bin\inkscape" p.svg --export-filename=WhitePawn_%1.png --export-width=%1 --export-height=%1
+"C:\Program Files\Inkscape\bin\inkscape" l.svg --export-filename=l_%1.png --export-width=%1 --export-height=%1 --export-dpi=96
+"C:\Program Files\Inkscape\bin\inkscape" r.svg --export-filename=WhiteRook_%1.png --export-width=%1 --export-height=%1 --export-dpi=96
+"C:\Program Files\Inkscape\bin\inkscape" n.svg --export-filename=WhiteKnight_%1.png --export-width=%1 --export-height=%1 --export-dpi=96
+"C:\Program Files\Inkscape\bin\inkscape" b.svg --export-filename=WhiteBishop_%1.png --export-width=%1 --export-height=%1 --export-dpi=96
+"C:\Program Files\Inkscape\bin\inkscape" q.svg --export-filename=WhiteQueen_%1.png --export-width=%1 --export-height=%1 --export-dpi=96
+"C:\Program Files\Inkscape\bin\inkscape" k.svg --export-filename=WhiteKing_%1.png --export-width=%1 --export-height=%1 --export-dpi=96
+"C:\Program Files\Inkscape\bin\inkscape" p.svg --export-filename=WhitePawn_%1.png --export-width=%1 --export-height=%1 --export-dpi=96
 
-"C:\Program Files\Inkscape\bin\inkscape" l_b.svg --export-filename=BlackCircle_%1.png --export-width=%1 --export-height=%1
-"C:\Program Files\Inkscape\bin\inkscape" r_b.svg --export-filename=BlackRook_%1.png --export-width=%1 --export-height=%1
-"C:\Program Files\Inkscape\bin\inkscape" n_b.svg --export-filename=BlackKnight_%1.png --export-width=%1 --export-height=%1
-"C:\Program Files\Inkscape\bin\inkscape" b_b.svg --export-filename=BlackBishop_%1.png --export-width=%1 --export-height=%1
-"C:\Program Files\Inkscape\bin\inkscape" q_b.svg --export-filename=BlackQueen_%1.png --export-width=%1 --export-height=%1
-"C:\Program Files\Inkscape\bin\inkscape" k_b.svg --export-filename=BlackKing_%1.png --export-width=%1 --export-height=%1
-"C:\Program Files\Inkscape\bin\inkscape" p_b.svg --export-filename=BlackPawn_%1.png --export-width=%1 --export-height=%1
+"C:\Program Files\Inkscape\bin\inkscape" l_b.svg --export-filename=BlackCircle_%1.png --export-width=%1 --export-height=%1 --export-dpi=96
+"C:\Program Files\Inkscape\bin\inkscape" r_b.svg --export-filename=BlackRook_%1.png --export-width=%1 --export-height=%1 --export-dpi=96
+"C:\Program Files\Inkscape\bin\inkscape" n_b.svg --export-filename=BlackKnight_%1.png --export-width=%1 --export-height=%1 --export-dpi=96
+"C:\Program Files\Inkscape\bin\inkscape" b_b.svg --export-filename=BlackBishop_%1.png --export-width=%1 --export-height=%1 --export-dpi=96
+"C:\Program Files\Inkscape\bin\inkscape" q_b.svg --export-filename=BlackQueen_%1.png --export-width=%1 --export-height=%1 --export-dpi=96
+"C:\Program Files\Inkscape\bin\inkscape" k_b.svg --export-filename=BlackKing_%1.png --export-width=%1 --export-height=%1 --export-dpi=96
+"C:\Program Files\Inkscape\bin\inkscape" p_b.svg --export-filename=BlackPawn_%1.png --export-width=%1 --export-height=%1 --export-dpi=96
