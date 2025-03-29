0a160ee69c8f45303b94576ab9001a93dafd6ef1
czbar <czbar@users.noreply.github.com>
2025-03-29 12:57:00 +0100

    Color selection in the dialog

--- ChessForge/DiagramImageSizeDialog.xaml.cs
+++ ChessForge/DiagramImageSizeDialog.xaml.cs
@@ -10,6 +10,16 @@ namespace ChessForge
     public partial class DiagramImageSize : Window
     {
         /// <summary>
+        /// The selected color set for the diagram.
+        /// </summary>
+        private int _selectedColorSet = 1;
+
+        /// <summary>
+        /// The maximum number of color sets.
+        /// </summary>
+        private const int MAX_COLOR_SET = 6;
+
+        /// <summary>
         /// Initializes a new instance of the <see cref="DiagramImageSize"/> class.
         /// </summary>
         public DiagramImageSize()
@@ -21,6 +31,114 @@ namespace ChessForge
             UiCbDoNotAskAgain.IsChecked = Configuration.DoNotAskDiagramImageSize;
             UiLblMinMax.Content = "(" + Properties.Resources.Min + " " + Constants.MIN_DIAGRAM_SIZE 
                                     + " - " + Properties.Resources.Max + " " + Constants.MAX_DIAGRAM_SIZE + ")";
+            SelectColorSet();
+
+        }
+
+        /// <summary>
+        /// Selects the color set based on the configuration.
+        /// </summary>
+        private void SelectColorSet()
+        {
+            _selectedColorSet = Math.Min(MAX_COLOR_SET, Configuration.DiagramImageColors);
+            _selectedColorSet = Math.Max(1, _selectedColorSet);
+
+            switch (_selectedColorSet)
+            {
+                case 1:
+                    UiImgColors_1_MouseDown(null, null);
+                    break;
+                case 2:
+                    UiImgColors_2_MouseDown(null, null);
+                    break;
+                case 3:
+                    UiImgColors_3_MouseDown(null, null);
+                    break;
+                case 4:
+                    UiImgColors_4_MouseDown(null, null);
+                    break;
+                case 5:
+                    UiImgColors_5_MouseDown(null, null);
+                    break;
+                case 6:
+                    UiImgColors_6_MouseDown(null, null);
+                    break;
+                default:
+                    UiImgColors_1_MouseDown(null, null);
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Handles the click event for the color set buttons.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void UiImgColors_1_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            UiImgColorsSelector.Margin = new Thickness(UiImgColors_1.Margin.Left - 1, UiImgColors_1.Margin.Top - 1,
+                                                       UiImgColors_1.Margin.Right + 1, UiImgColors_1.Margin.Bottom + 1);
+            _selectedColorSet = 1;
+        }
+
+        /// <summary>
+        /// Handles the click event for the color set buttons.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void UiImgColors_2_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            UiImgColorsSelector.Margin = new Thickness(UiImgColors_2.Margin.Left - 1, UiImgColors_2.Margin.Top - 1,
+                                                       UiImgColors_2.Margin.Right + 1, UiImgColors_2.Margin.Bottom + 1);
+            _selectedColorSet = 2;
+        }
+
+        /// <summary>
+        /// Handles the click event for the color set buttons.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void UiImgColors_3_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            UiImgColorsSelector.Margin = new Thickness(UiImgColors_3.Margin.Left - 1, UiImgColors_3.Margin.Top - 1,
+                                                       UiImgColors_3.Margin.Right + 1, UiImgColors_3.Margin.Bottom + 1);
+            _selectedColorSet = 3;
+        }
+
+        /// <summary>
+        /// Handles the click event for the color set buttons.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void UiImgColors_4_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            UiImgColorsSelector.Margin = new Thickness(UiImgColors_4.Margin.Left - 1, UiImgColors_4.Margin.Top - 1,
+                                                       UiImgColors_4.Margin.Right + 1, UiImgColors_4.Margin.Bottom + 1);
+            _selectedColorSet = 4;
+        }
+
+        /// <summary>
+        /// Handles the click event for the color set buttons.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void UiImgColors_5_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            UiImgColorsSelector.Margin = new Thickness(UiImgColors_5.Margin.Left - 1, UiImgColors_5.Margin.Top - 1,
+                                                       UiImgColors_5.Margin.Right + 1, UiImgColors_5.Margin.Bottom + 1);
+            _selectedColorSet = 5;
+        }
+
+        /// <summary>
+        /// Handles the click event for the color set buttons.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void UiImgColors_6_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            UiImgColorsSelector.Margin = new Thickness(UiImgColors_6.Margin.Left - 1, UiImgColors_6.Margin.Top - 1,
+                                                       UiImgColors_6.Margin.Right + 1, UiImgColors_6.Margin.Bottom + 1);
+            _selectedColorSet = 6;
         }
 
         /// <summary>
@@ -35,6 +153,7 @@ namespace ChessForge
             sideSize = Math.Min(sideSize, Constants.MAX_DIAGRAM_SIZE);
             
             Configuration.DiagramImageSize = sideSize;
+            Configuration.DiagramImageColors = _selectedColorSet;
             Configuration.DoNotAskDiagramImageSize = UiCbDoNotAskAgain.IsChecked == true;
             
             DialogResult = true;
