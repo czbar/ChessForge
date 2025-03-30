942043b781f1cff99de4d9911eb489e91af23ecc
czbar <czbar@users.noreply.github.com>
2025-03-30 12:13:46 +0200

    Update

--- ChessForge/Properties/Resources.Designer.cs
+++ ChessForge/Properties/Resources.Designer.cs
@@ -4156,6 +4156,15 @@ namespace ChessForge.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Intros.
+        /// </summary>
+        public static string Intros {
+            get {
+                return ResourceManager.GetString("Intros", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Invalid engine move received.
         /// </summary>
         public static string InvalidEngineMoveReceived {
