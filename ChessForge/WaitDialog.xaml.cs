7a07665b085c527e5c1501bad5845b82d3db539a
czbar <czbar@users.noreply.github.com>
2025-04-08 17:27:23 +0200

    Created WaitDialog

---
+++ ChessForge/WaitDialog.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace ChessForge
+{
+    /// <summary>
+    /// Interaction logic for WaitDialog.xaml
+    /// </summary>
+    public partial class WaitDialog : Window
+    {
+        public WaitDialog(string operation)
+        {
+            InitializeComponent();
+
+            UiLblProcess.Content = operation;
+            UiLblPleaseWait.Content = Properties.Resources.PleaseWait + "...";
+        }
+    }
+}
