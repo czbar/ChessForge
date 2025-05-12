14cca6409cb080f65159ab76028b9b7342737408
czbar <czbar@users.noreply.github.com>
2025-05-12 17:30:46 +0200

    Cleanup usings

--- ChessForge/BackgroundPgnProcessor.cs
+++ ChessForge/BackgroundPgnProcessor.cs
@@ -1,11 +1,7 @@
-﻿using GameTree;
-using ChessPosition;
+﻿using ChessPosition;
+using GameTree;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ChessForge
 {
