f8c855e531f8064a0f13745198937889ffb6bcee
czbar <czbar@users.noreply.github.com>
2025-05-18 14:53:51 +0200

    First version of DeleteLeafMove()

--- ChessForge/VariationTreeView.cs
+++ ChessForge/VariationTreeView.cs
@@ -1572,6 +1572,99 @@ namespace ChessForge
         }
 
         /// <summary>
+        /// Checks if the passed node is a 'super leaf' and if so, deletes it.
+        /// Cleans up all the artefacts associated with the node.
+        /// By 'super leaf' we mean a node that has no children and no siblings.
+        /// Additionally we will not delete here if there is a move Run representing a node 
+        /// after this one in the same paragraph.
+        /// This is because there may be a layout one day that could allow that.
+        /// </summary>
+        /// <param name="nd"></param>
+        /// <returns>True if the node was deleted.</returns>
+        public bool DeleteLeafMove(TreeNode nd)
+        {
+            // TODO: to be used when deleteing a move from the view.
+            bool success = false;
+
+            try
+            {
+                if (nd != null && nd.Children.Count == 0 && nd.Parent != null && !TreeUtils.NodeHasSiblings(nd))
+                {
+                    // get the run for the move
+                    if (_dictNodeToRun.TryGetValue(nd.NodeId, out Run rMoveRun))
+                    {
+                        bool proceed = true;
+                        Inline inline = rMoveRun.NextInline;
+                        
+                        List<Run> lstRunsToRemove = new List<Run>();
+                        lstRunsToRemove.Add(rMoveRun);
+
+                        while (inline != null)
+                        {
+                            inline = inline.NextInline;
+                            if (inline is Run run)
+                            {
+                                if (run.Name != null && run.Name.StartsWith(RichTextBoxUtilities.RunMovePrefix))
+                                {
+                                    proceed = false;
+                                    break;
+                                }
+                                else
+                                {
+                                    lstRunsToRemove.Add(run);
+                                }
+                            }
+                            else
+                            {
+                                proceed = false;
+                                break;
+                            }
+                        }
+
+                        if (proceed)
+                        {
+                            Paragraph para = _dictRunToParagraph[rMoveRun];
+                            if (para != null)
+                            {
+                                // remove the node from the tree
+                                nd.Parent.Children.Remove(nd);
+
+                                // remove the entries from the dictionaries
+                                _dictNodeToRun.Remove(nd.NodeId);
+
+                                Run commentRun = _dictNodeToCommentRun[nd.NodeId] as Run;
+                                _dictNodeToCommentRun.Remove(nd.NodeId);
+
+                                Run commentBeforeRun = _dictNodeToCommentBeforeMoveRun[nd.NodeId] as Run;
+                                _dictNodeToCommentBeforeMoveRun.Remove(nd.NodeId);
+
+                                _dictRunToParagraph.Remove(rMoveRun);
+                                _dictCommentRunToParagraph.Remove(commentRun);
+                                _dictCommentBeforeMoveRunToParagraph.Remove(commentBeforeRun);
+
+                                // remove the runs associated with the move.
+                                foreach (Run r in lstRunsToRemove)
+                                {
+                                    if (r != null)
+                                    {
+                                        para.Inlines.Remove(r);
+                                    }
+                                }
+                            }
+                            success = true;
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                success = false;
+            }
+
+            return success;
+        }
+
+        /// <summary>
         /// Each invoked instance builds text of a single Line in the Workbook. 
         /// Calls itself recursively and returns the text of the complete
         /// Workbook.
