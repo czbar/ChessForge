25a71a4734616932aef52714d4c7c0a54af31b8a
czbar <czbar@users.noreply.github.com>
2025-10-07 12:32:50 +0200

    Refined the Help link

--- ChessForge/SearchPositionDialog.xaml.cs
+++ ChessForge/SearchPositionDialog.xaml.cs
@@ -793,7 +793,7 @@ namespace ChessForge
         /// <param name="e"></param>
         private void UiBtnHelp_Click(object sender, RoutedEventArgs e)
         {
-            System.Diagnostics.Process.Start("https://github.com/czbar/ChessForge/wiki/Finding-Positions");
+            System.Diagnostics.Process.Start("https://github.com/czbar/ChessForge/wiki/Finding-Positions#partial-match-positions");
         }
 
         //************************************************************
