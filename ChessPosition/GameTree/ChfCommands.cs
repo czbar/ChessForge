92db08b926e4f6cd6be3404507e705669967064f
czbar <czbar@users.noreply.github.com>
2026-01-26 12:09:25 +1100

    Updated CHESS_BASE_DIAGRAM_LONG

--- ChessPosition/GameTree/ChfCommands.cs
+++ ChessPosition/GameTree/ChfCommands.cs
@@ -25,7 +25,7 @@ namespace GameTree
 
         // ChessBase diagram command
         public const string CHESS_BASE_DIAGRAM = "[#]";
-        public const string CHESS_BASE_DIAGRAM_LONG = "Diagram [#]";
+        public const string CHESS_BASE_DIAGRAM_LONG = "Diagram #";
 
         /// <summary>
         /// Command IDs
