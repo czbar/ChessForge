0ffb935b85c0aa90d1aa6ab37ff7bc10450450d4
czbar <czbar@users.noreply.github.com>
2026-03-04 05:12:22 +1100

    Implemented SetHeader methods

--- WebAccess/RestApiRequest.cs
+++ WebAccess/RestApiRequest.cs
@@ -15,6 +15,35 @@ namespace WebAccess
         static public HttpClient TablebaseClient = new HttpClient();
         static public HttpClient GameImportClient = new HttpClient();
 
+        // Lichess API token, to be set by the user in the settings dialog. Required for making requests to lichess API.
+        public static string LichessAuthToken = "";
+
+        /// <summary>
+        /// Number of retries to do if we get 429 Too Many Requests response from lichess API.
+        /// </summary>
+        public static int LichessApiRetries = 3;
+
+        /// <summary>
+        /// Sets the default headers for the HttpClient to be used for making requests to lichess API. This includes the User-Agent and Authorization headers.
+        /// </summary>
+        /// <param name="httpClient"></param>
+        public static void SetHttpClientLichessDefaultHeaders(HttpClient httpClient)
+        {
+            httpClient.DefaultRequestHeaders.Clear();
+            httpClient.DefaultRequestHeaders.Add("User-Agent", RestApiRequest.UserAgentLichess);
+            httpClient.DefaultRequestHeaders.Add("Authorization", "Bearer " + LichessAuthToken);
+        }
+
+        /// <summary>
+        /// Sets the default headers for the HttpClient to be used for making requests to chess.com API. This includes the User-Agent header.
+        /// </summary>
+        /// <param name="httpClient"></param>
+        public static void SetHttpClientChesscomDefaultHeaders(HttpClient httpClient)
+        {
+            httpClient.DefaultRequestHeaders.Clear();
+            httpClient.DefaultRequestHeaders.Add("User-Agent", RestApiRequest.UserAgentChesscom);
+        }
+
         /// <summary>
         /// Parses the response from the server to extract the reponse code.
         /// </summary>
