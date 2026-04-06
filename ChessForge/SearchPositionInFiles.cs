abd346dcd0137631bffe0284ece4a86cbf89c42e
czbar <czbar@users.noreply.github.com>
2026-04-06 19:04:10 +1000

    Placeholder implementation

---
+++ ChessForge/SearchPositionInFiles.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessForge
+{
+    public class SearchPositionInFiles
+    {
+        public static void Search(SearchPositionCriteria crits)
+        {
+            SearchPgnFilesDialog dlg = new SearchPgnFilesDialog(crits);
+            GuiUtilities.PositionDialog(dlg, AppState.MainWin, 100);
+
+            if (dlg.ShowDialog() == true)
+            {
+            // Open a PGN file marked as selected.
+            // TODO: implement
+            }
+        }
+    }
+}
