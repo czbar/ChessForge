10252185a4874560a03ce26f43530b3ed7a325c9
czbar <czbar@users.noreply.github.com>
2026-04-06 19:03:29 +1000

    Basic implementation
    No search functionality yet

--- ChessForge/SearchPgnFilesDialog.xaml.cs
+++ ChessForge/SearchPgnFilesDialog.xaml.cs
@@ -1,16 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace ChessForge
 {
@@ -19,9 +10,115 @@ namespace ChessForge
     /// </summary>
     public partial class SearchPgnFilesDialog : Window
     {
-        public SearchPgnFilesDialog()
+        // root folder to search for PGN files in
+        private string _rootFolder;
+
+        // whether a search is in progress
+        private bool _isSearchInProgress = false;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="crits"></param>
+        public SearchPgnFilesDialog(SearchPositionCriteria crits)
         {
             InitializeComponent();
+            _rootFolder = Configuration.LastOpenDirectory;
+            UiTbDirectory.Text = _rootFolder;
+        }
+
+        /// <summary>
+        /// Gets the list of PGN files in the passed folder and its subfolders.
+        /// </summary>
+        /// <param name="rootFolder"></param>
+        /// <returns></returns>
+        private static IEnumerable<string> GetPgnFilesSafe(string rootFolder)
+        {
+            if (string.IsNullOrWhiteSpace(rootFolder))
+                yield break;
+
+            var pending = new Stack<string>();
+            pending.Push(rootFolder);
+
+            while (pending.Count > 0)
+            {
+                string currentFolder = pending.Pop();
+
+                string[] subfolders;
+                try
+                {
+                    subfolders = Directory.GetDirectories(currentFolder);
+                }
+                catch
+                {
+                    continue;
+                }
+
+                foreach (string subfolder in subfolders)
+                    pending.Push(subfolder);
+
+                string[] files;
+                try
+                {
+                    files = Directory.GetFiles(currentFolder, "*.pgn");
+                }
+                catch
+                {
+                    continue;
+                }
+
+                foreach (string file in files)
+                    yield return file;
+            }
+        }
+
+        /// <summary>
+        /// In response to the user clicking the select button,
+        /// opens the dialog to select the root folder to search for PGN files in.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void UiBtnSelectFolder_Click(object sender, RoutedEventArgs e)
+        {
+            _rootFolder = FolderPicker.ShowDialog(this, Properties.Resources.SelectFolder, UiTbDirectory.Text);
+        }
+
+        /// <summary>
+        /// The user clicks the start/stop button, we either start the search for the position in the PGN files
+        /// or stop it if it is already in progress.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void UiBtnStartStop_Click(object sender, RoutedEventArgs e)
+        {
+            if (_isSearchInProgress)
+            {
+                // stop the search
+                _isSearchInProgress = false;
+                UiBtnStartStop.Content = Properties.Resources.Search;
+            }
+            else
+            {
+
+                UiBtnStartStop.Content = Properties.Resources.Stop;
+                List<string> files = GetPgnFilesSafe(_rootFolder).ToList();
+                if (files.Count > 0)
+                {
+                    _isSearchInProgress = true;
+                    // begin background search for the position in the files
+                    //TODO: implement the search and report the results in the dialog
+                }
+            }
+        }
+
+        /// <summary>
+        /// The user clicks the close button, we just close the dialog.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void UiBtnClose_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
         }
     }
 }
