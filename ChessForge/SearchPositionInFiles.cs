4cba3e60073e4457fd0420903979b984d136875d
czbar <czbar@users.noreply.github.com>
2026-04-07 12:33:59 +1000

    Update SearchPositionInFiles.cs

--- ChessForge/SearchPositionInFiles.cs
+++ ChessForge/SearchPositionInFiles.cs
@@ -1,8 +1,6 @@
-﻿using System;
+﻿using GameTree;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace ChessForge
 {
@@ -15,9 +13,45 @@ namespace ChessForge
 
             if (dlg.ShowDialog() == true)
             {
-            // Open a PGN file marked as selected.
-            // TODO: implement
+                // Open a PGN file marked as selected.
+                // TODO: implement
             }
+        }
+
+        public static List<string> FindFilesWithPosition(List<string> paths, SearchPositionCriteria crits)
+        {
+            List<string> filesWithPosition = new List<string>();
+            foreach (string path in paths)
+            {
+                List<GameData> games = PgnMultiGameParser.ParsePgnMultiGameText(File.ReadAllText(path), out _);
+
+                foreach (GameData gm in games)
+                {
+                    string fen = gm.Header.GetFenString();
+                    if (!gm.Header.IsExercise())
+                    {
+                        fen = null;
+                    }
+
+                    try
+                    {
+                        VariationTree tree = new VariationTree(gm.GetContentType(false));
+                        PgnGameParser parser = new PgnGameParser(gm.GameText, tree, fen);
+                        List<TreeNode> lstNodes = SearchPosition.FindIdenticalNodes(tree, crits, true);
+                        if (lstNodes.Count > 0)
+                        {
+                            filesWithPosition.Add(path);
+                            break;
+                        }
+                    }
+                    catch
+                    {
+                        continue;
+                    }
+                }
+            }
+
+            return filesWithPosition;
         }
     }
 }
