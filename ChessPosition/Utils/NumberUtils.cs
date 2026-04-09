5c0dc14290c5093ac4e423b33c0db8a389382b89
czbar <czbar@users.noreply.github.com>
2026-04-09 19:38:41 +1000

    Created NumberUtils.cs

---
+++ ChessPosition/Utils/NumberUtils.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessPosition
+{
+    public class NumberUtils
+    {
+        /// <summary>
+        /// Parses a double value from a string, trying first the current culture 
+        /// and if that fails the invariant culture formats.
+        /// </summary>
+        /// <param name="sDouble"></param>
+        /// <param name="dValue"></param>
+        /// <returns></returns>
+        public static bool ParseDouble(string sDouble, out double dValue)
+        {
+            // try parsing with the current culture (e.g. for cases when the decimal separator is not dot but comma)
+            bool success = double.TryParse(sDouble, out dValue);
+
+            if (!success)
+            {
+                // try parsing with invariant culture (the decimal separator is a comma)
+                success = double.TryParse(sDouble, System.Globalization.NumberStyles.Any, System.Globalization.CultureInfo.InvariantCulture, out dValue);
+            }
+
+            return success;
+        }
+    }
+}
