5a90485538c3354d86fd7135fbbb76a57c26330b
czbar <czbar@users.noreply.github.com>
2026-04-10 17:09:33 +1000

    Improved ParseDouble

--- ChessPosition/Utils/NumberUtils.cs
+++ ChessPosition/Utils/NumberUtils.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Globalization;
 
 namespace ChessPosition
 {
@@ -17,14 +13,15 @@ namespace ChessPosition
         /// <returns></returns>
         public static bool ParseDouble(string sDouble, out double dValue)
         {
-            // try parsing with the current culture (e.g. for cases when the decimal separator is not dot but comma)
-            bool success = double.TryParse(sDouble, out dValue);
+            bool success = false;
 
-            if (!success)
+            if (!string.IsNullOrEmpty(sDouble))
             {
-                // try parsing with invariant culture (the decimal separator is a comma)
-                success = double.TryParse(sDouble, System.Globalization.NumberStyles.Any, System.Globalization.CultureInfo.InvariantCulture, out dValue);
+                sDouble = sDouble.Replace(',', '.');
             }
+
+            // try parsing with invariant culture (the decimal separator is a comma)
+            success = double.TryParse(sDouble, NumberStyles.Float, CultureInfo.InvariantCulture, out dValue);
 
             return success;
         }
