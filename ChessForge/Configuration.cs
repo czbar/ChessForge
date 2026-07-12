371c66f9991e9bc9a39dd4985dbf28fa7e535c44
czbar <czbar@users.noreply.github.com>
2026-07-12 20:53:56 +0200

    Added Cleanup* Session parameters

--- ChessForge/Configuration.cs
+++ ChessForge/Configuration.cs
@@ -154,6 +154,41 @@ namespace ChessForge
         /// </summary>
         public static string LastPgnSearchDirectory = "";
 
+        //**
+        // Cleanup operation parameters
+        //**
+
+        /// <summary>
+        /// Flags determining which move attributes to remove when cleaning up articles.
+        /// </summary>
+        public static int CleanupMoveAttrs = 0;
+
+        /// <summary>
+        /// Flags determining which article attributes to remove when cleaning up articles.
+        /// </summary>
+        public static int CleanupArticleAttrs = 0;
+
+        /// <summary>
+        /// The scope of the cleanup operation (Study, Games, Exercises).
+        /// </summary>
+        public static OperationScope CleanupScope { get; set; }
+
+        /// <summary>
+        /// Whether to apply the operation to Study/Studies.
+        /// </summary>
+        public static bool CleanupApplyToStudies { get; set; }
+
+        /// <summary>
+        /// Whether to apply the operation to Model Games.
+        /// </summary>
+        public static bool CleanupApplyToGames { get; set; }
+
+        /// <summary>
+        /// Whether to apply the operation to Exercises.
+        /// </summary>
+        public static bool CleanupApplyToExercises { get; set; }
+
+
         //*********************************
         //
         //   CONFIGURATION ITEMS
